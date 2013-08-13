6aa1ab6379849399df9ece8d95d0c90ac8affef6
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2013-08-13 14:18:26 +0200

    TrackMate_: add an action that can merge data from another file
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/gui/descriptors/SomeDialogDescriptor.java
+++ src/main/java/fiji/plugin/trackmate/gui/descriptors/SomeDialogDescriptor.java
@@ -6,31 +6,31 @@ import java.io.File;
 import fiji.plugin.trackmate.gui.LogPanel;
 
 /**
- * An abstract class made for describing panels that generate a dialog, 
- * like save and load panels.
+ * An abstract class made for describing panels that generate a dialog, like
+ * save and load panels.
+ * 
  * @author Jean-Yves Tinevez
- *
+ * 
  */
 public abstract class SomeDialogDescriptor implements WizardPanelDescriptor {
 
 	/**
-	 * File that governs saving and loading. We make it a static field so that 
-	 * loading and sharing events always point to a single file location by default.
+	 * File that governs saving and loading. We make it a static field so that
+	 * loading and sharing events always point to a single file location by
+	 * default.
 	 */
-	protected static File file;
+	public static File file;
 	protected final LogPanel logPanel;
-	
 
-	public SomeDialogDescriptor(LogPanel logPanel) {
+	public SomeDialogDescriptor(final LogPanel logPanel) {
 		this.logPanel = logPanel;
 	}
-	
 
 	@Override
 	public Component getComponent() {
 		return logPanel;
 	}
-	
+
 	@Override
 	public void aboutToDisplayPanel() {}
 
