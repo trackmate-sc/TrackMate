7b44fec8500dd15608b04a0302ee5ddc43d796c1
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2013-11-29 09:19:43 +0100

    TrackMate_: put back the old DogDetector implementation.
    The speed toll is too high.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/detection/DogDetector.java
+++ src/main/java/fiji/plugin/trackmate/detection/DogDetector.java
@@ -1,26 +1,25 @@
 package fiji.plugin.trackmate.detection;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import net.imglib2.Interval;
-import net.imglib2.Point;
 import net.imglib2.RandomAccess;
-import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.algorithm.dog.DogDetection;
-import net.imglib2.algorithm.localextrema.RefinedPeak;
-import net.imglib2.exception.IncompatibleTypeException;
+import net.imglib2.algorithm.legacy.scalespace.DifferenceOfGaussian;
+import net.imglib2.algorithm.legacy.scalespace.DifferenceOfGaussianPeak;
+import net.imglib2.algorithm.legacy.scalespace.SubpixelLocalization;
 import net.imglib2.img.Img;
 import net.imglib2.img.ImgFactory;
 import net.imglib2.meta.ImgPlus;
+import net.imglib2.outofbounds.OutOfBoundsFactory;
+import net.imglib2.outofbounds.OutOfBoundsMirrorExpWindowingFactory;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
-import net.imglib2.view.Views;
 import fiji.plugin.trackmate.Spot;
 import fiji.plugin.trackmate.util.TMUtils;
 
-public class DogDetector <T extends RealType<T>  & NativeType<T>> extends LogDetector<T> {
+public class DogDetector<T extends RealType<T> & NativeType<T>> extends LogDetector<T> {
 
 	/*
 	 * CONSTANTS
@@ -37,88 +36,111 @@ public class DogDetector <T extends RealType<T>  & NativeType<T>> extends LogDet
 		this.baseErrorMessage = BASE_ERROR_MESSAGE;
 	}
 
-
 	/*
 	 * METHODS
 	 */
-
 
 	@Override
 	public boolean process() {
 
 		final long start = System.currentTimeMillis();
 
-		/*
-		 * Copy to float otherwise it fails.
-		 */
-
-		ImgFactory<FloatType> factory = null;
-		try {
-			factory = img.factory().imgFactory(new FloatType());
-		} catch (final IncompatibleTypeException e) {
-			errorMessage = baseErrorMessage + "Failed creating float image factory: " + e.getMessage();
-			return false;
-		}
-		Img<FloatType> floatImg = copyToFloatImg(img, factory);
-
 		// Deal with median filter:
+		Img<T> intermediateImage = img;
 		if (doMedianFilter) {
-			floatImg = applyMedianFilter(floatImg);
-			if (null == floatImg) {
+			intermediateImage = applyMedianFilter(intermediateImage);
+			if (null == intermediateImage) {
 				return false;
 			}
 		}
 
-		final double s1 = 0.95 * radius / Math.sqrt(img.numDimensions());
-		final double s2 = 1.05 * radius / Math.sqrt(img.numDimensions());
 		final double[] calibration = TMUtils.getSpatialCalibration(img);
 
-		final Dd<FloatType> dog = new Dd<FloatType>(Views.extendMirrorSingle(floatImg), floatImg, calibration, s2, s1, DogDetection.ExtremaType.MAXIMA, -threshold);
-		dog.setNumThreads(numThreads);
+		// First we need an image factory
+		final ImgFactory<T> imageFactory = img.factory();
 
-		final ArrayList<RefinedPeak<Point>> peaks;
-		if (doSubPixelLocalization) {
-			peaks = dog.getSubpixelPeaks();
-		} else {
-			final ArrayList<Point> sp = dog.getPeaks();
-			peaks = new ArrayList<RefinedPeak<Point>>(sp.size());
-			final RandomAccess<FloatType> ra = dog.getDogImg().randomAccess();
-			for (final Point point : sp) {
-				ra.setPosition(point);
-				peaks.add(new RefinedPeak<Point>(point, point, ra.get().getRealDouble(), true));
-			}
+		// And the out of bounds strategies for both types. It needs to be a value-oobs, with a constant
+		// value of 0; otherwise, we will miss maxima on the border of the image.
+		final OutOfBoundsFactory<T, RandomAccessibleInterval<T>> oobs2 = new OutOfBoundsMirrorExpWindowingFactory<T, RandomAccessibleInterval<T>>();
+
+		final double[] sigma1 = new double[img.numDimensions()];
+		final double[] sigma2 = new double[img.numDimensions()];
+		for (int i = 0; i < sigma2.length; i++) {
+			sigma1[i] = 2 / (1 + Math.sqrt(img.numDimensions())) * radius / calibration[i]; // in pixel units
+			sigma2[i] = Math.sqrt(img.numDimensions()) * sigma1[i];
 		}
 
-		spots = new ArrayList<Spot>(peaks.size());
-		for (final RefinedPeak<Point> peak : peaks) {
+		final DifferenceOfGaussian<T> dog = new DifferenceOfGaussian<T>(intermediateImage, imageFactory, oobs2, sigma1, sigma2, threshold, 1.0);
+		/*
+		 * The DogDetector class will be called in a multi-threaded way, so the
+		 * DifferenceOfGaussianRealNI does not need to be multi-threaded. On top
+		 * of that, reports from users on win32 platform indicate that
+		 * multi-threading generates some silent problems, with some frames
+		 * (first ones being not present in the final SpotColleciton. On 64-bit
+		 * platforms, I could see that keeping the DogRNI multi-threaded
+		 * translated by a speedup of about 10%, which I sacrifice without
+		 * hesitation if i can make the trackmate more stable.
+		 */
+		dog.setNumThreads(1);
+
+		// Keep laplace image if needed
+		if (doSubPixelLocalization)
+			dog.setKeepDoGImg(true);
+
+		// Execute
+		if (!dog.checkInput() || !dog.process()) {
+			errorMessage = baseErrorMessage + dog.getErrorMessage();
+			return false;
+		}
+
+		// Get all peaks
+		final List<DifferenceOfGaussianPeak<FloatType>> list = dog.getPeaks();
+		final RandomAccess<T> cursor = img.randomAccess();
+
+		// Prune non-relevant peaks
+		List<DifferenceOfGaussianPeak<FloatType>> pruned_list = new ArrayList<DifferenceOfGaussianPeak<FloatType>>();
+		for (final DifferenceOfGaussianPeak<FloatType> dogpeak : list) {
+			if ((dogpeak.getPeakType() != DifferenceOfGaussian.SpecialPoint.MAX))
+				continue;
+			cursor.setPosition(dogpeak);
+			if (cursor.get().getRealDouble() < threshold)
+				continue;
+
+			pruned_list.add(dogpeak);
+		}
+
+		// Deal with sub-pixel localization if required
+		if (doSubPixelLocalization && pruned_list.size() > 0) {
+			final Img<FloatType> laplacian = dog.getDoGImg();
+			final SubpixelLocalization<FloatType> locator = new SubpixelLocalization<FloatType>(laplacian, pruned_list);
+			locator.setNumThreads(1); // Since the calls to a segmenter  are already multi-threaded.
+			if (!locator.checkInput() || !locator.process()) {
+				errorMessage = baseErrorMessage + locator.getErrorMessage();
+				return false;
+			}
+			pruned_list = locator.getDoGPeaks();
+		}
+
+		// Create spots
+		spots.clear();
+		for (final DifferenceOfGaussianPeak<FloatType> dogpeak : pruned_list) {
 			final double[] coords = new double[3];
-			peak.localize(coords);
-			for (int d = 0; d < coords.length; d++) {
-				coords[d] *= calibration[d];
+			if (doSubPixelLocalization) {
+				for (int i = 0; i < img.numDimensions(); i++)
+					coords[i] = dogpeak.getSubPixelPosition(i) * calibration[i];
+			} else {
+				for (int i = 0; i < img.numDimensions(); i++)
+					coords[i] = dogpeak.getDoublePosition(i) * calibration[i];
 			}
 			final Spot spot = new Spot(coords);
-			spot.putFeature(Spot.QUALITY, Double.valueOf(peak.getValue()));
+			spot.putFeature(Spot.QUALITY, Double.valueOf(-dogpeak.getValue().get()));
 			spot.putFeature(Spot.RADIUS, radius);
 			spots.add(spot);
 		}
-
-		// Prune overlapping spots
-//		spots = TMUtils.suppressSpots(spots, Spot.QUALITY);
 
 		final long end = System.currentTimeMillis();
 		processingTime = end - start;
 
 		return true;
-	}
-
-	private static final class Dd<T extends RealType<T> & NativeType<T>> extends DogDetection<T> {
-
-		public Dd(final RandomAccessible<T> input, final Interval interval, final double[] calibration, final double sigma1, final double sigma2, final ExtremaType extremaType, final double minPeakValue) {
-			super(input, interval, calibration, sigma1, sigma2, extremaType, minPeakValue);
-		}
-
-		private RandomAccessibleInterval<T> getDogImg() {
-			return dogImg;
-		}
 	}
 }
