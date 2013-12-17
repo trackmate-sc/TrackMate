eb0bde3910e22e7fc3cf44af42f85e4f6c774062
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2013-12-17 11:15:44 +0100

    TrackMate_: code style changes.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/visualization/TrackMateModelView.java
+++ src/main/java/fiji/plugin/trackmate/visualization/TrackMateModelView.java
@@ -6,18 +6,16 @@ import java.util.Map;
 import org.jfree.chart.renderer.InterpolatePaintScale;
 
 import fiji.plugin.trackmate.InfoTextable;
-import fiji.plugin.trackmate.Spot;
 import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.Spot;
 
-public interface TrackMateModelView extends InfoTextable {
-
-
+public interface TrackMateModelView extends InfoTextable
+{
 
 	/*
 	 * KEY-VALUE CONSTANTS FOR LOOK & FILL CUSTOMIZATION
 	 */
 
-	
 	/*
 	 * KEYS
 	 */
@@ -28,97 +26,99 @@ public interface TrackMateModelView extends InfoTextable {
 	public static final String KEY_COLOR = "Color";
 
 	/**
-	 * Defines the key for the highlight color, used to paint selection. Accepted values are color.
+	 * Defines the key for the highlight color, used to paint selection.
+	 * Accepted values are color.
 	 */
 	public static final String KEY_HIGHLIGHT_COLOR = "HighlightColor";
-	
+
 	/**
 	 * Defines the key for the track display mode. Possible values are
 	 * {@link #TRACK_DISPLAY_MODE_WHOLE}, {@link #TRACK_DISPLAY_MODE_LOCAL},
-	 * {@link #TRACK_DISPLAY_MODE_LOCAL_BACKWARD}, {@value #TRACK_DISPLAY_MODE_LOCAL_FORWARD}.
+	 * {@link #TRACK_DISPLAY_MODE_LOCAL_BACKWARD},
+	 * {@value #TRACK_DISPLAY_MODE_LOCAL_FORWARD}.
 	 */
 	public static final String KEY_TRACK_DISPLAY_MODE = "TrackDisplaymode";
 
 	/**
-	 * Defines the key for the track display depth. Values are integer, and 
-	 * they defines how many frames away the track can be seen from the current
+	 * Defines the key for the track display depth. Values are integer, and they
+	 * defines how many frames away the track can be seen from the current
 	 * time-point.
 	 */
 	public static final String KEY_TRACK_DISPLAY_DEPTH = "TrackDisplayDepth";
 
 	/**
-	 * Defines the key for the track visibility. Values are boolean. If <code>false</code>,
-	 * tracks are not visible.
+	 * Defines the key for the track visibility. Values are boolean. If
+	 * <code>false</code>, tracks are not visible.
 	 */
 	public static final String KEY_TRACKS_VISIBLE = "TracksVisible";
-	
+
 	/**
-	 * Defines the key for the track coloring method. Values are concrete implementations
-	 * of {@link TrackColorGenerator}.
+	 * Defines the key for the track coloring method. Values are concrete
+	 * implementations of {@link TrackColorGenerator}.
 	 */
 	public static final String KEY_TRACK_COLORING = "TrackColoring";
 
 	/**
-	 * Defines the key for the spot visibility. Values are boolean. If <code>false</code>,
-	 * spots are not visible.
+	 * Defines the key for the spot visibility. Values are boolean. If
+	 * <code>false</code>, spots are not visible.
 	 * */
 	public static final String KEY_SPOTS_VISIBLE = "SpotsVisible";
 
 	/**
-	 * Defines the key for the spot name display. Values are boolean. If <code>false</code>,
-	 * spot names are not visible.
+	 * Defines the key for the spot name display. Values are boolean. If
+	 * <code>false</code>, spot names are not visible.
 	 * */
 	public static final String KEY_DISPLAY_SPOT_NAMES = "DisplaySpotNames";
 
 	/**
-	 * Defines the key for the spot radius ratio. Value should be a positive float number.
-	 * Spots will be rendered with a radius equals to their actual radius multiplied
-	 * by this ratio.
+	 * Defines the key for the spot radius ratio. Value should be a positive
+	 * float number. Spots will be rendered with a radius equals to their actual
+	 * radius multiplied by this ratio.
 	 */
 	public static final String KEY_SPOT_RADIUS_RATIO = "SpotRadiusRatio";
 
 	/**
-	 * Defines the key for the spot coloring method. Accepted values are implementation
-	 * of {@link FeatureColorGenerator}
+	 * Defines the key for the spot coloring method. Accepted values are
+	 * implementation of {@link FeatureColorGenerator}
 	 */
 	public static final String KEY_SPOT_COLORING = "SpotColoring";
 
 	/**
 	 * Defines the key for the color map to use for painting overlay. Acceptable
-	 * values are {@link InterpolatePaintScale}s, the default is {@link InterpolatePaintScale#Jet}.
+	 * values are {@link InterpolatePaintScale}s, the default is
+	 * {@link InterpolatePaintScale#Jet}.
 	 */
 	public static final String KEY_COLORMAP = "ColorMap";
-	
-	
+
 	/*
 	 * VALUES
 	 */
 
 	/**
-	 * Track display mode where the whole tracks are drawn, ignoring
-	 * the value of {@link #KEY_TRACK_DISPLAY_DEPTH}.
+	 * Track display mode where the whole tracks are drawn, ignoring the value
+	 * of {@link #KEY_TRACK_DISPLAY_DEPTH}.
 	 */
 	public static final int TRACK_DISPLAY_MODE_WHOLE = 0;
 
 	/**
 	 * Track display mode where the only part of the tracks close to the current
-	 * time-point are drawn backward and forward. Edges away from current time point
-	 * are faded in the background. How much can be seen is 
-	 * defined by the value of {@link #KEY_TRACK_DISPLAY_DEPTH}.
+	 * time-point are drawn backward and forward. Edges away from current time
+	 * point are faded in the background. How much can be seen is defined by the
+	 * value of {@link #KEY_TRACK_DISPLAY_DEPTH}.
 	 */
 	public static final int TRACK_DISPLAY_MODE_LOCAL = 1;
 
 	/**
 	 * Track display mode where the only part of the tracks close to the current
-	 * time-point are drawn, backward only. How much can be seen is 
-	 * defined by the value of {@link #KEY_TRACK_DISPLAY_DEPTH}.
+	 * time-point are drawn, backward only. How much can be seen is defined by
+	 * the value of {@link #KEY_TRACK_DISPLAY_DEPTH}.
 	 */
 	public static final int TRACK_DISPLAY_MODE_LOCAL_BACKWARD = 2;
 
 	/**
 	 * Track display mode where the only part of the tracks close to the current
-	 * time-point are drawn, forward only. How much can be seen is 
-	 * defined by the value of {@link #KEY_TRACK_DISPLAY_DEPTH}.
+	 * time-point are drawn, forward only. How much can be seen is defined by
+	 * the value of {@link #KEY_TRACK_DISPLAY_DEPTH}.
 	 */
 	public static final int TRACK_DISPLAY_MODE_LOCAL_FORWARD = 3;
 
@@ -129,14 +129,14 @@ public interface TrackMateModelView extends InfoTextable {
 	public static final int TRACK_DISPLAY_MODE_LOCAL_QUICK = 4;
 
 	/**
-	 * Track display mode similar to {@link #TRACK_DISPLAY_MODE_LOCAL_BACKWARD}, except
-	 * that for the sake of speed, edges are not faded.
+	 * Track display mode similar to {@link #TRACK_DISPLAY_MODE_LOCAL_BACKWARD},
+	 * except that for the sake of speed, edges are not faded.
 	 */
 	public static final int TRACK_DISPLAY_MODE_LOCAL_BACKWARD_QUICK = 5;
 
 	/**
-	 * Track display mode similar to {@link #TRACK_DISPLAY_MODE_LOCAL_FORWARD}, except
-	 * that for the sake of speed, edges are not faded.
+	 * Track display mode similar to {@link #TRACK_DISPLAY_MODE_LOCAL_FORWARD},
+	 * except that for the sake of speed, edges are not faded.
 	 */
 	public static final int TRACK_DISPLAY_MODE_LOCAL_FORWARD_QUICK = 6;
 
@@ -144,69 +144,64 @@ public interface TrackMateModelView extends InfoTextable {
 	 * DESCRIPTIONS
 	 */
 
-	/** 
+	/**
 	 * String that describe the corresponding track display mode.
 	 */
-	public static final String[] TRACK_DISPLAY_MODE_DESCRIPTION = new String[] {
-		"Show all entire tracks",
-		"Show local tracks",
-		"Show local tracks, backward",
-		"Show local tracks, forward",
-		"Local tracks (fast)",
-		"Local tracks, backward (fast)",
-		"Local tracks, forward (fast)"
-	};
-	
+	public static final String[] TRACK_DISPLAY_MODE_DESCRIPTION = new String[] { "Show all entire tracks", "Show local tracks", "Show local tracks, backward", "Show local tracks, forward", "Local tracks (fast)", "Local tracks, backward (fast)", "Local tracks, forward (fast)" };
+
 	/*
 	 * DEFAULTS
 	 */
 
-	/** 
+	/**
 	 * The default color for spots.
 	 */
-	public static final Color DEFAULT_SPOT_COLOR = new Color(1f, 0, 1f);
-	
-	/** 
+	public static final Color DEFAULT_SPOT_COLOR = new Color( 1f, 0, 1f );
+
+	/**
 	 * The default track color.
 	 */
-	public static final Color DEFAULT_TRACK_COLOR = new Color(250, 250, 0);
+	public static final Color DEFAULT_TRACK_COLOR = new Color( 250, 250, 0 );
 
-
-	/** 
+	/**
 	 * The default color for highlighting.
 	 */
-	public static final Color DEFAULT_HIGHLIGHT_COLOR = new Color(0, 1f, 0);
+	public static final Color DEFAULT_HIGHLIGHT_COLOR = new Color( 0, 1f, 0 );
 
 	/**
 	 * The default track display mode.
 	 */
 	public static final int DEFAULT_TRACK_DISPLAY_MODE = TRACK_DISPLAY_MODE_WHOLE;
-	
+
 	/**
 	 * The default track display mode
 	 */
 	public static final int DEFAULT_TRACK_DISPLAY_DEPTH = 10;
-	
-	/** 
-	 * The default color map. 
+
+	/**
+	 * The default color map.
 	 */
 	public static final InterpolatePaintScale DEFAULT_COLOR_MAP = InterpolatePaintScale.Jet;
+
+	public static final int DEFAULT_DRAW_DEPTH = 10;
 
 	/*
 	 * INTERFACE METHODS
 	 */
 
 	/**
-	 * Initializes this displayer and render it according to its concrete implementation, 
-	 * target model.
+	 * Initializes this displayer and render it according to its concrete
+	 * implementation, target model.
+	 *
 	 * @see #setModel(Model)
 	 */
 	public void render();
 
 	/**
-	 * Refreshes the displayer display with current model. If the underlying model was modified,
-	 * or the display settings were changed, calling this method should be enough to update 
-	 * the display with changes.
+	 * Refreshes the displayer display with current model. If the underlying
+	 * model was modified, or the display settings were changed, calling this
+	 * method should be enough to update the display with changes.
+	 *
 	 * @see #setDisplaySettings(String, Object)
 	 */
 	public void refresh();
@@ -215,34 +210,37 @@ public interface TrackMateModelView extends InfoTextable {
 	 * Removes any overlay (for spots or tracks) from this displayer.
 	 */
 	public void clear();
-	
+
 	/**
 	 * Centers the view on the given spot.
 	 */
-	public void centerViewOn(final Spot spot);
+	public void centerViewOn( final Spot spot );
 
 	/**
 	 * Returns the current display settings map.
 	 */
-	public Map<String, Object> getDisplaySettings();
+	public Map< String, Object > getDisplaySettings();
 
-	/** 
-	 * Set a display parameter.
-	 * @param key  the key of the parameter to change.
-	 * @param value  the value for the display parameter
-	 */
-	public void setDisplaySettings(final String key, final Object value);
-	
 	/**
-	 * Returns the value of a specific display parameter. 
+	 * Set a display parameter.
+	 *
+	 * @param key
+	 *            the key of the parameter to change.
+	 * @param value
+	 *            the value for the display parameter
 	 */
-	public Object getDisplaySettings(final String key);
-	
+	public void setDisplaySettings( final String key, final Object value );
+
+	/**
+	 * Returns the value of a specific display parameter.
+	 */
+	public Object getDisplaySettings( final String key );
+
 	/**
 	 * Returns the model displayed in this view.
 	 */
 	public Model getModel();
-	
+
 	/**
 	 * Returns a unique identifier that identifies this view.
 	 */
