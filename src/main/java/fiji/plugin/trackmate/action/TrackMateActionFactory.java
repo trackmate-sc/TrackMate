df22ade1aa8cd29542842395a63b6267c3641463
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-01-17 14:19:37 +0100

    Action factories are now TrackMateModules.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/action/TrackMateActionFactory.java
+++ src/main/java/fiji/plugin/trackmate/action/TrackMateActionFactory.java
@@ -1,28 +1,9 @@
 package fiji.plugin.trackmate.action;
 
-import javax.swing.ImageIcon;
-
-import org.scijava.plugin.SciJavaPlugin;
-
 import fiji.plugin.trackmate.TrackMateModule;
 import fiji.plugin.trackmate.gui.TrackMateGUIController;
 
-public interface TrackMateActionFactory extends SciJavaPlugin, TrackMateModule
+public interface TrackMateActionFactory extends TrackMateModule
 {
 	public TrackMateAction create( TrackMateGUIController controller );
-
-	/**
-	 * Returns the icon for this action. Can be null.
-	 */
-	public ImageIcon getIcon();
-
-	/**
-	 * Returns the unique identifier of this action.
-	 *
-	 * @return the action key, as a string.
-	 */
-	public String getKey();
-
-	public String getName();
-
 }
