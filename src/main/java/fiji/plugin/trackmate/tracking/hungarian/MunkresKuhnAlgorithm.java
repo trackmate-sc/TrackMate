85c70899c73db679847bfb2a6c2c574042878503
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-01-20 11:06:36 -0600

    Move the Munkres-Kuhn test into src/test/ where it belongs
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/main/java/fiji/plugin/trackmate/tracking/hungarian/MunkresKuhnAlgorithm.java
+++ src/main/java/fiji/plugin/trackmate/tracking/hungarian/MunkresKuhnAlgorithm.java
@@ -1,7 +1,5 @@
 package fiji.plugin.trackmate.tracking.hungarian;
 
-import java.util.Arrays;
-
 /** This implements optimal matching between two sets given a weight matrix
  * (where the goal is to minimize the cumulative weight of the matches).
  * <p>
@@ -346,34 +344,5 @@ public class MunkresKuhnAlgorithm implements AssignmentAlgorithm {
 			y = nextY;
 		}
 		return "[" + result;
-	}
-
-	public static void main(String[] args) {
-		double[][] weight = new double[3][3];
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++)
-				weight[i][j] = Math.floor(Math.random() * 10);
-			System.err.println(Arrays.toString(weight[i]));
-		}
-
-		MunkresKuhnAlgorithm algo = new MunkresKuhnAlgorithm();
-		algo.computeAssignments(weight);
-		for (int x = 0; x < algo.M; x++)
-			System.err.println("map " + x + " to " + algo.matchingY[x]);
-		double minWeight = algo.getTotalWeight();
-		System.err.println("weight: " + minWeight);
-		for (int a = 0; a < 3; a++) {
-			double weight1 = weight[0][a];
-			for (int b = 0; b < 3; b++)
-				if (a != b) {
-					int c = 3 - a - b;
-					double weight2 = weight1 + weight[1][b] + weight[2][c];
-					if (weight2 > minWeight)
-						continue;
-					if (weight2 < minWeight)
-						System.err.println("ERROR: " + weight2);
-					System.err.println("0-" + a + " 1-" + b + " 2-" + c);
-				}
-		}
 	}
 }
