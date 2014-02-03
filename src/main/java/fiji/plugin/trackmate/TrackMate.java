b842eea4a0e6a51f4bb14804213976f5af1c453b
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-02-03 09:05:54 +0100

    En route for v2.2.0-rc3.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/TrackMate.java
+++ src/main/java/fiji/plugin/trackmate/TrackMate.java
@@ -44,7 +44,7 @@ public class TrackMate implements Benchmark, MultiThreaded, Algorithm
 
 	public static final String PLUGIN_NAME_STR = "TrackMate";
 
-	public static final String PLUGIN_NAME_VERSION = "2.2.0-rc2";
+	public static final String PLUGIN_NAME_VERSION = "2.2.0-rc3";
 
 	/**
 	 * The model this trackmate will shape.
