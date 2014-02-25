c82fa37289349938e19b33a4d6f4159f764f2776
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-02-25 15:10:44 +0100

    Code style changes.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/features/FeatureFilter.java
+++ src/main/java/fiji/plugin/trackmate/features/FeatureFilter.java
@@ -2,27 +2,32 @@ package fiji.plugin.trackmate.features;
 
 /**
  * A helper class to store a feature filter. It is just made of 3 public fields.
+ *
  * @author Jean-Yves Tinevez <jeanyves.tinevez@gmail.com> Sep 23, 2010
  *
  */
-public class FeatureFilter {
+public class FeatureFilter
+{
 	public final String feature;
+
 	public final Double value;
+
 	public final boolean isAbove;
-	
-	
-	public FeatureFilter(String feature, Double value, boolean isAbove) {
+
+	public FeatureFilter( final String feature, final Double value, final boolean isAbove )
+	{
 		this.feature = feature;
 		this.value = value;
 		this.isAbove = isAbove;
 	}
-	
+
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		String str = feature.toString();
-		if (isAbove)
+		if ( isAbove )
 			str += " > ";
-		else 
+		else
 			str += " < ";
 		str += value.toString();
 		return str;
