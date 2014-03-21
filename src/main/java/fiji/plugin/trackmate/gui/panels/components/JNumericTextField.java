e7f1eceb1e4f57935c8be8d80655262b7683bc87
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-03-21 16:22:55 +0100

    Fix a potential (and potentially long standing) bug with numerical input.
    The Locale we were using in JNuericTextfield uses the comma ',' as
    thousands separator, which breaks reading it with Double.parseDouble...
    We get back to using the plain
        "" + val
    command to make a string out of a double.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/gui/panels/components/JNumericTextField.java
+++ src/main/java/fiji/plugin/trackmate/gui/panels/components/JNumericTextField.java
@@ -5,8 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.text.NumberFormat;
-import java.util.Locale;
 
 import javax.swing.JTextField;
 import javax.swing.border.Border;
@@ -18,33 +16,33 @@ public class JNumericTextField extends JTextField {
 	/*
 	 * FIELDS
 	 */
-	
-	private static final long serialVersionUID = 5967459761896269716L;
-	private static final Border BORDER_FOCUSED = new LineBorder(new Color(252, 117, 0), 1, true); 
-	private static final Border BORDER_UNFOCUSED = new LineBorder(new Color(150, 150, 150), 1, true); 
+
+	private static final long serialVersionUID = 1L;
+	private static final Border BORDER_FOCUSED = new LineBorder(new Color(252, 117, 0), 1, true);
+	private static final Border BORDER_UNFOCUSED = new LineBorder(new Color(150, 150, 150), 1, true);
 	private final ActionListener al = new ActionListener() {
-		
+
 		@Override
-		public void actionPerformed(ActionEvent e) {
+		public void actionPerformed(final ActionEvent e) {
 			checkInput();
 		}
 	};
 	private double value = 0;
-	private double oldValue = 0; 
-	
-	
+	private double oldValue = 0;
+
+
 	/*
 	 * CONSTRUCTORS
 	 */
-	
-	public JNumericTextField(Document doc, String text, int columns) {
+
+	public JNumericTextField(final Document doc, final String text, final int columns) {
 		super(doc, text, columns);
 		setBorder(BORDER_UNFOCUSED);
 		if (text != null) {
 			try {
 				value = Double.parseDouble(text);
 				oldValue = value;
-			} catch (NumberFormatException nfe) {
+			} catch (final NumberFormatException nfe) {
 				oldValue = 0;
 				value = 0;
 			}
@@ -52,61 +50,61 @@ public class JNumericTextField extends JTextField {
 		addActionListener(al);
 		addFocusListener(new FocusListener() {
 			@Override
-			public void focusLost(FocusEvent e) {
+			public void focusLost(final FocusEvent e) {
 				checkInput();
 				setBorder(BORDER_UNFOCUSED);
 			}
-			
+
 			@Override
-			public void focusGained(FocusEvent e) {
+			public void focusGained(final FocusEvent e) {
 				setBorder(BORDER_FOCUSED);
 			}
 		});
 	}
 
 
-	public JNumericTextField(int columns) {
+	public JNumericTextField(final int columns) {
 		this(null, null, columns);
 	}
 
 
-	public JNumericTextField(String text, int columns) {
+	public JNumericTextField(final String text, final int columns) {
 		this(null, text, columns);
 	}
 
 
-	public JNumericTextField(String text) {
+	public JNumericTextField(final String text) {
 		this(null, text, 0);
 	}
-	
+
 	public JNumericTextField() {
 		this(null, null, 0);
 	}
-	
-	public JNumericTextField(double value) {
-		this(NumberFormat.getNumberInstance(Locale.US).format(value));
+
+	public JNumericTextField(final double value) {
+		this( "" + value );
 	}
-	
+
 	public double getValue() {
 		checkInput();
 		return value;
 	}
-	
+
 	/*
 	 * METHODS
 	 */
-	
+
 	private void checkInput() {
-		String str = getText();
+		final String str = getText();
 		try {
 			value = Double.parseDouble(str);
 			oldValue = value;
-		} catch (NumberFormatException nfe) {
+		} catch (final NumberFormatException nfe) {
 			value = oldValue;
-			setText(NumberFormat.getNumberInstance(Locale.US).format(value));
+			setText( "" + value );
 		}
-		
+
 	}
-	
-	
+
+
 }
