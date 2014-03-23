8ee40243d31ad9086d73e4486b8b96d7fef0af30
Christian Dietz <christian.dietz@uni-konstanz.de>
2014-03-23 15:12:23 +0100

    TrackBranchAnalyzer: Formatting

--- src/main/java/fiji/plugin/trackmate/features/track/TrackBranchingAnalyzer.java
+++ src/main/java/fiji/plugin/trackmate/features/track/TrackBranchingAnalyzer.java
@@ -1,5 +1,6 @@
 package fiji.plugin.trackmate.features.track;
 
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -18,14 +19,13 @@ import org.jgrapht.graph.DefaultWeightedEdge;
 import org.scijava.plugin.Plugin;
 
 import fiji.plugin.trackmate.Dimension;
-import fiji.plugin.trackmate.FeatureHolderUtils;
 import fiji.plugin.trackmate.Model;
 import fiji.plugin.trackmate.Spot;
-import fiji.plugin.trackmate.TrackmateConstants;
+import fiji.plugin.trackmate.tracking.TrackableObject;
+import fiji.plugin.trackmate.tracking.TrackableObjectUtils;
 
-@Plugin( type = TrackAnalyzer.class )
-public class TrackBranchingAnalyzer implements TrackAnalyzer, MultiThreaded
-{
+@Plugin(type = TrackAnalyzer.class)
+public class TrackBranchingAnalyzer implements TrackAnalyzer, MultiThreaded {
 
 	/*
 	 * CONSTANTS
@@ -42,134 +42,126 @@ public class TrackBranchingAnalyzer implements TrackAnalyzer, MultiThreaded
 
 	public static final String NUMBER_SPOTS = "NUMBER_SPOTS";
 
-	public static final List< String > FEATURES = new ArrayList< String >( 5 );
+	public static final List<String> FEATURES = new ArrayList<String>(5);
 
-	public static final Map< String, String > FEATURE_NAMES = new HashMap< String, String >( 5 );
+	public static final Map<String, String> FEATURE_NAMES = new HashMap<String, String>(
+			5);
 
-	public static final Map< String, String > FEATURE_SHORT_NAMES = new HashMap< String, String >( 5 );
+	public static final Map<String, String> FEATURE_SHORT_NAMES = new HashMap<String, String>(
+			5);
 
-	public static final Map< String, Dimension > FEATURE_DIMENSIONS = new HashMap< String, Dimension >( 5 );
+	public static final Map<String, Dimension> FEATURE_DIMENSIONS = new HashMap<String, Dimension>(
+			5);
 
-	public static final Map< String, Boolean > IS_INT = new HashMap< String, Boolean >( 5 );
+	public static final Map<String, Boolean> IS_INT = new HashMap<String, Boolean>(
+			5);
 
-	static
-	{
-		FEATURES.add( NUMBER_SPOTS );
-		FEATURES.add( NUMBER_GAPS );
-		FEATURES.add( NUMBER_SPLITS );
-		FEATURES.add( NUMBER_MERGES );
-		FEATURES.add( NUMBER_COMPLEX );
+	static {
+		FEATURES.add(NUMBER_SPOTS);
+		FEATURES.add(NUMBER_GAPS);
+		FEATURES.add(NUMBER_SPLITS);
+		FEATURES.add(NUMBER_MERGES);
+		FEATURES.add(NUMBER_COMPLEX);
 
-		FEATURE_NAMES.put( NUMBER_SPOTS, "Number of spots in track" );
-		FEATURE_NAMES.put( NUMBER_GAPS, "Number of gaps" );
-		FEATURE_NAMES.put( NUMBER_SPLITS, "Number of split events" );
-		FEATURE_NAMES.put( NUMBER_MERGES, "Number of merge events" );
-		FEATURE_NAMES.put( NUMBER_COMPLEX, "Complex points" );
+		FEATURE_NAMES.put(NUMBER_SPOTS, "Number of spots in track");
+		FEATURE_NAMES.put(NUMBER_GAPS, "Number of gaps");
+		FEATURE_NAMES.put(NUMBER_SPLITS, "Number of split events");
+		FEATURE_NAMES.put(NUMBER_MERGES, "Number of merge events");
+		FEATURE_NAMES.put(NUMBER_COMPLEX, "Complex points");
 
-		FEATURE_SHORT_NAMES.put( NUMBER_SPOTS, "N spots" );
-		FEATURE_SHORT_NAMES.put( NUMBER_GAPS, "Gaps" );
-		FEATURE_SHORT_NAMES.put( NUMBER_SPLITS, "Splits" );
-		FEATURE_SHORT_NAMES.put( NUMBER_MERGES, "Merges" );
-		FEATURE_SHORT_NAMES.put( NUMBER_COMPLEX, "Complex" );
+		FEATURE_SHORT_NAMES.put(NUMBER_SPOTS, "N spots");
+		FEATURE_SHORT_NAMES.put(NUMBER_GAPS, "Gaps");
+		FEATURE_SHORT_NAMES.put(NUMBER_SPLITS, "Splits");
+		FEATURE_SHORT_NAMES.put(NUMBER_MERGES, "Merges");
+		FEATURE_SHORT_NAMES.put(NUMBER_COMPLEX, "Complex");
 
-		FEATURE_DIMENSIONS.put( NUMBER_SPOTS, Dimension.NONE );
-		FEATURE_DIMENSIONS.put( NUMBER_GAPS, Dimension.NONE );
-		FEATURE_DIMENSIONS.put( NUMBER_SPLITS, Dimension.NONE );
-		FEATURE_DIMENSIONS.put( NUMBER_MERGES, Dimension.NONE );
-		FEATURE_DIMENSIONS.put( NUMBER_COMPLEX, Dimension.NONE );
+		FEATURE_DIMENSIONS.put(NUMBER_SPOTS, Dimension.NONE);
+		FEATURE_DIMENSIONS.put(NUMBER_GAPS, Dimension.NONE);
+		FEATURE_DIMENSIONS.put(NUMBER_SPLITS, Dimension.NONE);
+		FEATURE_DIMENSIONS.put(NUMBER_MERGES, Dimension.NONE);
+		FEATURE_DIMENSIONS.put(NUMBER_COMPLEX, Dimension.NONE);
 
-		IS_INT.put( NUMBER_SPOTS, Boolean.TRUE );
-		IS_INT.put( NUMBER_GAPS, Boolean.TRUE );
-		IS_INT.put( NUMBER_SPLITS, Boolean.TRUE );
-		IS_INT.put( NUMBER_MERGES, Boolean.TRUE );
-		IS_INT.put( NUMBER_COMPLEX, Boolean.TRUE );
+		IS_INT.put(NUMBER_SPOTS, Boolean.TRUE);
+		IS_INT.put(NUMBER_GAPS, Boolean.TRUE);
+		IS_INT.put(NUMBER_SPLITS, Boolean.TRUE);
+		IS_INT.put(NUMBER_MERGES, Boolean.TRUE);
+		IS_INT.put(NUMBER_COMPLEX, Boolean.TRUE);
 	}
 
 	private int numThreads;
 
 	private long processingTime;
 
-	public TrackBranchingAnalyzer()
-	{
+	public TrackBranchingAnalyzer() {
 		setNumThreads();
 	}
 
 	@Override
-	public boolean isLocal()
-	{
+	public boolean isLocal() {
 		return true;
 	}
 
 	@Override
-	public void process( final Collection< Integer > trackIDs, final Model model )
-	{
+	public void process(final Collection<Integer> trackIDs, final Model model) {
 
-		if ( trackIDs.isEmpty() ) { return; }
+		if (trackIDs.isEmpty()) {
+			return;
+		}
 
-		final ArrayBlockingQueue< Integer > queue = new ArrayBlockingQueue< Integer >( trackIDs.size(), false, trackIDs );
+		final ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(
+				trackIDs.size(), false, trackIDs);
 
-		final Thread[] threads = SimpleMultiThreading.newThreads( numThreads );
-		for ( int i = 0; i < threads.length; i++ )
-		{
-			threads[ i ] = new Thread( "TrackBranchingAnalyzer thread " + i )
-			{
+		final Thread[] threads = SimpleMultiThreading.newThreads(numThreads);
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread("TrackBranchingAnalyzer thread " + i) {
 				@Override
-				public void run()
-				{
+				public void run() {
 					Integer trackID;
-					while ( ( trackID = queue.poll() ) != null )
-					{
+					while ((trackID = queue.poll()) != null) {
 
-						final Set< Spot > track = model.getTrackModel().trackSpots( trackID );
+						final Set<Spot> track = model.getTrackModel()
+								.trackSpots(trackID);
 
 						int nmerges = 0;
 						int nsplits = 0;
 						int ncomplex = 0;
-						for ( final Spot spot : track )
-						{
-							final Set< DefaultWeightedEdge > edges = model.getTrackModel().edgesOf( spot );
+						for (final Spot spot : track) {
+							final Set<DefaultWeightedEdge> edges = model
+									.getTrackModel().edgesOf(spot);
 
 							// get neighbors
-							final Set< Spot > neighbors = new HashSet< Spot >();
-							for ( final DefaultWeightedEdge edge : edges )
-							{
-								neighbors.add( model.getTrackModel().getEdgeSource( edge ) );
-								neighbors.add( model.getTrackModel().getEdgeTarget( edge ) );
+							final Set<TrackableObject> neighbors = new HashSet<TrackableObject>();
+							for (final DefaultWeightedEdge edge : edges) {
+								neighbors.add(model.getTrackModel()
+										.getEdgeSource(edge));
+								neighbors.add(model.getTrackModel()
+										.getEdgeTarget(edge));
 							}
-							neighbors.remove( spot );
+							neighbors.remove(spot);
 
 							// inspect neighbors relative time position
 							int earlier = 0;
 							int later = 0;
-							for ( final Spot neighbor : neighbors )
-							{
-								if ( FeatureHolderUtils.diffTo(spot, neighbor, TrackmateConstants.FRAME ) > 0 )
-								{
+							for (final TrackableObject neighbor : neighbors) {
+								if (TrackableObjectUtils.frameDiff(spot,
+										neighbor) > 0) {
 									earlier++; // neighbor is before in time
-								}
-								else
-								{
+								} else {
 									later++;
 								}
 							}
 
 							// Test for classical spot
-							if ( earlier == 1 && later == 1 )
-							{
+							if (earlier == 1 && later == 1) {
 								continue;
 							}
 
 							// classify spot
-							if ( earlier <= 1 && later > 1 )
-							{
+							if (earlier <= 1 && later > 1) {
 								nsplits++;
-							}
-							else if ( later <= 1 && earlier > 1 )
-							{
+							} else if (later <= 1 && earlier > 1) {
 								nmerges++;
-							}
-							else if ( later > 1 && earlier > 1 )
-							{
+							} else if (later > 1 && earlier > 1) {
 								ncomplex++;
 							}
 						}
@@ -183,22 +175,21 @@ public class TrackBranchingAnalyzer implements TrackAnalyzer, MultiThreaded
 									.getEdgeTarget(edge);
 							if (Math.abs(TrackableObjectUtils.frameDiff(target,
 									source)) > 1) {
-						for ( final DefaultWeightedEdge edge : model.getTrackModel().trackEdges( trackID ) )
-						{
-							final Spot source = model.getTrackModel().getEdgeSource( edge );
-							final Spot target = model.getTrackModel().getEdgeTarget( edge );
-							if ( Math.abs( FeatureHolderUtils.diffTo(target, source, TrackmateConstants.FRAME ) ) > 1 )
-							{
 								ngaps++;
 							}
 						}
 
 						// Put feature data
-						model.getFeatureModel().putTrackFeature( trackID, NUMBER_GAPS, Double.valueOf( ngaps ) );
-						model.getFeatureModel().putTrackFeature( trackID, NUMBER_SPLITS, Double.valueOf( nsplits ) );
-						model.getFeatureModel().putTrackFeature( trackID, NUMBER_MERGES, Double.valueOf( nmerges ) );
-						model.getFeatureModel().putTrackFeature( trackID, NUMBER_COMPLEX, Double.valueOf( ncomplex ) );
-						model.getFeatureModel().putTrackFeature( trackID, NUMBER_SPOTS, Double.valueOf( track.size() ) );
+						model.getFeatureModel().putTrackFeature(trackID,
+								NUMBER_GAPS, Double.valueOf(ngaps));
+						model.getFeatureModel().putTrackFeature(trackID,
+								NUMBER_SPLITS, Double.valueOf(nsplits));
+						model.getFeatureModel().putTrackFeature(trackID,
+								NUMBER_MERGES, Double.valueOf(nmerges));
+						model.getFeatureModel().putTrackFeature(trackID,
+								NUMBER_COMPLEX, Double.valueOf(ncomplex));
+						model.getFeatureModel().putTrackFeature(trackID,
+								NUMBER_SPOTS, Double.valueOf(track.size()));
 
 					}
 
@@ -207,93 +198,79 @@ public class TrackBranchingAnalyzer implements TrackAnalyzer, MultiThreaded
 		}
 
 		final long start = System.currentTimeMillis();
-		SimpleMultiThreading.startAndJoin( threads );
+		SimpleMultiThreading.startAndJoin(threads);
 		final long end = System.currentTimeMillis();
 		processingTime = end - start;
 	}
 
 	@Override
-	public int getNumThreads()
-	{
+	public int getNumThreads() {
 		return numThreads;
 	}
 
 	@Override
-	public void setNumThreads()
-	{
+	public void setNumThreads() {
 		this.numThreads = Runtime.getRuntime().availableProcessors();
 	}
 
 	@Override
-	public void setNumThreads( final int numThreads )
-	{
+	public void setNumThreads(final int numThreads) {
 		this.numThreads = numThreads;
 
 	}
 
 	@Override
-	public long getProcessingTime()
-	{
+	public long getProcessingTime() {
 		return processingTime;
 	}
 
 	@Override
-	public List< String > getFeatures()
-	{
+	public List<String> getFeatures() {
 		return FEATURES;
 	}
 
 	@Override
-	public Map< String, String > getFeatureShortNames()
-	{
+	public Map<String, String> getFeatureShortNames() {
 		return FEATURE_SHORT_NAMES;
 	}
 
 	@Override
-	public Map< String, String > getFeatureNames()
-	{
+	public Map<String, String> getFeatureNames() {
 		return FEATURE_NAMES;
 	}
 
 	@Override
-	public Map< String, Dimension > getFeatureDimensions()
-	{
+	public Map<String, Dimension> getFeatureDimensions() {
 		return FEATURE_DIMENSIONS;
 	}
 
 	@Override
-	public String getKey()
-	{
+	public String getKey() {
 		return KEY;
 	}
 
 	@Override
-	public String getInfoText()
-	{
+	public String getInfoText() {
 		return null;
 	}
 
 	@Override
-	public ImageIcon getIcon()
-	{
+	public ImageIcon getIcon() {
 		return null;
 	}
 
 	@Override
-	public String getName()
-	{
+	public String getName() {
 		return KEY;
 	}
 
 	@Override
-	public Map< String, Boolean > getIsIntFeature()
-	{
+	public Map<String, Boolean> getIsIntFeature() {
 		return IS_INT;
 	}
 
 	@Override
-	public boolean isManualFeature()
-	{
+	public boolean isManualFeature() {
 		return false;
 	}
 
