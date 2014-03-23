f02a56132542b236983bed8135c201dc6a4872d1
Christian Dietz <christian.dietz@uni-konstanz.de>
2014-03-23 13:57:13 +0100

    Added FastLapTracker
    - bases on CostCalculator
    - using generics

--- src/main/java/fiji/plugin/trackmate/tracking/FastLAPTracker.java
+++ src/main/java/fiji/plugin/trackmate/tracking/FastLAPTracker.java
@@ -2,15 +2,15 @@ package fiji.plugin.trackmate.tracking;
 
 import java.util.Map;
 
+import fiji.plugin.trackmate.tracking.costfunction.CostCalculator;
 import fiji.plugin.trackmate.tracking.hungarian.AssignmentAlgorithm;
 import fiji.plugin.trackmate.tracking.hungarian.JonkerVolgenantAlgorithm;
-import fiji.plugin.trackmate.tracking.spot.SpotCollection;
 
-public class FastLAPTracker extends LAPTracker {
+public class FastLAPTracker<T extends TrackableObject> extends LAPTracker<T> {
 
-	public FastLAPTracker( final SpotCollection spots, final Map< String, Object > settings )
+	public FastLAPTracker( final CostCalculator<T> costCalculator, final TrackableObjectCollection<T> spots, final Map< String, Object > settings )
 	{
-		super( spots, settings );
+		super( costCalculator, spots, settings );
 	}
 
 	@Override
