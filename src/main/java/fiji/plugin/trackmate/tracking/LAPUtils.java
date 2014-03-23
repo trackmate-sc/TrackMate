a57823f19a1799040e216e591145ec7f5dd8ff16
Christian Dietz <christian.dietz@uni-konstanz.de>
2014-03-23 13:58:17 +0100

    LAPUtils: Removed deprecated utility function in LAPUtils
    - It's implemented in the CostCalculator now

--- src/main/java/fiji/plugin/trackmate/tracking/LAPUtils.java
+++ src/main/java/fiji/plugin/trackmate/tracking/LAPUtils.java
@@ -54,8 +54,6 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableModel;
 
-import fiji.plugin.trackmate.Spot;
-
 public class LAPUtils {
 
 	private static final Border RED_BORDER = new LineBorder(Color.RED);
@@ -153,54 +151,6 @@ public class LAPUtils {
 		return str;
 
 	}
-
-	/**
-	 * Compute the cost to link two spots, in the default way for the TrackMate trackmate.
-	 * <p>
-	 * This cost is calculated as follow:
-	 * <ul>
-	 * 	<li> The distance between the two spots <code>D</code> is calculated
-	 * 	<li> If the spots are separated by more than the distance cutoff, the cost is
-	 * set to be the blocking value. If not,
-	 * 	<li> For each feature in the map, a penalty <code>p</code> is calculated as
-	 * <code>p = 3 × α × |f1-f2| / (f1+f2)</code>, where <code>α</code> is the factor
-	 * associated to the feature in the map. This expression is such that:
-	 * 	<ul>
-	 * 	<li> there is no penalty if the 2 feature values <code>f1</code> and <code>f2</code>
-	 * are the same;
-	 * 	<li> that, with a factor of 1, the penalty if 1 is one value is the double of the other;
-	 * 	<li>the penalty is 2 if one is 5 times the other one.
-	 * 	</ul>
-	 * 	<li> All penalties are summed, to form <code>P = (1 + ∑ p )</code>
-	 *  <li> The cost is set to the square of the product: <code>C = ( D × P )²</code>
-	 * </ul>
-	 * For instance: if 2 spots differ by twice the value in a feature which is
-	 * in the penalty map with a factor of 1, they will <i>look</i> as if they were
-	 * twice as far.
-	 */
-	public static final double computeLinkingCostFor(final Spot s0, final Spot s1,
-			final double distanceCutOff, final double blockingValue, final Map<String, Double> featurePenalties) {
-		final double d2 = s0.squareDistanceTo(s1);
-
-		// Distance threshold
-		if (d2 > distanceCutOff * distanceCutOff) {
-			return blockingValue;
-		}
-
-		double penalty = 1;
-		for (final String feature : featurePenalties.keySet()) {
-			final double ndiff = s0.normalizeDiffTo(s1, feature);
-			if (Double.isNaN(ndiff))
-				continue;
-			final double factor = featurePenalties.get(feature);
-			penalty += factor * 1.5 * ndiff;
-		}
-
-		// Set score
-		return d2 * penalty * penalty;
-	}
-
-
 
 	/**
 	 * @return true if the settings map can be used with the LAP trackers. We do not check that all the spot features
