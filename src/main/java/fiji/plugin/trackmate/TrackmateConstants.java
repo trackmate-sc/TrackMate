6bedeb449c9864284228d693ec411e1937b1d2d1
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-03-25 15:12:12 +0100

    Declare VISIBILITY as a spot feature.
    Otherwise saving does not work: the visbility feature is needed and must
    be marshalled like any other feature.
    
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/TrackmateConstants.java
+++ src/main/java/fiji/plugin/trackmate/TrackmateConstants.java
@@ -9,11 +9,11 @@ import java.util.Map;
 
 /**
  * Utility class for constants of {@link FeatureHolder}s
- * 
+ *
  * @author Christian Dietz (University of Konstanz)
  */
 public final class TrackmateConstants {
-	
+
 	/** numeric constants */
 	public static Double ZERO = Double.valueOf( 0d );
 
@@ -28,7 +28,7 @@ public final class TrackmateConstants {
 
 	/** The name of the frame feature. */
 	public static final String QUALITY = "QUALITY";
-	
+
 	/** The name of the frame feature. */
 	public static final String VISIBILITY = "VISIBILITY";
 
@@ -53,46 +53,50 @@ public final class TrackmateConstants {
 	public static Map< String, String > FEATURE_SHORT_NAMES = new HashMap<String, String>();
 
 	public static Map< String, Dimension > FEATURE_DIMENSIONS = new HashMap<String, Dimension>();
-	
+
 	public final static Collection< String > FEATURES = new ArrayList< String >( 7 );
-	
+
 	/** The 7 privileged spot feature isInt flags. */
 	public final static Map< String, Boolean > IS_INT = new HashMap< String, Boolean >( 7 );
-	
+
 	static
 	{
-		FEATURES.add( TrackmateConstants.QUALITY );
-		FEATURES.add( TrackmateConstants.POSITION_X );
-		FEATURES.add( TrackmateConstants.POSITION_Y );
-		FEATURES.add( TrackmateConstants.POSITION_Z );
-		FEATURES.add( TrackmateConstants.POSITION_T );
-		FEATURES.add( TrackmateConstants.FRAME );
-		FEATURES.add( TrackmateConstants.RADIUS );
+		FEATURES.add( QUALITY );
+		FEATURES.add( POSITION_X );
+		FEATURES.add( POSITION_Y );
+		FEATURES.add( POSITION_Z );
+		FEATURES.add( POSITION_T );
+		FEATURES.add( FRAME );
+		FEATURES.add( RADIUS );
+		FEATURES.add( VISIBILITY );
 
-		FEATURE_NAMES.put( TrackmateConstants.POSITION_X, "X" );
-		FEATURE_NAMES.put( TrackmateConstants.POSITION_Y, "Y" );
-		FEATURE_NAMES.put( TrackmateConstants.POSITION_Z, "Z" );
-		FEATURE_NAMES.put( TrackmateConstants.POSITION_T, "T" );
-		FEATURE_NAMES.put( TrackmateConstants.FRAME, "Frame" );
-		FEATURE_NAMES.put( TrackmateConstants.RADIUS, "Radius" );
-		FEATURE_NAMES.put( TrackmateConstants.QUALITY, "Quality" );
+		FEATURE_NAMES.put( POSITION_X, "X" );
+		FEATURE_NAMES.put( POSITION_Y, "Y" );
+		FEATURE_NAMES.put( POSITION_Z, "Z" );
+		FEATURE_NAMES.put( POSITION_T, "T" );
+		FEATURE_NAMES.put( FRAME, "Frame" );
+		FEATURE_NAMES.put( RADIUS, "Radius" );
+		FEATURE_NAMES.put( QUALITY, "Quality" );
+		FEATURE_NAMES.put( VISIBILITY, "Visibility" );
 
-		FEATURE_SHORT_NAMES.put( TrackmateConstants.POSITION_X, "X" );
-		FEATURE_SHORT_NAMES.put( TrackmateConstants.POSITION_Y, "Y" );
-		FEATURE_SHORT_NAMES.put( TrackmateConstants.POSITION_Z, "Z" );
-		FEATURE_SHORT_NAMES.put( TrackmateConstants.POSITION_T, "T" );
-		FEATURE_SHORT_NAMES.put( TrackmateConstants.FRAME, "Frame" );
-		FEATURE_SHORT_NAMES.put( TrackmateConstants.RADIUS, "R" );
-		FEATURE_SHORT_NAMES.put( TrackmateConstants.QUALITY, "Quality" );
+		FEATURE_SHORT_NAMES.put( POSITION_X, "X" );
+		FEATURE_SHORT_NAMES.put( POSITION_Y, "Y" );
+		FEATURE_SHORT_NAMES.put( POSITION_Z, "Z" );
+		FEATURE_SHORT_NAMES.put( POSITION_T, "T" );
+		FEATURE_SHORT_NAMES.put( FRAME, "Frame" );
+		FEATURE_SHORT_NAMES.put( RADIUS, "R" );
+		FEATURE_SHORT_NAMES.put( QUALITY, "Quality" );
+		FEATURE_SHORT_NAMES.put( VISIBILITY, "Visibility" );
 
-		FEATURE_DIMENSIONS.put( TrackmateConstants.POSITION_X, Dimension.POSITION );
-		FEATURE_DIMENSIONS.put( TrackmateConstants.POSITION_Y, Dimension.POSITION );
-		FEATURE_DIMENSIONS.put( TrackmateConstants.POSITION_Z, Dimension.POSITION );
-		FEATURE_DIMENSIONS.put( TrackmateConstants.POSITION_T, Dimension.TIME );
-		FEATURE_DIMENSIONS.put( TrackmateConstants.FRAME, Dimension.NONE );
-		FEATURE_DIMENSIONS.put( TrackmateConstants.RADIUS, Dimension.LENGTH );
-		FEATURE_DIMENSIONS.put( TrackmateConstants.QUALITY, Dimension.QUALITY );
-		
+		FEATURE_DIMENSIONS.put( POSITION_X, Dimension.POSITION );
+		FEATURE_DIMENSIONS.put( POSITION_Y, Dimension.POSITION );
+		FEATURE_DIMENSIONS.put( POSITION_Z, Dimension.POSITION );
+		FEATURE_DIMENSIONS.put( POSITION_T, Dimension.TIME );
+		FEATURE_DIMENSIONS.put( FRAME, Dimension.NONE );
+		FEATURE_DIMENSIONS.put( RADIUS, Dimension.LENGTH );
+		FEATURE_DIMENSIONS.put( QUALITY, Dimension.QUALITY );
+		FEATURE_DIMENSIONS.put( VISIBILITY, Dimension.NONE );
+
 		IS_INT.put( POSITION_X, Boolean.FALSE );
 		IS_INT.put( POSITION_Y, Boolean.FALSE );
 		IS_INT.put( POSITION_Z, Boolean.FALSE );
@@ -100,7 +104,7 @@ public final class TrackmateConstants {
 		IS_INT.put( FRAME, Boolean.TRUE );
 		IS_INT.put( RADIUS, Boolean.FALSE );
 		IS_INT.put( QUALITY, Boolean.FALSE );
-		IS_INT.put( TrackmateConstants.VISIBILITY, Boolean.TRUE );
+		IS_INT.put( VISIBILITY, Boolean.TRUE );
 	}
 
 }
