c23bd14bb9addf2f1a3cd408d7abc28333a57815
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-06-19 09:28:42 +0200

    Code style changes.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/action/CopyOverlayAction.java
+++ src/main/java/fiji/plugin/trackmate/action/CopyOverlayAction.java
@@ -21,69 +21,77 @@ import fiji.plugin.trackmate.visualization.TrackMateModelView;
 import fiji.plugin.trackmate.visualization.hyperstack.HyperStackDisplayer;
 import fiji.plugin.trackmate.visualization.threedviewer.SpotDisplayer3D;
 
-public class CopyOverlayAction extends AbstractTMAction {
+public class CopyOverlayAction extends AbstractTMAction
+{
 
-	public static final ImageIcon ICON = new ImageIcon(TrackMateWizard.class.getResource("images/page_copy.png"));
+	public static final ImageIcon ICON = new ImageIcon( TrackMateWizard.class.getResource( "images/page_copy.png" ) );
+
 	public static final String NAME = "Copy overlay to...";
 
 	public static final String KEY = "COPY_OVERLAY";
-	public static final String INFO_TEXT = "<html>" +
-			"This action copies the overlay (spots and tracks) to a new existing ImageJ window <br> " +
-			"or to a new 3D viewer window. This can be useful to have the tracks and spots <br> " +
-			"displayed on a modified image. "
-			+ "<p>"
-			+ "The new view will be independent, and will have its own control panel.<br> " +
-			"</html>" ;
+
+	public static final String INFO_TEXT = "<html>" + "This action copies the overlay (spots and tracks) to a new existing ImageJ window <br> " + "or to a new 3D viewer window. This can be useful to have the tracks and spots <br> " + "displayed on a modified image. " + "<p>" + "The new view will be independent, and will have its own control panel.<br> " + "</html>";
 
 	@Override
-	public void execute(final TrackMate trackmate) {
+	public void execute( final TrackMate trackmate )
+	{
 		final ImagePlusChooser impChooser = new ImagePlusChooser( "Copy overlay", "Copy overlay to:", "New 3D viewer" );
-		impChooser.setLocationRelativeTo(null);
-		impChooser.setVisible(true);
-		final ActionListener copyOverlayListener = new ActionListener() {
+		impChooser.setLocationRelativeTo( null );
+		impChooser.setVisible( true );
+		final ActionListener copyOverlayListener = new ActionListener()
+		{
 			@Override
-			public void actionPerformed(final ActionEvent e) {
-				if (e == impChooser.OK_BUTTON_PUSHED) {
-					new Thread("TrackMate copying thread") {
+			public void actionPerformed( final ActionEvent e )
+			{
+				if ( e == impChooser.OK_BUTTON_PUSHED )
+				{
+					new Thread( "TrackMate copying thread" )
+					{
 						@Override
-						public void run() {
-							final SelectionModel selectionModel = new SelectionModel(trackmate.getModel());
+						public void run()
+						{
+							final SelectionModel selectionModel = new SelectionModel( trackmate.getModel() );
 							// Instantiate displayer
 							final ImagePlus dest = impChooser.getSelectedImagePlus();
-							impChooser.setVisible(false);
+							impChooser.setVisible( false );
 							TrackMateModelView newDisplayer;
 							String title;
-							if (null == dest) {
-								logger.log("Copying data and overlay to new 3D viewer\n");
+							if ( null == dest )
+							{
+								logger.log( "Copying data and overlay to new 3D viewer\n" );
 								final Image3DUniverse universe = new Image3DUniverse();
-								newDisplayer = new SpotDisplayer3D(trackmate.getModel(), selectionModel, universe);
+								newDisplayer = new SpotDisplayer3D( trackmate.getModel(), selectionModel, universe );
 								title = "3D viewer overlay";
-							} else {
-								logger.log("Copying overlay to "+dest.getShortTitle()+"\n");
-								newDisplayer = new HyperStackDisplayer(trackmate.getModel(), selectionModel , dest);
+							}
+							else
+							{
+								logger.log( "Copying overlay to " + dest.getShortTitle() + "\n" );
+								newDisplayer = new HyperStackDisplayer( trackmate.getModel(), selectionModel, dest );
 								title = dest.getShortTitle() + " ctrl";
 							}
 							newDisplayer.render();
 
-							final ConfigureViewsPanel newDisplayerPanel = new ConfigureViewsPanel(trackmate.getModel());
+							final ConfigureViewsPanel newDisplayerPanel = new ConfigureViewsPanel( trackmate.getModel() );
 							final JFrame newFrame = new JFrame();
-							newFrame.getContentPane().add(newDisplayerPanel);
+							newFrame.getContentPane().add( newDisplayerPanel );
 							newFrame.pack();
-							newFrame.setTitle(title);
-							newFrame.setSize(300, 470);
-							newFrame.setLocationRelativeTo(null);
-							newFrame.setVisible(true);
-							logger.log("Done.\n");
+							newFrame.setTitle( title );
+							newFrame.setSize( 300, 470 );
+							newFrame.setLocationRelativeTo( null );
+							newFrame.setVisible( true );
+							logger.log( "Done.\n" );
 
 						}
 					}.start();
-				} else {
-					impChooser.removeActionListener(this);
-					impChooser.setVisible(false);
+				}
+				else
+				{
+					impChooser.removeActionListener( this );
+					impChooser.setVisible( false );
 				}
 			}
 		};
-		impChooser.addActionListener(copyOverlayListener);
+		impChooser.addActionListener( copyOverlayListener );
 	}
 
 	@Plugin( type = TrackMateActionFactory.class )
