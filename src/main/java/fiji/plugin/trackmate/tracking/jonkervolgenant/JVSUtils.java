2d3931769ddb011b7ff35883bf54841a2f33f1e2
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-07-10 17:17:54 +0200

    Sparse JV solver - WIP
    First failed attempt.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

---
+++ src/main/java/fiji/plugin/trackmate/tracking/jonkervolgenant/JVSUtils.java
@@ -0,0 +1,152 @@
+package fiji.plugin.trackmate.tracking.jonkervolgenant;
+
+import java.util.Arrays;
+
+/**
+ * Static utilities for the sparse Konker-Volgenant solver.
+ * 
+ * @author Jean-Yves Tinevez - 2014
+ */
+public class JVSUtils
+{
+	/*
+	 * UTIL METHODS
+	 */
+	
+
+	/**
+	 * Returns a 3-element array of <code>int</code> arrays:
+	 * <ol start="0">
+	 * <li>the array of unique values in the specified array, sorted by
+	 * ascending order.</li>
+	 * <li>the count of each of these unique values in the specified array.</li>
+	 * <li>the index in the unique value array of each value in the specified
+	 * array.</li>
+	 * </ol>
+	 * 
+	 * @return a new <code>int[][]</code>.
+	 */
+	public static final int[][] bincount( final int[] vals )
+	{
+		final int[] unique = unique( vals );
+		final int[] index = indexIn( vals, unique );
+		final int[] bc = new int[ unique.length ];
+		for ( int i = 0; i < vals.length; i++ )
+		{
+			bc[ index[ i ] ]++;
+		}
+		return new int[][] { unique, bc, index };
+	}
+
+	public static final String bincountToString( final int[][] bc )
+	{
+		final int[] unique = bc[ 0 ];
+		final int[] count = bc[ 1 ];
+		final int[] index = bc[ 2 ];
+		final StringBuilder str = new StringBuilder();
+		str.append( "Array with " + index.length + " elements.\n" );
+		str.append( "\tunique: " );
+		for ( final int i : unique )
+		{
+			str.append( "\t" + i );
+		}
+		str.append( '\n' );
+		str.append( "\tcount\t:" );
+		for ( final int i : count )
+		{
+			str.append( "\t" + i );
+		}
+		str.append( '\n' );
+		return str.toString();
+	}
+
+	/**
+	 * Returns the sorted array of unique values of the specified array.
+	 * 
+	 * @param vals
+	 *            the array to inspect.
+	 * @return a new <code>int[]</code> array containing the unique values,
+	 *         sorted by ascending order.
+	 */
+	public static final int[] unique( final int[] vals )
+	{
+		final int[] result = vals.clone();
+		Arrays.sort( result );
+		int j = 0;
+		for ( int i = 1; i < vals.length; i++ )
+		{
+			if ( result[ j ] != result[ i ] )
+			{
+				result[ ++j ] = result[ i ];
+			}
+		}
+		return Arrays.copyOf( result, ++j );
+	}
+
+	/**
+	 * Return the index of each value in the first array in the second one. The
+	 * second array <b>must be sorted</b>.
+	 * 
+	 * @param vals
+	 *            the array to index.
+	 * @param catalog
+	 *            the catalog array, must be sorted.
+	 * @return the index array, that is, <code>index[i]</code> contains the
+	 *         index in the catalog of the value <code>val[i]</code>;
+	 */
+	public static final int[] indexIn( final int[] vals, final int[] catalog )
+	{
+		final int[] index = new int[ vals.length ];
+		for ( int i = 0; i < vals.length; i++ )
+		{
+			final int val = vals[ i ];
+			final int p = Arrays.binarySearch( catalog, val );
+			index[ i ] = p;
+		}
+		return index;
+	}
+
+	public final static String resultToString( final int[][] result )
+	{
+		final StringBuilder str = new StringBuilder();
+		final int[] iUnique = result[ 0 ];
+		final int[] x = result[ 1 ];
+		final int[] jUnique = result[ 2 ];
+		final int[] y = result[ 3 ];
+
+		str.append( "SOURCE\t→\tTARGET\n" );
+		for ( int k = 0; k < x.length; k++ )
+		{
+			String targetStr;
+			if ( x[ k ] >= jUnique.length )
+			{
+				targetStr = "unsgnd";
+			}
+			else
+			{
+				targetStr = "" + jUnique[ x[ k ] ];
+			}
+			final int source = iUnique[ k ];
+			str.append( "" + source + "\t→\t" + targetStr + "\n" );
+		}
+
+		str.append( "SOURCE\t←\tTARGET\n" );
+		for ( int l = 0; l < y.length; l++ )
+		{
+			String sourceStr;
+			if ( y[ l ] >= iUnique.length )
+			{
+				sourceStr = "unsgnd";
+			}
+			else
+			{
+				sourceStr = "" + iUnique[ y[ l ] ];
+			}
+			final int target = jUnique[ l ];
+			str.append( sourceStr + "\t←\t" + target + "\n" );
+		}
+
+		return str.toString();
+	}
+
+}
--- src/main/java/fiji/plugin/trackmate/tracking/jonkervolgenant/JonkerVolgenantSolver.java
+++ src/main/java/fiji/plugin/trackmate/tracking/jonkervolgenant/JonkerVolgenantSolver.java
@@ -1,6 +1,11 @@
 package fiji.plugin.trackmate.tracking.jonkervolgenant;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 import net.imglib2.algorithm.OutputAlgorithm;
+import net.imglib2.util.Util;
 
 /**
  * Ported from Lee code.
@@ -27,6 +32,8 @@ public class JonkerVolgenantSolver implements OutputAlgorithm< int[][] >
 	 * <code>j[n]</code>.
 	 */
 	private final double[] c;
+
+	private int[][] result;
 
 	private String errorMessage;
 
@@ -64,19 +71,173 @@ public class JonkerVolgenantSolver implements OutputAlgorithm< int[][] >
 	@Override
 	public boolean process()
 	{
-		// Check all targets have at least a source
-		final int[] jcount = bincount( j );
-		for ( final int jc : jcount )
+		/*
+		 * Order and test source indices.
+		 */
+
+		final int[][] jbc = JVSUtils.bincount( j );
+		final int[] jUnique = jbc[ 0 ];
+		final int[] jCount = jbc[ 1 ];
+		final int[] jIndex = jbc[ 2 ];
+		System.out.println( JVSUtils.bincountToString( jbc ) );
+
+		for ( final int jc : jCount )
 		{
 			if ( jc == 0 )
 			{
-				errorMessage = BASE_ERROR_MESSAGE + "Target object with index " + jc + " does not have a source.";
+				errorMessage = BASE_ERROR_MESSAGE + "All target objects must have at least one cost linking to a source object.";
 				return false;
 			}
 		}
 
-		// TODO Auto-generated method stub
-		return false;
+		final int[][] ibc = JVSUtils.bincount( i );
+		final int[] iUnique = ibc[ 0 ];
+		final int[] iCount = ibc[ 1 ];
+		final int[] iIndex = ibc[ 2 ];
+		System.out.println( JVSUtils.bincountToString( ibc ) );
+
+		for ( final int ic : iCount )
+		{
+			if ( ic == 0 )
+			{
+				errorMessage = BASE_ERROR_MESSAGE + "All source objects must have at least one cost linking to a target object.";
+				return false;
+			}
+		}
+
+		/*
+		 * Prepare outputs
+		 */
+
+		// For each i, the assigned j
+		final int[] x = new int[ iUnique.length ];
+		Arrays.fill( x, jUnique.length ); // Means: not assigned.
+
+		// For each j, the assigned i
+		final int[] y = new int[ jUnique.length ];
+		Arrays.fill( y, iUnique.length ); // Means: not assigned.
+
+		/*
+		 * COLUMN REDUCTION.
+		 */
+
+		/*
+		 * For a given j, find the i with the minimum cost. We iterate in
+		 * reverse order, as suggested in the paper.
+		 */
+
+		final double[] v = new double[ jUnique.length ];
+		// How manu matches have we found for i?
+		final int[] iMatchCount = new int[ iUnique.length ];
+
+		for ( int kju = jUnique.length - 1; kju > -1; kju-- )
+		{
+			/*
+			 * We focus on the value ju, and look where it is in the index. It
+			 * can be found by iterating the index, and searching for index
+			 * values equal to kju. Then at this index, but in the cost vector
+			 * we have the cost that maps this ju to a i.
+			 */
+			double minCostJtoI = Double.POSITIVE_INFINITY;
+			int minCostJtoIindex = -1;
+			for ( int kindex = 0; kindex < jIndex.length; kindex++ )
+			{
+				if ( jIndex[ kindex ] == kju )
+				{
+					if (c[kindex] < minCostJtoI) {
+						minCostJtoI = c[ kindex ];
+						minCostJtoIindex = kindex;
+					}
+				}
+			}
+
+
+			if ( minCostJtoIindex >= 0 )
+			{
+				// Found.
+				final int targetI = i[ minCostJtoIindex ];
+				System.out.println( "For column " + kju + ", found a min cost = " + minCostJtoI + " at row " + targetI ); // DEBUG
+				System.out.println( "V = " + Util.printCoordinates( v ) ); // DEBUG
+				final int kiu = Arrays.binarySearch( iUnique, targetI );
+
+				if ( iMatchCount[ kiu ] == 0 )
+				{
+					v[ kju ] = minCostJtoI;
+					y[ kju ] = kiu;
+					x[ kiu ] = kju;
+				}
+				else if ( minCostJtoI < v[ x[ kiu ] ] )
+				{
+					v[ kju ] = minCostJtoI;
+					final int temp = x[ kiu ];
+					y[ kju ] = kiu;
+					x[ kiu ] = kju;
+					y[ temp ] = jUnique.length; // erase
+				}
+				else
+				{
+					y[ kju ] = jUnique.length;
+				}
+				iMatchCount[ kiu ]++;
+			}
+			else
+			{
+				/*
+				 * Should not happen at this stage, since we ensured that there
+				 * is a I for any J. But hey, things can go wrong.
+				 */
+				final int ju = jUnique[ kju ];
+				errorMessage = BASE_ERROR_MESSAGE + "Could not find a single matching source for the target " + ju + ". All target objects must have at least one cost linking to a source object.";
+				return false;
+			}
+
+			result = new int[][] { iUnique, x, jUnique, y };
+			System.out.println( Util.printCoordinates( iMatchCount ) ); // DEBUG
+			System.out.println( JVSUtils.resultToString( result ) ); // DEBUG
+			System.out.println(); // DEBUG
+
+		}
+
+		/*
+		 * REDUCTION TRANSFER
+		 */
+
+		final Collection< Integer > freeRows = new ArrayList< Integer >();
+
+		for ( int kiu = 0; kiu < iMatchCount.length; kiu++ )
+		{
+			if ( iMatchCount[ kiu ] == 0 )
+			{
+				freeRows.add( Integer.valueOf( kiu ) );
+			}
+			else if ( iMatchCount[ kiu ] == 1 )
+			{
+				final int j1 = x[ kiu ];
+				double minU = Double.POSITIVE_INFINITY;
+				for ( int kju = 0; kju < jUnique.length; kju++ )
+				{
+					if ( kju == j1 )
+					{
+						continue;
+					}
+					// Search for the cost
+
+					if ( minU > -v[ kju ] )
+					{
+						minU = -v[ kju ];
+					}
+				}
+			}
+
+
+		}
+
+		/*
+		 * TERMINATE
+		 */
+		result = new int[][] { iUnique, x, jUnique, y };
+
+		return true;
 	}
 
 	@Override
@@ -88,35 +249,115 @@ public class JonkerVolgenantSolver implements OutputAlgorithm< int[][] >
 	@Override
 	public int[][] getResult()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return result;
 	}
+
 
 	/*
-	 * UTIL METHODS
+	 * MAIN METHOD
 	 */
 
-	/**
-	 * Returns the number of occurrences of each value in array of non-negative
-	 * ints.
-	 */
-	private static final int[] bincount( final int[] vals )
+	public static void main( final String[] args )
 	{
-		// Find the max in i.
-		int max = vals[ 0 ];
-		for ( int k = 1; k < vals.length; k++ )
-		{
-			if ( vals[ k ] > max )
-			{
-				max = vals[ k ];
-			}
-		}
+//		final int[] i = new int[] { 0, 0, 1, 1, 2, 2 };
+//		final int[] j = new int[] { 0, 1, 1, 2, 2, 3 };
+//		final double[] c = new double[] { 21, 12, 43, 64, 25, 36 };
 
-		final int[] bc = new int[ max ];
-		for ( int k = 0; k < vals.length; k++ )
+		final int[] i = new int[] {
+			     0,
+			     0,
+			     0,
+			     0,
+			     0,
+			     1,
+			     1,
+			     1,
+			     1,
+			     1,
+			     2,
+			     2,
+			     2,
+			     2,
+			     2,
+			     3,
+			     3,
+			     3,
+			     3,
+			     3,
+			     4,
+			     4,
+			     4,
+			     4,
+			     4
+		};
+		final int[] j = new int[] {
+				0,
+				1,
+				2,
+				3,
+				4,
+				0,
+				1,
+				2,
+				3,
+				4,
+				0,
+				1,
+				2,
+				3,
+				4,
+				0,
+				1,
+				2,
+				3,
+				4,
+				0,
+				1,
+				2,
+				3,
+				4
+		};
+
+		final double[] c = new double[] {
+				17,
+				23,
+				4,
+				10,
+				11,
+				24,
+				5,
+				6,
+				12,
+				18,
+				1,
+				7,
+				13,
+				19,
+				25,
+				8,
+				14,
+				20,
+				21,
+				2,
+				15,
+				16,
+				22,
+				3,
+				9,
+		};
+
+		final JonkerVolgenantSolver solver = new JonkerVolgenantSolver( i, j, c );
+		final long start = System.currentTimeMillis();
+		if ( !solver.checkInput() || !solver.process() )
 		{
-			bc[ vals[ k ] ]++;
+			System.out.println( "Problem with Jonker-Volgenant solver:\n" + solver.getErrorMessage() );
+			System.exit( 1 );
 		}
-		return bc;
+		final long end = System.currentTimeMillis();
+		final int[][] result = solver.getResult();
+		System.out.println( "Algorithm converged in " + ( end - start ) + " ms." );
+		System.out.println( JVSUtils.resultToString( solver.getResult() ) );
+
 	}
+
 }
