a464216392e4f679ade04c12bad5e862865c7047
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2014-08-30 22:52:11 +0200

    WIP: tool to play with Kalman tracking.
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src/main/java/fiji/plugin/trackmate/tracking/kalman/KalmanTracker.java
+++ src/main/java/fiji/plugin/trackmate/tracking/kalman/KalmanTracker.java
@@ -1,5 +1,8 @@
 package fiji.plugin.trackmate.tracking.kalman;
 
+import ij.ImagePlus;
+
+import java.io.File;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.NavigableSet;
@@ -9,9 +12,16 @@ import org.jgrapht.graph.SimpleWeightedGraph;
 
 import Jama.Matrix;
 import fiji.plugin.trackmate.Logger;
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.SelectionModel;
+import fiji.plugin.trackmate.Settings;
 import fiji.plugin.trackmate.Spot;
 import fiji.plugin.trackmate.SpotCollection;
+import fiji.plugin.trackmate.io.TmXmlReader;
 import fiji.plugin.trackmate.tracking.SpotTracker;
+import fiji.plugin.trackmate.visualization.SpotColorGenerator;
+import fiji.plugin.trackmate.visualization.TrackMateModelView;
+import fiji.plugin.trackmate.visualization.hyperstack.HyperStackDisplayer;
 
 public class KalmanTracker implements SpotTracker
 {
@@ -25,6 +35,8 @@ public class KalmanTracker implements SpotTracker
 	private final SpotCollection spots;
 
 	private final Map< String, Object > settings;
+
+	private Model model;
 
 	/*
 	 * CONSTRUCTOR
@@ -78,39 +90,81 @@ public class KalmanTracker implements SpotTracker
 				secondSpot.diffTo( firstSpot, Spot.POSITION_Z ) };
 		final Matrix XP = new Matrix( xp, 6 );
 
+		System.out.println( "Initial state:" );// DEBUG
+		XP.print( 7, 1 );
 
 		final double[] xm = new double[] {
 				secondSpot.getDoublePosition( 0 ),
 				secondSpot.getDoublePosition( 1 ),
 				secondSpot.getDoublePosition( 2 ),
-				secondSpot.diffTo( firstSpot, Spot.POSITION_X ),
-				secondSpot.diffTo( firstSpot, Spot.POSITION_Y ),
-				secondSpot.diffTo( firstSpot, Spot.POSITION_Z ) };
-		final Matrix XM = new Matrix( xm, 6 );
-		
+		};
+		final Matrix XM = new Matrix( xm, 3 );
+
 		final Matrix A = Matrix.identity( 6, 6 );
 		final double dt = secondFrame - firstFrame;
 		for ( int i = 0; i < 3; i++ )
 		{
-			A.set( 3 + i, i, dt );
+			A.set( i, 3 + i, dt );
 		}
+		System.out.println( "Evolution matrix:" );// DEBUG
+		A.print( 7, 1 );
 
 		final Matrix H = Matrix.identity( 3, 6 );
 
-		final Matrix P = Matrix.identity( 6, 6 ).times( 100d );
+		Matrix P = Matrix.identity( 6, 6 ).times( 100d );
 		final Matrix Q = Matrix.identity( 6, 6 ).times( 1e-2 );
 		final Matrix R = Matrix.identity( 3, 3 ).times( 1e-2 );
 
-		final Matrix PP = A.times( P.times( A.transpose() ) ).plus( Q );
+		P = A.times( P.times( A.transpose() ) ).plus( Q );
+		Matrix TEMP = H.times( P.times( H.transpose() ) ).plus( R );
+		Matrix K = P.times( H.transpose() ).times( TEMP.inverse() );
 
-		final Matrix TEMP = H.times( PP.times( H.transpose() ) ).plus( R );
+		Matrix X = XP;
+		Spot previousSpot = firstSpot;
+		graph.addVertex( previousSpot );
+		final Spot location = new Spot( previousSpot );
 
-		final Matrix K = PP.times( H.transpose() ).times( TEMP.inverse() );
+		model.beginUpdate(); // DEBUG
+		for ( int frame = secondFrame; frame < keySet.last(); frame++ )
+		{
+			// Predict
+			X = A.times( X );
 
-		final Matrix X = XP.plus( K.times( XM.minus( H.times( XP ) ) ) );
+			System.out.println( "Prediction at frame " + frame );// DEBUG
+			X.print( 7, 1 );
 
-		System.out.println( X );// DEBUG
+			// Measure: the closest spot from prediction.
+			location.putFeature( Spot.POSITION_X, X.get( 0, 0 ) );
+			location.putFeature( Spot.POSITION_Y, X.get( 1, 0 ) );
+			location.putFeature( Spot.POSITION_Z, X.get( 2, 0 ) );
 
+			final Spot measurement = spots.getClosestSpot( location, frame + 1, true );
+			XM.set( 0, 0, measurement.getDoublePosition( 0 ) );
+			XM.set( 1, 0, measurement.getDoublePosition( 1 ) );
+			XM.set( 2, 0, measurement.getDoublePosition( 2 ) );
+
+			// Just for fun, we add it to the model.
+			final Spot sm = new Spot( location );
+			sm.putFeature( Spot.QUALITY, -1d );
+			model.addSpotTo( sm, frame + 1 );
+
+			System.out.println( "Measurement :" );// DEBUG
+			XM.print( 7, 1 );
+
+			graph.addVertex( measurement );
+			graph.addEdge( previousSpot, measurement );
+
+			// Update state
+			P = A.times( P.times( A.transpose() ) ).plus( Q );
+			TEMP = H.times( P.times( H.transpose() ) ).plus( R );
+			K = P.times( H.transpose() ).times( TEMP.inverse() );
+
+			X = X.plus( K.times( XM.minus( H.times( X ) ) ) );
+
+			// Loop
+			previousSpot = measurement;
+		}
+		model.endUpdate();
 		return true;
 	}
 
@@ -140,4 +194,45 @@ public class KalmanTracker implements SpotTracker
 		this.logger = logger;
 	}
 
+	/*
+	 * MAIN METHODS
+	 */
+
+	public static void main( final String[] args )
+	{
+		final File file = new File( "samples/FakeTracks.xml" );
+		final TmXmlReader reader = new TmXmlReader( file );
+		if ( !reader.isReadingOk() )
+		{
+			System.out.println( reader.getErrorMessage() );
+			return;
+		}
+
+		final Model model = reader.getModel();
+		final SpotCollection spots = model.getSpots();
+
+		final Settings settings = new Settings();
+		reader.readSettings( settings, null, null, null, null, null );
+
+		final KalmanTracker tracker = new KalmanTracker( spots, null );
+
+		tracker.model = model; // DEBUG
+
+		if (!tracker.checkInput() || !tracker.process()	) {
+			System.out.println(tracker.getErrorMessage());
+			return;
+		}
+
+		final SimpleWeightedGraph< Spot, DefaultWeightedEdge > graph= tracker.getResult();
+		model.setTracks( graph, false );
+
+		ij.ImageJ.main( args );
+		final SelectionModel selectionModel = new SelectionModel( model );
+		final ImagePlus imp = settings.imp;
+		final HyperStackDisplayer view = new HyperStackDisplayer( model, selectionModel, imp );
+		final SpotColorGenerator scg = new SpotColorGenerator( model );
+		scg.setFeature( Spot.QUALITY );
+		view.setDisplaySettings( TrackMateModelView.KEY_SPOT_COLORING, scg );
+		view.render();
+	}
 }
