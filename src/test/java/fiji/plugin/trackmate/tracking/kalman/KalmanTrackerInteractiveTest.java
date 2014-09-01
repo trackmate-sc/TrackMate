e8362b554de6f7a6549e6a4184bad810b285cd74
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2014-09-01 20:47:21 +0200

    More radical interactive test.
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src/test/java/fiji/plugin/trackmate/tracking/kalman/KalmanTrackerInteractiveTest.java
+++ src/test/java/fiji/plugin/trackmate/tracking/kalman/KalmanTrackerInteractiveTest.java
@@ -5,13 +5,16 @@ import fiji.plugin.trackmate.SelectionModel;
 import fiji.plugin.trackmate.Spot;
 import fiji.plugin.trackmate.SpotCollection;
 import fiji.plugin.trackmate.features.track.TrackIndexAnalyzer;
-import fiji.plugin.trackmate.tracking.kalman.KalmanTracker;
 import fiji.plugin.trackmate.visualization.PerTrackFeatureColorGenerator;
 import fiji.plugin.trackmate.visualization.SpotColorGenerator;
 import fiji.plugin.trackmate.visualization.TrackMateModelView;
 import fiji.plugin.trackmate.visualization.hyperstack.HyperStackDisplayer;
+import fiji.plugin.trackmate.visualization.trackscheme.TrackScheme;
 import ij.ImageJ;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class KalmanTrackerInteractiveTest
@@ -19,6 +22,8 @@ public class KalmanTrackerInteractiveTest
 	private static final int NFRAMES = 41;
 
 	private static final double WIDTH = 400;
+
+	private static final int NSINGLETRACKS = 30;
 
 	public static void main( final String[] args )
 	{
@@ -49,17 +54,37 @@ public class KalmanTrackerInteractiveTest
 
 		try
 		{
-		for ( final Integer f : predictions.keySet() )
-		{
-			for ( final Spot s : predictions.iterable( f, true ) )
+			for ( final Integer f : predictions.keySet() )
 			{
-				model.addSpotTo( s, f );
+				for ( final Spot s : predictions.iterable( f, true ) )
+				{
+					model.addSpotTo( s, f );
+				}
 			}
-		}
 		}
 		finally
 		{
 			model.endUpdate();
+		}
+
+		for ( final Integer trackID : model.getTrackModel().trackIDs( true ) )
+		{
+			final List< Spot > track = new ArrayList< Spot >( model.getTrackModel().trackSpots( trackID ) );
+			Collections.sort( track, Spot.frameComparator );
+
+			final Spot first = track.get( 0 );
+			final Spot last = track.get( track.size() - 1 );
+
+			final boolean mixed = first.getFeature( Spot.QUALITY ).doubleValue() != last.getFeature( Spot.QUALITY ).doubleValue();
+			if ( mixed )
+			{
+				System.out.println( "Track ID " + trackID + " got confused: it went from spot line " + first.getFeature( Spot.QUALITY ).doubleValue() + " to " + last.getFeature( Spot.QUALITY ).doubleValue() + "." );
+			}
+			else
+			{
+				System.out.println( "Track ID " + trackID + " kept its identity, spot line " + first.getFeature( Spot.QUALITY ).doubleValue() + "." );
+			}
+
 		}
 
 
@@ -75,7 +100,13 @@ public class KalmanTrackerInteractiveTest
 		final PerTrackFeatureColorGenerator tcg = new PerTrackFeatureColorGenerator( model, TrackIndexAnalyzer.TRACK_INDEX );
 		view.setDisplaySettings( TrackMateModelView.KEY_TRACK_COLORING, tcg );
 		view.setDisplaySettings( TrackMateModelView.KEY_DISPLAY_SPOT_NAMES, true );
+		view.setDisplaySettings( TrackMateModelView.KEY_TRACK_DISPLAY_MODE, TrackMateModelView.TRACK_DISPLAY_MODE_LOCAL );
 		view.render();
+
+		final TrackScheme trackscheme = new TrackScheme( model, selectionModel );
+		trackscheme.setDisplaySettings( TrackMateModelView.KEY_TRACK_COLORING, tcg );
+		trackscheme.setDisplaySettings( TrackMateModelView.KEY_DISPLAY_SPOT_NAMES, true );
+		trackscheme.render();
 
 	}
 
@@ -87,26 +118,31 @@ public class KalmanTrackerInteractiveTest
 		/*
 		 * The 4th lonely riders.
 		 */
-		final double[] x0 = new double[] { WIDTH / 10, WIDTH / 10, WIDTH / 2, 9 * WIDTH / 10 };
-		final double[] y0 = new double[] { WIDTH / 2, WIDTH / 10, WIDTH / 10, WIDTH / 10 };
-		final double[] vx0 = new double[] { WIDTH / ( NFRAMES - 1 ), WIDTH / ( NFRAMES - 1 ), 0, -WIDTH / ( NFRAMES - 1 ) };
-		final double[] vy0 = new double[] { 0, WIDTH / ( NFRAMES - 1 ), WIDTH / ( NFRAMES - 1 ), WIDTH / ( NFRAMES - 1 ) };
+		final double[] x0 = new double[ NSINGLETRACKS ];
+		final double[] y0 = new double[ NSINGLETRACKS ];
+		final double[] vx0 = new double[ NSINGLETRACKS ];
+		final double[] vy0 = new double[ NSINGLETRACKS ];
+		for ( int k = 0; k < NSINGLETRACKS; k++ )
+		{
+			final double angle = 2 * k * Math.PI / NSINGLETRACKS;
+			x0[ k ] = WIDTH / 2 * ( 1 + Math.cos( angle ) );
+			y0[ k ] = WIDTH / 2 * ( 1 + Math.sin( angle ) );
+			vx0[ k ] = -WIDTH / ( NFRAMES - 1 ) * Math.cos( angle );
+			vy0[ k ] = -WIDTH / ( NFRAMES - 1 ) * Math.sin( angle );
+		}
 
-		final double[] x = x0;
-		final double[] y = y0;
+		final double[] x = x0.clone();
+		final double[] y = y0.clone();
 		for ( int t = 0; t < NFRAMES; t++ )
 		{
-			for ( int i = 0; i < y.length; i++ )
+			for ( int k = 0; k < y.length; k++ )
 			{
 				final Spot spot = new Spot(
-						x[ i ] + ran.nextGaussian() * WIDTH / 200,
-						y[ i ] + ran.nextGaussian() * WIDTH / 200,
-						0, 2, i, "T_" + i + "_S_" + t );
-//				final Spot spot = new Spot( x[ i ], y[ i ], 0, 2, i, "T_" + i + "_S_" + t );
+						x[ k ] + ran.nextGaussian() * WIDTH / 200, y[ k ] + ran.nextGaussian() * WIDTH / 200, 0, 2, k, "T_" + k + "_S_" + t );
 				spots.add( spot, t );
 
-				x[ i ] += vx0[ i ];
-				y[ i ] += vy0[ i ];
+				x[ k ] += vx0[ k ];
+				y[ k ] += vy0[ k ];
 			}
 		}
 		return spots;
