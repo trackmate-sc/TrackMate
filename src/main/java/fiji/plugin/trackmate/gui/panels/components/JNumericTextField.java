c6efeb15967f02f83b3bfce0c3e24f0c2d1bc32f
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-09-03 16:25:20 +0200

    Code style changes.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/gui/panels/components/JNumericTextField.java
+++ src/main/java/fiji/plugin/trackmate/gui/panels/components/JNumericTextField.java
@@ -1,7 +1,5 @@
 package fiji.plugin.trackmate.gui.panels.components;
 
-import fiji.util.NumberParser;
-
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,81 +11,101 @@ import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import javax.swing.text.Document;
 
-public class JNumericTextField extends JTextField {
+import fiji.util.NumberParser;
+
+public class JNumericTextField extends JTextField
+{
 
 	/*
 	 * FIELDS
 	 */
 
 	private static final long serialVersionUID = 1L;
-	private static final Border BORDER_FOCUSED = new LineBorder(new Color(252, 117, 0), 1, true);
-	private static final Border BORDER_UNFOCUSED = new LineBorder(new Color(150, 150, 150), 1, true);
-	private final ActionListener al = new ActionListener() {
+
+	private static final Border BORDER_FOCUSED = new LineBorder( new Color( 252, 117, 0 ), 1, true );
+
+	private static final Border BORDER_UNFOCUSED = new LineBorder( new Color( 150, 150, 150 ), 1, true );
+
+	private final ActionListener al = new ActionListener()
+	{
 
 		@Override
-		public void actionPerformed(final ActionEvent e) {
+		public void actionPerformed( final ActionEvent e )
+		{
 			checkInput();
 		}
 	};
-	private double value = 0;
-	private double oldValue = 0;
 
+	private double value = 0;
+
+	private double oldValue = 0;
 
 	/*
 	 * CONSTRUCTORS
 	 */
 
-	public JNumericTextField(final Document doc, final String text, final int columns) {
-		super(doc, text, columns);
-		setBorder(BORDER_UNFOCUSED);
-		if (text != null) {
-			try {
-				value = NumberParser.parseDouble(text);
+	public JNumericTextField( final Document doc, final String text, final int columns )
+	{
+		super( doc, text, columns );
+		setBorder( BORDER_UNFOCUSED );
+		if ( text != null )
+		{
+			try
+			{
+				value = NumberParser.parseDouble( text );
 				oldValue = value;
-			} catch (final NumberFormatException nfe) {
+			}
+			catch ( final NumberFormatException nfe )
+			{
 				oldValue = 0;
 				value = 0;
 			}
 		}
-		addActionListener(al);
-		addFocusListener(new FocusListener() {
+		addActionListener( al );
+		addFocusListener( new FocusListener()
+		{
 			@Override
-			public void focusLost(final FocusEvent e) {
+			public void focusLost( final FocusEvent e )
+			{
 				checkInput();
-				setBorder(BORDER_UNFOCUSED);
+				setBorder( BORDER_UNFOCUSED );
 			}
 
 			@Override
-			public void focusGained(final FocusEvent e) {
-				setBorder(BORDER_FOCUSED);
+			public void focusGained( final FocusEvent e )
+			{
+				setBorder( BORDER_FOCUSED );
 			}
-		});
+		} );
 	}
 
-
-	public JNumericTextField(final int columns) {
-		this(null, null, columns);
+	public JNumericTextField( final int columns )
+	{
+		this( null, null, columns );
 	}
 
-
-	public JNumericTextField(final String text, final int columns) {
-		this(null, text, columns);
+	public JNumericTextField( final String text, final int columns )
+	{
+		this( null, text, columns );
 	}
 
-
-	public JNumericTextField(final String text) {
-		this(null, text, 0);
+	public JNumericTextField( final String text )
+	{
+		this( null, text, 0 );
 	}
 
-	public JNumericTextField() {
-		this(null, null, 0);
+	public JNumericTextField()
+	{
+		this( null, null, 0 );
 	}
 
-	public JNumericTextField(final double value) {
+	public JNumericTextField( final double value )
+	{
 		this( String.format( "%.3f", value ) );
 	}
 
-	public double getValue() {
+	public double getValue()
+	{
 		checkInput();
 		return value;
 	}
@@ -96,17 +114,20 @@ public class JNumericTextField extends JTextField {
 	 * METHODS
 	 */
 
-	private void checkInput() {
+	private void checkInput()
+	{
 		final String str = getText();
-		try {
-			value = NumberParser.parseDouble(str);
+		try
+		{
+			value = NumberParser.parseDouble( str );
 			oldValue = value;
-		} catch (final NumberFormatException nfe) {
+		}
+		catch ( final NumberFormatException nfe )
+		{
 			value = oldValue;
 			setText( String.format( "%.3f", value ) );
 		}
 
 	}
-
 
 }
