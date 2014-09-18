bff23c594abef19061c59646d5a41fafed8f30a8
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2014-09-18 15:34:28 +0200

    Make the start dialog panel resizable.
    And properly diplay numbers.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/gui/panels/StartDialogPanel.java
+++ src/main/java/fiji/plugin/trackmate/gui/panels/StartDialogPanel.java
@@ -11,8 +11,11 @@ import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 import fiji.plugin.trackmate.Model;
 import fiji.plugin.trackmate.Settings;
@@ -208,230 +211,365 @@ public class StartDialogPanel extends ActionListenablePanel {
 	private void initGUI() {
 		try {
 			this.setPreferredSize(new java.awt.Dimension(266, 476));
-			setLayout(null);
 			{
 				jLabelImageName = new JLabel();
-				jLabelImageName.setBounds(10, 14, 245, 17);
-				this.add(jLabelImageName);
 				jLabelImageName.setText("Select an image, and press refresh.");
 				jLabelImageName.setFont(BIG_FONT);
 			}
 			{
 				jLabelCheckCalibration = new JLabel();
-				jLabelCheckCalibration.setBounds(10, 107, 93, 13);
-				this.add(jLabelCheckCalibration);
 				jLabelCheckCalibration.setText("Calibration settings:");
 				jLabelCheckCalibration.setFont(SMALL_FONT);
 			}
 			{
 				jLabelPixelWidth = new JLabel();
-				jLabelPixelWidth.setBounds(63, 131, 55, 13);
-				this.add(jLabelPixelWidth);
 				jLabelPixelWidth.setText("Pixel width:");
 				jLabelPixelWidth.setFont(SMALL_FONT);
 			}
 			{
 				jLabelPixelHeight = new JLabel();
-				jLabelPixelHeight.setBounds(58, 151, 60, 13);
-				this.add(jLabelPixelHeight);
 				jLabelPixelHeight.setText("Pixel height:");
 				jLabelPixelHeight.setFont(SMALL_FONT);
 			}
 			{
 				jLabelVoxelDepth = new JLabel();
-				jLabelVoxelDepth.setBounds(58, 171, 60, 13);
-				this.add(jLabelVoxelDepth);
 				jLabelVoxelDepth.setText("Voxel depth:");
 				jLabelVoxelDepth.setFont(SMALL_FONT);
 			}
 			{
 				jLabelTimeInterval = new JLabel();
-				jLabelTimeInterval.setBounds(52, 191, 66, 13);
-				this.add(jLabelTimeInterval);
 				jLabelTimeInterval.setText("Time interval:" );				
 				jLabelTimeInterval.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldPixelWidth = new JNumericTextField();
-				jTextFieldPixelWidth.setBounds(128, 130, 40, 15);
-				this.add(jTextFieldPixelWidth);
+				jTextFieldPixelWidth.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldPixelWidth.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldPixelHeight = new JNumericTextField();
-				jTextFieldPixelHeight.setBounds(128, 150, 40, 15);
-				this.add(jTextFieldPixelHeight);
+				jTextFieldPixelHeight.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldPixelHeight.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldVoxelDepth = new JNumericTextField();
-				jTextFieldVoxelDepth.setBounds(128, 170, 40, 15);
-				this.add(jTextFieldVoxelDepth);
+				jTextFieldVoxelDepth.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldVoxelDepth.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldTimeInterval = new JNumericTextField();
-				jTextFieldTimeInterval.setBounds(128, 190, 40, 15);
-				this.add(jTextFieldTimeInterval);
+				jTextFieldTimeInterval.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldTimeInterval.setFont(SMALL_FONT);
 			}
 			{
 				jLabelUnits1 = new JLabel();
-				jLabelUnits1.setBounds(178, 131, 77, 13);
-				this.add(jLabelUnits1);
 				jLabelUnits1.setText("units");
 				jLabelUnits1.setFont(SMALL_FONT);
 			}
 			{
 				jLabelUnits2 = new JLabel();
-				jLabelUnits2.setBounds(178, 151, 77, 13);
-				this.add(jLabelUnits2);
 				jLabelUnits2.setText("units");
 				jLabelUnits2.setFont(SMALL_FONT);
 			}
 			{
 				jLabelUnits3 = new JLabel();
-				jLabelUnits3.setBounds(178, 171, 77, 13);
-				this.add(jLabelUnits3);
 				jLabelUnits3.setText("units");
 				jLabelUnits3.setFont(SMALL_FONT);
 			}
 			{
 				jLabelUnits4 = new JLabel();
-				jLabelUnits4.setBounds(178, 191, 78, 13);
-				this.add(jLabelUnits4);
 				jLabelUnits4.setText("units");
 				jLabelUnits4.setFont(SMALL_FONT);
 			}
 			{
 				jLabelCropSetting = new JLabel();
-				jLabelCropSetting.setBounds(10, 237, 111, 13);
-				this.add(jLabelCropSetting);
 				jLabelCropSetting.setText("Crop settings (in pixels, 0-based):");
 				jLabelCropSetting.setFont(SMALL_FONT);
 			}
 			{
 				jLabelX = new JLabel();
-				jLabelX.setBounds(58, 262, 7, 13);
-				this.add(jLabelX);
 				jLabelX.setText("X");
 				jLabelX.setFont(SMALL_FONT);
 			}
 			{
 				jLabelY = new JLabel();
-				jLabelY.setBounds(58, 285, 7, 13);
-				this.add(jLabelY);
 				jLabelY.setText("Y");
 				jLabelY.setFont(SMALL_FONT);
 			}
 			{
 				jLabelZ = new JLabel();
-				jLabelZ.setBounds(58, 308, 6, 13);
-				this.add(jLabelZ);
 				jLabelZ.setText("Z");
 				jLabelZ.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldXStart = new JNumericTextField();
-				jTextFieldXStart.setLocation(78, 260);
-				this.add(jTextFieldXStart);
-				jTextFieldXStart.setSize(40, 18);
+				jTextFieldXStart.setFormat( "%.0f" );
+				jTextFieldXStart.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldXStart.setPreferredSize(TEXTFIELD_DIMENSION);
 				jTextFieldXStart.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldYStart = new JNumericTextField();
-				jTextFieldYStart.setLocation(78, 283);
-				this.add(jTextFieldYStart);
-				jTextFieldYStart.setSize(40, 18);
+				jTextFieldYStart.setFormat( "%.0f" );
+				jTextFieldYStart.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldYStart.setPreferredSize(TEXTFIELD_DIMENSION);
 				jTextFieldYStart.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldZStart = new JNumericTextField();
-				jTextFieldZStart.setBounds(78, 306, 40, 18);
-				this.add(jTextFieldZStart);
+				jTextFieldZStart.setFormat( "%.0f" );
+				jTextFieldZStart.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldZStart.setPreferredSize(TEXTFIELD_DIMENSION);
 				jTextFieldZStart.setFont(SMALL_FONT);
 			}
 			{
 				jLabelTo1 = new JLabel();
-				jLabelTo1.setBounds(146, 262, 9, 13);
-				this.add(jLabelTo1);
 				jLabelTo1.setText("to");
 				jLabelTo1.setFont(SMALL_FONT);
 			}
 			{
 				jLabelTo2 = new JLabel();
-				jLabelTo2.setBounds(146, 285, 9, 13);
-				this.add(jLabelTo2);
 				jLabelTo2.setText("to");
 				jLabelTo2.setFont(SMALL_FONT);
 			}
 			{
 				jLabelTo3 = new JLabel();
-				jLabelTo3.setBounds(146, 308, 9, 13);
-				this.add(jLabelTo3);
 				jLabelTo3.setText("to");
 				jLabelTo3.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldXEnd = new JNumericTextField();
-				jTextFieldXEnd.setBounds(178, 260, 40, 18);
-				this.add(jTextFieldXEnd);
+				jTextFieldXEnd.setFormat( "%.0f" );
+				jTextFieldXEnd.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldXEnd.setPreferredSize(TEXTFIELD_DIMENSION);
 				jTextFieldXEnd.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldYEnd = new JNumericTextField();
-				jTextFieldYEnd.setBounds(178, 283, 40, 18);
-				this.add(jTextFieldYEnd);
+				jTextFieldYEnd.setFormat( "%.0f" );
+				jTextFieldYEnd.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldYEnd.setPreferredSize(TEXTFIELD_DIMENSION);
 				jTextFieldYEnd.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldZEnd = new JNumericTextField();
-				jTextFieldZEnd.setBounds(178, 306, 40, 18);
-				this.add(jTextFieldZEnd);
+				jTextFieldZEnd.setFormat( "%.0f" );
+				jTextFieldZEnd.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldZEnd.setPreferredSize(TEXTFIELD_DIMENSION);
 				jTextFieldZEnd.setFont(SMALL_FONT);
 			}
 			{
 				jLabelT = new JLabel();
-				jLabelT.setBounds(58, 332, 7, 13);
-				this.add(jLabelT);
 				jLabelT.setText("T");
 				jLabelT.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldTStart = new JNumericTextField();
-				jTextFieldTStart.setBounds(78, 330, 40, 18);
-				this.add(jTextFieldTStart);
+				jTextFieldTStart.setFormat( "%.0f" );
+				jTextFieldTStart.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldTStart.setPreferredSize(TEXTFIELD_DIMENSION);
 				jTextFieldTStart.setFont(SMALL_FONT);
 			}
 			{
 				jLabelTo4 = new JLabel();
-				jLabelTo4.setBounds(146, 332, 9, 13);
-				this.add(jLabelTo4);
 				jLabelTo4.setText("to");
 				jLabelTo4.setFont(SMALL_FONT);
 			}
 			{
 				jTextFieldTEnd = new JNumericTextField();
-				jTextFieldTEnd.setBounds(178, 330, 40, 18);
-				this.add(jTextFieldTEnd);
+				jTextFieldTEnd.setFormat( "%.0f" );
+				jTextFieldTEnd.setHorizontalAlignment( SwingConstants.CENTER );
 				jTextFieldTEnd.setPreferredSize(TEXTFIELD_DIMENSION);
 				jTextFieldTEnd.setFont(SMALL_FONT);
 			}
 			
 			{
 				jButtonRefresh = new JButton();
-				jButtonRefresh.setBounds(10, 422, 108, 29);
-				this.add(jButtonRefresh);
 				jButtonRefresh.setText("Refresh source");
 				jButtonRefresh.setToolTipText(TOOLTIP);
 				jButtonRefresh.setFont(SMALL_FONT);
+				final GroupLayout groupLayout = new GroupLayout( this );
+				groupLayout.setHorizontalGroup(
+						groupLayout.createParallelGroup( Alignment.LEADING )
+								.addGroup( groupLayout.createSequentialGroup()
+										.addGap( 10 )
+										.addComponent( jLabelImageName, GroupLayout.PREFERRED_SIZE, 245, GroupLayout.PREFERRED_SIZE ) )
+								.addGroup( groupLayout.createSequentialGroup()
+										.addGap( 58 )
+										.addComponent( jLabelPixelHeight, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE )
+										.addGap( 10 )
+										.addComponent( jTextFieldPixelHeight, GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE )
+										.addGap( 10 )
+										.addComponent( jLabelUnits2, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE )
+										.addGap( 11 ) )
+								.addGroup( groupLayout.createSequentialGroup()
+										.addGap( 58 )
+										.addComponent( jLabelVoxelDepth, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE )
+										.addGap( 10 )
+										.addComponent( jTextFieldVoxelDepth, GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE )
+										.addGap( 10 )
+										.addComponent( jLabelUnits3, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE )
+										.addGap( 11 ) )
+								.addGroup( Alignment.TRAILING, groupLayout.createSequentialGroup()
+										.addGap( 58 )
+										.addComponent( jLabelX )
+										.addGap( 13 )
+										.addComponent( jTextFieldXStart, GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE )
+										.addGap( 28 )
+										.addComponent( jLabelTo1 )
+										.addGap( 23 )
+										.addComponent( jTextFieldXEnd, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE )
+										.addGap( 48 ) )
+								.addGroup( Alignment.TRAILING, groupLayout.createSequentialGroup()
+										.addGap( 58 )
+										.addComponent( jLabelY )
+										.addGap( 13 )
+										.addComponent( jTextFieldYStart, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE )
+										.addGap( 28 )
+										.addComponent( jLabelTo2 )
+										.addGap( 23 )
+										.addComponent( jTextFieldYEnd, GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE )
+										.addGap( 48 ) )
+								.addGroup( Alignment.TRAILING, groupLayout.createSequentialGroup()
+										.addGap( 58 )
+										.addComponent( jLabelZ )
+										.addGap( 14 )
+										.addComponent( jTextFieldZStart, GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE )
+										.addGap( 28 )
+										.addComponent( jLabelTo3 )
+										.addGap( 23 )
+										.addComponent( jTextFieldZEnd, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE )
+										.addGap( 48 ) )
+								.addGroup( groupLayout.createSequentialGroup()
+										.addGap( 58 )
+										.addComponent( jLabelT, GroupLayout.PREFERRED_SIZE, 7, GroupLayout.PREFERRED_SIZE )
+										.addGap( 13 )
+										.addComponent( jTextFieldTStart, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE )
+										.addGap( 28 )
+										.addComponent( jLabelTo4 )
+										.addGap( 23 )
+										.addComponent( jTextFieldTEnd, GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE )
+										.addGap( 48 ) )
+								.addGroup( groupLayout.createSequentialGroup()
+										.addGap( 10 )
+										.addComponent( jButtonRefresh, GroupLayout.PREFERRED_SIZE, 108, Short.MAX_VALUE )
+										.addGap( 148 ) )
+								.addGroup( groupLayout.createSequentialGroup()
+										.addGroup( groupLayout.createParallelGroup( Alignment.TRAILING )
+												.addGroup( Alignment.LEADING, groupLayout.createSequentialGroup()
+														.addGap( 10 )
+														.addComponent( jLabelCropSetting, GroupLayout.PREFERRED_SIZE, 62, Short.MAX_VALUE ) )
+												.addGroup( Alignment.LEADING, groupLayout.createSequentialGroup()
+														.addGap( 52 )
+														.addComponent( jLabelTimeInterval, GroupLayout.PREFERRED_SIZE, 66, GroupLayout.PREFERRED_SIZE )
+														.addGap( 10 )
+														.addComponent( jTextFieldTimeInterval, GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE )
+														.addGap( 10 )
+														.addComponent( jLabelUnits4, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE ) ) )
+										.addGap( 10 ) )
+								.addGroup( groupLayout.createSequentialGroup()
+										.addGroup( groupLayout.createParallelGroup( Alignment.TRAILING )
+												.addGroup( Alignment.LEADING, groupLayout.createSequentialGroup()
+														.addGap( 10 )
+														.addComponent( jLabelCheckCalibration, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE ) )
+												.addGroup( Alignment.LEADING, groupLayout.createSequentialGroup()
+														.addGap( 63 )
+														.addComponent( jLabelPixelWidth, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE )
+														.addGap( 10 )
+														.addComponent( jTextFieldPixelWidth, GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE )
+														.addGap( 10 )
+														.addComponent( jLabelUnits1, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE ) ) )
+										.addGap( 11 ) )
+						);
+				groupLayout.setVerticalGroup(
+						groupLayout.createParallelGroup( Alignment.LEADING )
+								.addGroup( groupLayout.createSequentialGroup()
+										.addGap( 14 )
+										.addComponent( jLabelImageName )
+										.addGap( 76 )
+										.addComponent( jLabelCheckCalibration )
+										.addGap( 10 )
+										.addGroup( groupLayout.createParallelGroup( Alignment.LEADING )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 1 )
+														.addComponent( jLabelPixelWidth ) )
+												.addComponent( jTextFieldPixelWidth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 1 )
+														.addComponent( jLabelUnits1 ) ) )
+										.addGap( 5 )
+										.addGroup( groupLayout.createParallelGroup( Alignment.LEADING )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 1 )
+														.addComponent( jLabelPixelHeight ) )
+												.addComponent( jTextFieldPixelHeight, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 1 )
+														.addComponent( jLabelUnits2 ) ) )
+										.addGap( 5 )
+										.addGroup( groupLayout.createParallelGroup( Alignment.LEADING )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 1 )
+														.addComponent( jLabelVoxelDepth ) )
+												.addComponent( jTextFieldVoxelDepth, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 1 )
+														.addComponent( jLabelUnits3 ) ) )
+										.addGap( 5 )
+										.addGroup( groupLayout.createParallelGroup( Alignment.LEADING )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 1 )
+														.addComponent( jLabelTimeInterval ) )
+												.addComponent( jTextFieldTimeInterval, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 1 )
+														.addComponent( jLabelUnits4 ) ) )
+										.addGap( 32 )
+										.addComponent( jLabelCropSetting )
+										.addGap( 10 )
+										.addGroup( groupLayout.createParallelGroup( Alignment.LEADING )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 2 )
+														.addComponent( jLabelX ) )
+												.addComponent( jTextFieldXStart, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 2 )
+														.addComponent( jLabelTo1 ) )
+												.addComponent( jTextFieldXEnd, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE ) )
+										.addGap( 5 )
+										.addGroup( groupLayout.createParallelGroup( Alignment.LEADING )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 2 )
+														.addComponent( jLabelY ) )
+												.addComponent( jTextFieldYStart, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 2 )
+														.addComponent( jLabelTo2 ) )
+												.addComponent( jTextFieldYEnd, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE ) )
+										.addGap( 5 )
+										.addGroup( groupLayout.createParallelGroup( Alignment.LEADING )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 2 )
+														.addComponent( jLabelZ ) )
+												.addComponent( jTextFieldZStart, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 2 )
+														.addComponent( jLabelTo3 ) )
+												.addComponent( jTextFieldZEnd, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE ) )
+										.addGap( 6 )
+										.addGroup( groupLayout.createParallelGroup( Alignment.LEADING )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 2 )
+														.addComponent( jLabelT ) )
+												.addComponent( jTextFieldTStart, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE )
+												.addGroup( groupLayout.createSequentialGroup()
+														.addGap( 2 )
+														.addComponent( jLabelTo4 ) )
+												.addComponent( jTextFieldTEnd, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE ) )
+										.addGap( 74 )
+										.addComponent( jButtonRefresh ) )
+						);
+				setLayout( groupLayout );
 
 				jButtonRefresh.addActionListener(new ActionListener() {
 					@Override
