b2e729416fa8dd1bb92ef485c3143be289a07363
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-01-09 14:07:50 +0100

    Bump to next development cycle.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/TrackMate.java
+++ src/main/java/fiji/plugin/trackmate/TrackMate.java
@@ -44,7 +44,7 @@ public class TrackMate implements Benchmark, MultiThreaded, Algorithm
 
 	public static final String PLUGIN_NAME_STR = "TrackMate";
 
-	public static final String PLUGIN_NAME_VERSION = "2.6.5";
+	public static final String PLUGIN_NAME_VERSION = "2.6.6-SNAPSHOT";
 
 	/**
 	 * The model this trackmate will shape.
