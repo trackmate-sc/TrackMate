3eca604768c9613417cdb3019126891d74b56618
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-01-09 12:17:26 +0100

    Release v2.6.5
    * Put some sensible metadata defaults when presented a faulty metadata.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/trackmate/TrackMate.java
+++ src/main/java/fiji/plugin/trackmate/TrackMate.java
@@ -44,7 +44,7 @@ public class TrackMate implements Benchmark, MultiThreaded, Algorithm
 
 	public static final String PLUGIN_NAME_STR = "TrackMate";
 
-	public static final String PLUGIN_NAME_VERSION = "2.6.5-SNAPSHOT";
+	public static final String PLUGIN_NAME_VERSION = "2.6.5";
 
 	/**
 	 * The model this trackmate will shape.
