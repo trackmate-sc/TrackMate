520b0a6dcd0ae2b15fa37a1a8cbb2b361712b928
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2015-02-07 22:48:03 +0100

    Release v2.7.2.
    * Fix a but in the Linear motion LAP tracker.
    
    Signed-off-by: Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>

--- src/main/java/fiji/plugin/trackmate/TrackMate.java
+++ src/main/java/fiji/plugin/trackmate/TrackMate.java
@@ -44,7 +44,7 @@ public class TrackMate implements Benchmark, MultiThreaded, Algorithm
 
 	public static final String PLUGIN_NAME_STR = "TrackMate";
 
-	public static final String PLUGIN_NAME_VERSION = "2.7.2-SNAPSHOT";
+	public static final String PLUGIN_NAME_VERSION = "2.7.2";
 
 	/**
 	 * The model this trackmate will shape.
