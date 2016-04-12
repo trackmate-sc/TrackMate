07e149adad6d6ef731600b80ad05186b9ff8bca1
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2016-04-12 16:49:59 +0200

    Release v3.2.0.
    * TrackMate can now be called from an IJ macro.
    Only a subset of TrackMate features can be used, though. Right now, only the
    LoG detector and the Simple LAP tracker are used. But for instance, you can
    call TrackMate with the following syntax:
    
    run('TrackMate', "use_gui=false "
                            + "save_to=[/Users/tinevez/Desktop/TrackMateSaveTest.xml] "
                            + "export_to=[/Users/tinevez/Desktop/TrackMateExportTest.xml] "
                            + "display_results=true "
                            + "radius=2.5 "
                            + "threshold=50.1 "
                            + "subpixel=false "
                            + "median=false "
                            + "channel=1 "
                            + "max_frame_gap=0" )
    
    If you need other detectors and trackers, you have to rely on Python for now.
    See http://imagej.net/Scripting_TrackMate
    
    * Several TrackScheme improvements:
      - The row and column headers are now properly displayed and resized when
    zooming.
      - TrackScheme can be navigated with the keyboard:
        - The numeric keypad is used to pan around.
        - +/- zoom in/out.
      - Better navigation with mouse:
        - Hold space and drag to pan, as in ImageJ.
        - Hold space + mousewheel to zoom in/ out.

--- src/main/java/fiji/plugin/trackmate/TrackMate.java
+++ src/main/java/fiji/plugin/trackmate/TrackMate.java
@@ -40,7 +40,7 @@ public class TrackMate implements Benchmark, MultiThreaded, Algorithm
 
 	public static final String PLUGIN_NAME_STR = "TrackMate";
 
-	public static final String PLUGIN_NAME_VERSION = "3.1.1-SNAPSHOT";
+	public static final String PLUGIN_NAME_VERSION = "3.2.0";
 
 	/**
 	 * The model this trackmate will shape.
