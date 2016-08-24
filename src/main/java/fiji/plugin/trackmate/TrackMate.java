8cc89d3de72fbe3fbf1c9084377f978d174986be
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2016-08-24 02:15:51 +0200

    Release v3.4.1
    * Fix the spot SNR and contrast analyzer.
    This analyzer work by iterating outside the spot radius, over a radius
    twice as large. This was not done before this commit.
    
    * Fix exceptions when computing spot features for spots much smaller
    than 1 pixel.

--- src/main/java/fiji/plugin/trackmate/TrackMate.java
+++ src/main/java/fiji/plugin/trackmate/TrackMate.java
@@ -40,7 +40,7 @@ public class TrackMate implements Benchmark, MultiThreaded, Algorithm
 
 	public static final String PLUGIN_NAME_STR = "TrackMate";
 
-	public static final String PLUGIN_NAME_VERSION = "3.4.1-SNAPSHOT";
+	public static final String PLUGIN_NAME_VERSION = "3.4.1";
 
 	/**
 	 * The model this trackmate will shape.
