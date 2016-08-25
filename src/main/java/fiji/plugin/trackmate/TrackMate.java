54d7df05fa5231140c96a56583b45574752562d5
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2016-08-25 16:50:31 +0200

    Release v3.4.2
    * Fix statistics table crash when there is edge time feature.
    * Fix a regression that prevented e.g. MaMuT files to
    be opened in TrackMate.

--- src/main/java/fiji/plugin/trackmate/TrackMate.java
+++ src/main/java/fiji/plugin/trackmate/TrackMate.java
@@ -40,7 +40,7 @@ public class TrackMate implements Benchmark, MultiThreaded, Algorithm
 
 	public static final String PLUGIN_NAME_STR = "TrackMate";
 
-	public static final String PLUGIN_NAME_VERSION = "3.4.2-SNAPSHOT";
+	public static final String PLUGIN_NAME_VERSION = "3.4.2";
 
 	/**
 	 * The model this trackmate will shape.
