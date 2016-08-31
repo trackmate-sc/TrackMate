2db7a881c22d5ea01abd9bf184b396c43b0c8355
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2016-08-31 11:28:22 +0200

    Code style changes.

--- src/main/java/fiji/plugin/trackmate/action/RecalculateFeatureAction.java
+++ src/main/java/fiji/plugin/trackmate/action/RecalculateFeatureAction.java
@@ -1,35 +1,39 @@
 package fiji.plugin.trackmate.action;
 
+import javax.swing.ImageIcon;
+
+import org.scijava.plugin.Plugin;
+
 import fiji.plugin.trackmate.Logger;
 import fiji.plugin.trackmate.Model;
 import fiji.plugin.trackmate.TrackMate;
 import fiji.plugin.trackmate.gui.TrackMateGUIController;
 import fiji.plugin.trackmate.gui.TrackMateWizard;
 
-import javax.swing.ImageIcon;
+public class RecalculateFeatureAction extends AbstractTMAction
+{
 
-import org.scijava.plugin.Plugin;
+	public static final ImageIcon ICON = new ImageIcon( TrackMateWizard.class.getResource( "images/calculator.png" ) );
 
-public class RecalculateFeatureAction extends AbstractTMAction {
-
-	public static final ImageIcon ICON = new ImageIcon(TrackMateWizard.class.getResource("images/calculator.png"));
 	public static final String NAME = "Recompute all spot features";
 
 	public static final String KEY = "RECOMPUTE_SPOT_FEATURES";
+
 	public static final String INFO_TEXT = "<html>" +
 			"Calling this action causes the model to recompute all the features <br>" +
 			"for all spots." +
 			"</html>";
 
 	@Override
-	public void execute(final TrackMate trackmate) {
-		logger.log("Recalculating all features.\n");
+	public void execute( final TrackMate trackmate )
+	{
+		logger.log( "Recalculating all features.\n" );
 		final Model model = trackmate.getModel();
 		final Logger oldLogger = model.getLogger();
-		model.setLogger(logger);
-		trackmate.computeSpotFeatures(true);
-		model.setLogger(oldLogger);
-		logger.log("Done.\n");
+		model.setLogger( logger );
+		trackmate.computeSpotFeatures( true );
+		model.setLogger( oldLogger );
+		logger.log( "Done.\n" );
 	}
 
 	@Plugin( type = TrackMateActionFactory.class )
