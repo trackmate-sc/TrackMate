7af9eeb4efb1e0f8832b7a99dd3d36ef7731c959
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2017-12-29 10:31:08 +0100

    Code style changes.

--- src/main/java/fiji/plugin/trackmate/util/ExportableChartPanel.java
+++ src/main/java/fiji/plugin/trackmate/util/ExportableChartPanel.java
@@ -1,10 +1,5 @@
 package fiji.plugin.trackmate.util;
 
-import com.itextpdf.text.DocumentException;
-
-import ij.IJ;
-import ij.measure.ResultsTable;
-
 import java.awt.Container;
 import java.awt.FileDialog;
 import java.awt.Frame;
@@ -25,7 +20,13 @@ import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.ui.ExtensionFileFilter;
 
-public class ExportableChartPanel extends ChartPanel {
+import com.itextpdf.text.DocumentException;
+
+import ij.IJ;
+import ij.measure.ResultsTable;
+
+public class ExportableChartPanel extends ChartPanel
+{
 
 	/*
 	 * CONSTRUCTORS
@@ -33,36 +34,40 @@ public class ExportableChartPanel extends ChartPanel {
 
 	private static final long serialVersionUID = -6556930372813672992L;
 
-	public ExportableChartPanel(JFreeChart chart) {
-		super(chart);
+	public ExportableChartPanel( final JFreeChart chart )
+	{
+		super( chart );
 	}
 
-	public ExportableChartPanel(JFreeChart chart,
-			boolean properties,
-			boolean save,
-			boolean print,
-			boolean zoom,
-			boolean tooltips) {
-		super(chart, properties, save, print, zoom, tooltips);
+	public ExportableChartPanel( final JFreeChart chart,
+			final boolean properties,
+			final boolean save,
+			final boolean print,
+			final boolean zoom,
+			final boolean tooltips )
+	{
+		super( chart, properties, save, print, zoom, tooltips );
 	}
 
-	public ExportableChartPanel(JFreeChart chart, int width, int height,
-			int minimumDrawWidth, int minimumDrawHeight, int maximumDrawWidth,
-			int maximumDrawHeight, boolean useBuffer, boolean properties,
-			boolean save, boolean print, boolean zoom, boolean tooltips) {
-		super(chart, width, height, minimumDrawWidth, minimumDrawHeight, 
+	public ExportableChartPanel( final JFreeChart chart, final int width, final int height,
+			final int minimumDrawWidth, final int minimumDrawHeight, final int maximumDrawWidth,
+			final int maximumDrawHeight, final boolean useBuffer, final boolean properties,
+			final boolean save, final boolean print, final boolean zoom, final boolean tooltips )
+	{
+		super( chart, width, height, minimumDrawWidth, minimumDrawHeight,
 				maximumDrawWidth, maximumDrawHeight,
-				useBuffer, properties, save, print, zoom, tooltips);
+				useBuffer, properties, save, print, zoom, tooltips );
 	}
 
-	public ExportableChartPanel(JFreeChart chart, int width, int height,
-			int minimumDrawWidth, int minimumDrawHeight, int maximumDrawWidth,
-			int maximumDrawHeight, boolean useBuffer, boolean properties,
-			boolean copy, boolean save, boolean print, boolean zoom,
-			boolean tooltips) {
-		super(chart, width, height, minimumDrawWidth, minimumDrawHeight, 
+	public ExportableChartPanel( final JFreeChart chart, final int width, final int height,
+			final int minimumDrawWidth, final int minimumDrawHeight, final int maximumDrawWidth,
+			final int maximumDrawHeight, final boolean useBuffer, final boolean properties,
+			final boolean copy, final boolean save, final boolean print, final boolean zoom,
+			final boolean tooltips )
+	{
+		super( chart, width, height, minimumDrawWidth, minimumDrawHeight,
 				maximumDrawWidth, maximumDrawHeight,
-				useBuffer, properties, copy, save, print, zoom, tooltips);	
+				useBuffer, properties, copy, save, print, zoom, tooltips );
 	}
 
 	/*
@@ -70,141 +75,171 @@ public class ExportableChartPanel extends ChartPanel {
 	 */
 
 	@Override
-	protected JPopupMenu createPopupMenu(boolean properties, boolean copy, boolean save, boolean print, boolean zoom) {
-		JPopupMenu menu = super.createPopupMenu(properties, copy, save, print, zoom);
+	protected JPopupMenu createPopupMenu( final boolean properties, final boolean copy, final boolean save, final boolean print, final boolean zoom )
+	{
+		final JPopupMenu menu = super.createPopupMenu( properties, copy, save, print, zoom );
 
 		menu.addSeparator();
 
-		JMenuItem exportTableItem = new JMenuItem("Display data tables");
-		exportTableItem.setActionCommand("TABLES");
-		exportTableItem.addActionListener(new ActionListener() {
+		final JMenuItem exportTableItem = new JMenuItem( "Display data tables" );
+		exportTableItem.setActionCommand( "TABLES" );
+		exportTableItem.addActionListener( new ActionListener()
+		{
 
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed( final ActionEvent e )
+			{
 				createDataTable();
 			}
-		});
-		menu.add(exportTableItem);
+		} );
+		menu.add( exportTableItem );
 
 		return menu;
 	}
 
-
-	public void createDataTable() {
+	public void createDataTable()
+	{
 		XYPlot plot = null;
-		try {
+		try
+		{
 			plot = getChart().getXYPlot();
-		} catch (ClassCastException e) {
+		}
+		catch ( final ClassCastException e )
+		{
 			return;
 		}
 
-		String xColumnName = plot.getDomainAxis().getLabel();
+		final String xColumnName = plot.getDomainAxis().getLabel();
 
-		ResultsTable table = new ResultsTable(); // In OUR case this will work: there is one common X axis
+		final ResultsTable table = new ResultsTable(); // In OUR case this will
+														// work:
+														// there is one common X
+														// axis
 
-		int nPoints = plot.getDataset(0).getItemCount(0);
-		for (int k = 0; k < nPoints; k++) {
+		final int nPoints = plot.getDataset( 0 ).getItemCount( 0 );
+		for ( int k = 0; k < nPoints; k++ )
+		{
 			table.incrementCounter();
 
+			final double xVal = plot.getDataset( 0 ).getXValue( 0, k );
+			table.addValue( xColumnName, xVal );
 
-			double xVal =  plot.getDataset(0).getXValue(0, k);
-			table.addValue(xColumnName, xVal);
+			final int nSets = plot.getDatasetCount();
+			for ( int i = 0; i < nSets; i++ )
+			{
 
-			int nSets = plot.getDatasetCount();
-			for (int i = 0; i < nSets; i++) {
-
-				XYDataset dataset = plot.getDataset(i);
-				if (dataset instanceof XYEdgeSeriesCollection)
+				final XYDataset dataset = plot.getDataset( i );
+				if ( dataset instanceof XYEdgeSeriesCollection )
 					continue;
 
-				int nSeries = dataset.getSeriesCount();
-				for (int j = 0; j < nSeries; j++) {
+				final int nSeries = dataset.getSeriesCount();
+				for ( int j = 0; j < nSeries; j++ )
+				{
 
-					@SuppressWarnings("rawtypes")
-					Comparable seriesKey = dataset.getSeriesKey(j);
-					String yColumnName = seriesKey.toString() + "("+plot.getRangeAxis().getLabel()+")";
-					double yVal = dataset.getYValue(j, k);
-					table.addValue(yColumnName, yVal);
+					@SuppressWarnings( "rawtypes" )
+					final Comparable seriesKey = dataset.getSeriesKey( j );
+					final String yColumnName = seriesKey.toString() + "(" + plot.getRangeAxis().getLabel() + ")";
+					final double yVal = dataset.getYValue( j, k );
+					table.addValue( yColumnName, yVal );
 				}
 
 			}
 		}
-		table.show(getChart().getTitle().getText());
+		table.show( getChart().getTitle().getText() );
 	}
-
-
 
 	/**
 	 * Opens a file chooser and gives the user an opportunity to save the chart
 	 * in PNG, PDF or SVG format.
 	 *
-	 * @throws IOException if there is an I/O error.
+	 * @throws IOException
+	 *             if there is an I/O error.
 	 */
-	public void doSaveAs() throws IOException {
+	@Override
+	public void doSaveAs() throws IOException
+	{
 
 		File file = null;
 
-		if(IJ.isMacintosh()) {
+		if ( IJ.isMacintosh() )
+		{
 			// use the native file dialog on the mac
 			Container dialogParent = getParent();
-			while ( !(dialogParent instanceof Frame)) {
+			while ( !( dialogParent instanceof Frame ) )
+			{
 				dialogParent = dialogParent.getParent();
 			}
-			Frame frame = (Frame) dialogParent;
+			final Frame frame = ( Frame ) dialogParent;
 
-			FileDialog dialog =	new FileDialog(frame, "Export chart to PNG, PDF or SVG", FileDialog.SAVE);
+			final FileDialog dialog = new FileDialog( frame, "Export chart to PNG, PDF or SVG", FileDialog.SAVE );
 			String defaultDir = null;
-			if (getDefaultDirectoryForSaveAs() != null) {
+			if ( getDefaultDirectoryForSaveAs() != null )
+			{
 				defaultDir = getDefaultDirectoryForSaveAs().getPath();
 			}
-			dialog.setDirectory(defaultDir);
-			FilenameFilter filter = new FilenameFilter() {
+			dialog.setDirectory( defaultDir );
+			final FilenameFilter filter = new FilenameFilter()
+			{
 				@Override
-				public boolean accept(File dir, String name) {
-					return name.endsWith(".png") || name.endsWith(".pdf") || name.endsWith(".svg");
+				public boolean accept( final File dir, final String name )
+				{
+					return name.endsWith( ".png" ) || name.endsWith( ".pdf" ) || name.endsWith( ".svg" );
 				}
 			};
-			dialog.setFilenameFilter(filter);
-			dialog.setVisible(true);
-			String selectedFile = dialog.getFile();
+			dialog.setFilenameFilter( filter );
+			dialog.setVisible( true );
+			final String selectedFile = dialog.getFile();
 
-			if (null == selectedFile)
+			if ( null == selectedFile )
 				return;
 
-			file = new File(dialog.getDirectory(), selectedFile);
+			file = new File( dialog.getDirectory(), selectedFile );
 
-		} else {
+		}
+		else
+		{
 
-			JFileChooser fileChooser = new JFileChooser();
-			fileChooser.setCurrentDirectory(getDefaultDirectoryForSaveAs());
-			ExtensionFileFilter filter1 = new ExtensionFileFilter("PNG Image File", ".png");
-			ExtensionFileFilter filter2 = new ExtensionFileFilter("Portable Document File (PDF)", ".pdf");
-			ExtensionFileFilter filter3 = new ExtensionFileFilter("Scalable Vector Graphics (SVG)", ".svg");
-			fileChooser.setAcceptAllFileFilterUsed(false);
-			fileChooser.addChoosableFileFilter(filter1);
-			fileChooser.addChoosableFileFilter(filter2);
-			fileChooser.addChoosableFileFilter(filter3);
+			final JFileChooser fileChooser = new JFileChooser();
+			fileChooser.setCurrentDirectory( getDefaultDirectoryForSaveAs() );
+			final ExtensionFileFilter filter1 = new ExtensionFileFilter( "PNG Image File", ".png" );
+			final ExtensionFileFilter filter2 = new ExtensionFileFilter( "Portable Document File (PDF)", ".pdf" );
+			final ExtensionFileFilter filter3 = new ExtensionFileFilter( "Scalable Vector Graphics (SVG)", ".svg" );
+			fileChooser.setAcceptAllFileFilterUsed( false );
+			fileChooser.addChoosableFileFilter( filter1 );
+			fileChooser.addChoosableFileFilter( filter2 );
+			fileChooser.addChoosableFileFilter( filter3 );
 
-			int option = fileChooser.showSaveDialog(this);
-			if (option == JFileChooser.APPROVE_OPTION) {
-				file =  fileChooser.getSelectedFile();
+			final int option = fileChooser.showSaveDialog( this );
+			if ( option == JFileChooser.APPROVE_OPTION )
+			{
+				file = fileChooser.getSelectedFile();
 
-			} else {
+			}
+			else
+			{
 				return;
 			}
 		}
-		if (file.getPath().endsWith(".png")) {
-			ChartUtilities.saveChartAsPNG(file, getChart(), getWidth(), getHeight());
+		if ( file.getPath().endsWith( ".png" ) )
+		{
+			ChartUtilities.saveChartAsPNG( file, getChart(), getWidth(), getHeight() );
 
-		} else if (file.getPath().endsWith(".pdf")) {
-			try {
-				ChartExporter.exportChartAsPDF(getChart(), getBounds(), file);
-			} catch (DocumentException e) {
+		}
+		else if ( file.getPath().endsWith( ".pdf" ) )
+		{
+			try
+			{
+				ChartExporter.exportChartAsPDF( getChart(), getBounds(), file );
+			}
+			catch ( final DocumentException e )
+			{
 				e.printStackTrace();
 			}
 
-		} else if (file.getPath().endsWith(".svg")) {
-			ChartExporter.exportChartAsSVG(getChart(), getBounds(), file);
+		}
+		else if ( file.getPath().endsWith( ".svg" ) )
+		{
+			ChartExporter.exportChartAsSVG( getChart(), getBounds(), file );
 		}
 	}
 
--- src/main/java/fiji/plugin/trackmate/util/XYEdgeRenderer.java
+++ src/main/java/fiji/plugin/trackmate/util/XYEdgeRenderer.java
@@ -13,54 +13,58 @@ import org.jfree.chart.renderer.xy.XYItemRendererState;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.ui.RectangleEdge;
 
-public class XYEdgeRenderer extends AbstractXYItemRenderer {
-	
+public class XYEdgeRenderer extends AbstractXYItemRenderer
+{
+
 	private static final long serialVersionUID = -4565389588020243812L;
-	
+
 	@Override
-	public void drawItem(Graphics2D g2, XYItemRendererState state, Rectangle2D dataArea, PlotRenderingInfo info, XYPlot plot,
-			ValueAxis domainAxis, ValueAxis rangeAxis, XYDataset dataset, int series, int item, CrosshairState crosshairState, int pass) {
-		
+	public void drawItem( final Graphics2D g2, final XYItemRendererState state, final Rectangle2D dataArea, final PlotRenderingInfo info, final XYPlot plot,
+			final ValueAxis domainAxis, final ValueAxis rangeAxis, final XYDataset dataset, final int series, final int item, final CrosshairState crosshairState, final int pass )
+	{
+
 		// get the data point...
-		XYEdgeSeriesCollection edgeDataset = (XYEdgeSeriesCollection) dataset;
-		XYEdgeSeries s = edgeDataset.getSeries(series);
-		
-		double x0 = s.getEdgeXStart(item).doubleValue();
-		double y0 = s.getEdgeYStart(item).doubleValue();
-		double x1 = s.getEdgeXEnd(item).doubleValue();
-		double y1 = s.getEdgeYEnd(item).doubleValue();
-		
-        if (Double.isNaN(y1) || Double.isNaN(x1) || Double.isNaN(y0) || Double.isNaN(x0))
-            return;
-        
-        RectangleEdge xAxisLocation = plot.getDomainAxisEdge();
-        RectangleEdge yAxisLocation = plot.getRangeAxisEdge();
+		final XYEdgeSeriesCollection edgeDataset = ( XYEdgeSeriesCollection ) dataset;
+		final XYEdgeSeries s = edgeDataset.getSeries( series );
 
-        double transX0 = domainAxis.valueToJava2D(x0, dataArea, xAxisLocation);
-        double transY0 = rangeAxis.valueToJava2D(y0, dataArea, yAxisLocation);
+		final double x0 = s.getEdgeXStart( item ).doubleValue();
+		final double y0 = s.getEdgeYStart( item ).doubleValue();
+		final double x1 = s.getEdgeXEnd( item ).doubleValue();
+		final double y1 = s.getEdgeYEnd( item ).doubleValue();
 
-        double transX1 = domainAxis.valueToJava2D(x1, dataArea, xAxisLocation);
-        double transY1 = rangeAxis.valueToJava2D(y1, dataArea, yAxisLocation);
-        
-        // only draw if we have good values
-        if (Double.isNaN(transX0) || Double.isNaN(transY0) || Double.isNaN(transX1) || Double.isNaN(transY1)) 
-            return;
+		if ( Double.isNaN( y1 ) || Double.isNaN( x1 ) || Double.isNaN( y0 ) || Double.isNaN( x0 ) )
+			return;
 
-        PlotOrientation orientation = plot.getOrientation();
-        if (orientation == PlotOrientation.HORIZONTAL) {
-            state.workingLine.setLine(transY0, transX0, transY1, transX1);
-        }
-        else if (orientation == PlotOrientation.VERTICAL) {
-            state.workingLine.setLine(transX0, transY0, transX1, transY1);
-        }
+		final RectangleEdge xAxisLocation = plot.getDomainAxisEdge();
+		final RectangleEdge yAxisLocation = plot.getRangeAxisEdge();
 
-        if (state.workingLine.intersects(dataArea)) {
-            g2.setStroke(getItemStroke(series, item));
-            g2.setPaint(getItemPaint(series, item));
-            g2.draw(state.workingLine);
-        }
-		
-		
+		final double transX0 = domainAxis.valueToJava2D( x0, dataArea, xAxisLocation );
+		final double transY0 = rangeAxis.valueToJava2D( y0, dataArea, yAxisLocation );
+
+		final double transX1 = domainAxis.valueToJava2D( x1, dataArea, xAxisLocation );
+		final double transY1 = rangeAxis.valueToJava2D( y1, dataArea, yAxisLocation );
+
+		// only draw if we have good values
+		if ( Double.isNaN( transX0 ) || Double.isNaN( transY0 ) || Double.isNaN( transX1 ) || Double.isNaN( transY1 ) )
+			return;
+
+		final PlotOrientation orientation = plot.getOrientation();
+		if ( orientation == PlotOrientation.HORIZONTAL )
+		{
+			state.workingLine.setLine( transY0, transX0, transY1, transX1 );
+		}
+		else if ( orientation == PlotOrientation.VERTICAL )
+		{
+			state.workingLine.setLine( transX0, transY0, transX1, transY1 );
+		}
+
+		if ( state.workingLine.intersects( dataArea ) )
+		{
+			g2.setStroke( getItemStroke( series, item ) );
+			g2.setPaint( getItemPaint( series, item ) );
+			g2.draw( state.workingLine );
+		}
+
 	}
 
 }
