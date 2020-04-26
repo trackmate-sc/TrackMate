deff17dcb9786a74477df8943536f89f7fc2d23e
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2020-04-26 18:10:36 +0200

    Geometric utilities around SpotRoi.

--- src/main/java/fiji/plugin/trackmate/SpotRoi.java
+++ src/main/java/fiji/plugin/trackmate/SpotRoi.java
@@ -1,5 +1,7 @@
 package fiji.plugin.trackmate;
 
+import java.util.Arrays;
+
 import net.imagej.ImgPlus;
 import net.imglib2.IterableInterval;
 import net.imglib2.RandomAccessibleInterval;
@@ -91,5 +93,82 @@ public class SpotRoi
 		final WritablePolygon2D polygon = GeomMasks.closedPolygon2D( xp, yp );
 		final IterableRegion< BoolType > region = Masks.toIterableRegion( polygon );
 		return Regions.sample( region, Views.extendMirrorDouble( img ) );
+	}
+
+	public double radius()
+	{
+		return Math.sqrt( area() / Math.PI );
+	}
+
+	public double area()
+	{
+		return Math.abs( signedArea( x, y ) );
+	}
+
+	public void scale( final double alpha )
+	{
+		for ( int i = 0; i < x.length; i++ )
+		{
+			final double x = this.x[ i ];
+			final double y = this.y[ i ];
+			final double r = Math.sqrt( x * x + y * y );
+			final double costheta = x / r;
+			final double sintheta = y / r;
+			this.x[ i ] = costheta * r * alpha;
+			this.y[ i ] = sintheta * r * alpha;
+		}
+	}
+
+	public static Spot createSpot( final double[] x, final double[] y, final double quality )
+	{
+		// Put polygon coordinates with respect to centroid.
+		final double[] centroid = centroid( x, y );
+		final double xc = centroid[ 0 ];
+		final double yc = centroid[ 1 ];
+		final double[] xr = Arrays.stream( x ).map( x0 -> x0 - xc ).toArray();
+		final double[] yr = Arrays.stream( y ).map( y0 -> y0 - yc ).toArray();
+		
+		// Create roi.
+		final SpotRoi roi = new SpotRoi( xr, yr );
+		
+		// Create spot.
+		final double z = 0.;
+		final double r = roi.radius();
+		final Spot spot = new Spot( xc, yc, z, r, quality );
+		spot.setRoi( roi );
+		return spot;
+	}
+
+	/*
+	 * UTILS.
+	 */
+
+	private static final double[] centroid( final double[] x, final double[] y )
+	{
+		final double area = signedArea( x, y );
+		double ax = 0.0;
+		double ay = 0.0;
+		final int n = x.length;
+		for ( int i = 0; i < n - 1; i++ )
+		{
+			final double w = x[ i ] * y[ i + 1 ] - x[ i + 1 ] * y[ i ];
+			ax += ( x[ i ] + x[ i + 1 ] ) * w;
+			ay += ( y[ i ] + y[ i + 1 ] ) * w;
+		}
+
+		final double w0 = x[ n - 1 ] * y[ 0 ] - x[ 0 ] * y[ n - 1 ];
+		ax += ( x[ n - 1 ] + x[ 0 ] ) * w0;
+		ay += ( y[ n - 1 ] + y[ 0 ] ) * w0;
+		return new double[] { ax / 6. / area, ay / 6. / area };
+	}
+
+	private static final double signedArea( final double[] x, final double[] y )
+	{
+		final int n = x.length;
+		double a = 0.0;
+		for ( int i = 0; i < n - 1; i++ )
+			a += x[ i ] * y[ i + 1 ] - x[ i + 1 ] * y[ i ];
+
+		return ( a + x[ n - 1 ] * y[ 0 ] - x[ 0 ] * y[ n - 1 ] ) / 2.0;
 	}
 }
