beda83767306d80d9cfaf063c087c90dee92e173
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2020-04-27 20:25:22 +0200

    Skip choosing view in the GUI if there is only 1 to choose.

--- src/main/java/fiji/plugin/trackmate/gui/TrackMateGUIController.java
+++ src/main/java/fiji/plugin/trackmate/gui/TrackMateGUIController.java
@@ -782,9 +782,16 @@ public class TrackMateGUIController implements ActionListener
 		else if ( currentDescriptor == detectorConfigurationDescriptor )
 		{
 			if ( trackmate.getSettings().detectorFactory.getKey().equals( ManualDetectorFactory.DETECTOR_KEY ) )
-				return viewChoiceDescriptor;
-
-			return detectionDescriptor;
+			{
+				if ( viewProvider.getVisibleKeys().size() == 1 )
+					return spotFilterDescriptor;
+				else
+					return viewChoiceDescriptor;
+			}
+			else
+			{
+				return detectionDescriptor;
+			}
 
 		}
 		else if ( currentDescriptor == detectionDescriptor )
@@ -794,7 +801,11 @@ public class TrackMateGUIController implements ActionListener
 		}
 		else if ( currentDescriptor == initFilterDescriptor )
 		{
-			return viewChoiceDescriptor;
+			// Skip choice of view if we just have one.
+			if ( viewProvider.getVisibleKeys().size() == 1 )
+				return spotFilterDescriptor;
+			else
+				return viewChoiceDescriptor;
 
 		}
 		else if ( currentDescriptor == viewChoiceDescriptor )
@@ -886,7 +897,10 @@ public class TrackMateGUIController implements ActionListener
 		}
 		else if ( currentDescriptor == spotFilterDescriptor )
 		{
-			return viewChoiceDescriptor;
+			if ( viewProvider.getVisibleKeys().size() == 1 )
+				return detectorConfigurationDescriptor;
+			else
+				return viewChoiceDescriptor;
 
 		}
 		else if ( currentDescriptor == trackerChoiceDescriptor )
@@ -1152,9 +1166,7 @@ public class TrackMateGUIController implements ActionListener
 		// Check if the new panel has a next panel. If not, disable the next
 		// button
 		if ( null == previousDescriptor( panelDescriptor ) )
-		{
 			gui.setPreviousButtonEnabled( false );
-		}
 
 		// Re-enable the previous button, in case it was disabled
 		gui.setNextButtonEnabled( true );
