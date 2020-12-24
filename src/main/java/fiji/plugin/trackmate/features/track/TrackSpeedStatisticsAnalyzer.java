cadd653fcd32e11ae114913bced248498c6dfa2d
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2020-12-24 17:36:57 +0100

    Rework the TrackSpeedStatisticsAnalyzer.
    - Use 'speed' instead of 'velocity'. Several people indicated
    that this was a mistake since velocity is a vector. Someone
    told me it induces confusion in the field.
    - Simplify.

--- src/main/java/fiji/plugin/trackmate/features/track/TrackSpeedStatisticsAnalyzer.java
+++ src/main/java/fiji/plugin/trackmate/features/track/TrackSpeedStatisticsAnalyzer.java
@@ -10,9 +10,6 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 import javax.swing.ImageIcon;
 
-import net.imglib2.multithreading.SimpleMultiThreading;
-import net.imglib2.util.Util;
-
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.scijava.plugin.Plugin;
 
@@ -20,6 +17,9 @@ import fiji.plugin.trackmate.Dimension;
 import fiji.plugin.trackmate.FeatureModel;
 import fiji.plugin.trackmate.Model;
 import fiji.plugin.trackmate.Spot;
+import fiji.plugin.trackmate.util.TMUtils;
+import net.imglib2.multithreading.SimpleMultiThreading;
+import net.imglib2.util.Util;
 
 @SuppressWarnings( "deprecation" )
 @Plugin( type = TrackAnalyzer.class )
@@ -29,29 +29,18 @@ public class TrackSpeedStatisticsAnalyzer implements TrackAnalyzer
 	/*
 	 * CONSTANTS
 	 */
-	public static final String KEY = "Velocity";
+	public static final String KEY = "Track speed";
 
 	public static final String TRACK_MEAN_SPEED = "TRACK_MEAN_SPEED";
-
 	public static final String TRACK_MAX_SPEED = "TRACK_MAX_SPEED";
-
 	public static final String TRACK_MIN_SPEED = "TRACK_MIN_SPEED";
-
 	public static final String TRACK_MEDIAN_SPEED = "TRACK_MEDIAN_SPEED";
-
 	public static final String TRACK_STD_SPEED = "TRACK_STD_SPEED";
 
-	// public static final String TRACK_SPEED_KURTOSIS = "TRACK_SPEED_KURTOSIS";
-	// public static final String TRACK_SPEED_SKEWNESS = "TRACK_SPEED_SKEWNESS";
-
 	public static final List< String > FEATURES = new ArrayList< >( 5 );
-
 	public static final Map< String, String > FEATURE_NAMES = new HashMap< >( 5 );
-
 	public static final Map< String, String > FEATURE_SHORT_NAMES = new HashMap< >( 5 );
-
 	public static final Map< String, Dimension > FEATURE_DIMENSIONS = new HashMap< >( 5 );
-
 	public static final Map< String, Boolean > IS_INT = new HashMap< >( 5 );
 
 	static
@@ -61,40 +50,30 @@ public class TrackSpeedStatisticsAnalyzer implements TrackAnalyzer
 		FEATURES.add( TRACK_MIN_SPEED );
 		FEATURES.add( TRACK_MEDIAN_SPEED );
 		FEATURES.add( TRACK_STD_SPEED );
-		// FEATURES.add(TRACK_SPEED_KURTOSIS);
-		// FEATURES.add(TRACK_SPEED_SKEWNESS);
 
-		FEATURE_NAMES.put( TRACK_MEAN_SPEED, "Mean velocity" );
-		FEATURE_NAMES.put( TRACK_MAX_SPEED, "Maximal velocity" );
-		FEATURE_NAMES.put( TRACK_MIN_SPEED, "Minimal velocity" );
-		FEATURE_NAMES.put( TRACK_MEDIAN_SPEED, "Median velocity" );
-		FEATURE_NAMES.put( TRACK_STD_SPEED, "Velocity standard deviation" );
-		// FEATURE_NAMES.put(TRACK_SPEED_KURTOSIS, "Velocity kurtosis");
-		// FEATURE_NAMES.put(TRACK_SPEED_SKEWNESS, "Velocity skewness");
+		FEATURE_NAMES.put( TRACK_MEAN_SPEED, "Track mean speed" );
+		FEATURE_NAMES.put( TRACK_STD_SPEED, "Track std speed" );
+		FEATURE_NAMES.put( TRACK_MAX_SPEED, "Track max speed" );
+		FEATURE_NAMES.put( TRACK_MIN_SPEED, "Track min speed" );
+		FEATURE_NAMES.put( TRACK_MEDIAN_SPEED, "Track median speed" );
 
-		FEATURE_SHORT_NAMES.put( TRACK_MEAN_SPEED, "Mean V" );
-		FEATURE_SHORT_NAMES.put( TRACK_MAX_SPEED, "Max V" );
-		FEATURE_SHORT_NAMES.put( TRACK_MIN_SPEED, "Min V" );
-		FEATURE_SHORT_NAMES.put( TRACK_MEDIAN_SPEED, "Median V" );
-		FEATURE_SHORT_NAMES.put( TRACK_STD_SPEED, "V std" );
-		// FEATURE_SHORT_NAMES.put(TRACK_SPEED_KURTOSIS, "V kurtosis");
-		// FEATURE_SHORT_NAMES.put(TRACK_SPEED_SKEWNESS, "V skewness");
+		FEATURE_SHORT_NAMES.put( TRACK_MEAN_SPEED, "Mean speed" );
+		FEATURE_SHORT_NAMES.put( TRACK_MAX_SPEED, "Max speed" );
+		FEATURE_SHORT_NAMES.put( TRACK_MIN_SPEED, "Min speed" );
+		FEATURE_SHORT_NAMES.put( TRACK_MEDIAN_SPEED, "Median speed" );
+		FEATURE_SHORT_NAMES.put( TRACK_STD_SPEED, "Std speed" );
 
 		FEATURE_DIMENSIONS.put( TRACK_MEAN_SPEED, Dimension.VELOCITY );
 		FEATURE_DIMENSIONS.put( TRACK_MAX_SPEED, Dimension.VELOCITY );
 		FEATURE_DIMENSIONS.put( TRACK_MIN_SPEED, Dimension.VELOCITY );
 		FEATURE_DIMENSIONS.put( TRACK_MEDIAN_SPEED, Dimension.VELOCITY );
 		FEATURE_DIMENSIONS.put( TRACK_STD_SPEED, Dimension.VELOCITY );
-		// FEATURE_DIMENSIONS.put(TRACK_SPEED_KURTOSIS, Dimension.NONE);
-		// FEATURE_DIMENSIONS.put(TRACK_SPEED_SKEWNESS, Dimension.NONE);
 
 		IS_INT.put( TRACK_MEAN_SPEED, Boolean.FALSE );
 		IS_INT.put( TRACK_MAX_SPEED, Boolean.FALSE );
 		IS_INT.put( TRACK_MIN_SPEED, Boolean.FALSE );
 		IS_INT.put( TRACK_MEDIAN_SPEED, Boolean.FALSE );
 		IS_INT.put( TRACK_STD_SPEED, Boolean.FALSE );
-		// IS_INT.put(TRACK_SPEED_KURTOSIS, Boolean.FALSE);
-		// IS_INT.put(TRACK_SPEED_SKEWNESS, Boolean.FALSE);
 	}
 
 	private int numThreads;
@@ -139,74 +118,31 @@ public class TrackSpeedStatisticsAnalyzer implements TrackAnalyzer
 					{
 
 						final Set< DefaultWeightedEdge > track = model.getTrackModel().trackEdges( trackID );
-
-						double sum = 0;
-						double mean = 0;
-						double M2 = 0;
-						// double M3 = 0;
-						// double M4 = 0;
-						double delta, delta_n;
-						// double delta_n2;
-						double term1;
-						int n1;
-
-						// Others
-						Double val;
-						final double[] velocities = new double[ track.size() ];
+						final double[] speeds = new double[ track.size() ];
 						int n = 0;
-
 						for ( final DefaultWeightedEdge edge : track )
 						{
 							final Spot source = model.getTrackModel().getEdgeSource( edge );
 							final Spot target = model.getTrackModel().getEdgeTarget( edge );
-
-							// Edge velocity
 							final double d2 = source.squareDistanceTo( target );
 							final double dt = source.diffTo( target, Spot.POSITION_T );
-							val = Math.sqrt( d2 ) / Math.abs( dt );
-
-							// For median, min and max
-							velocities[ n ] = val;
-							// For variance and mean
-							sum += val;
-
-							// For kurtosis
-							n1 = n;
-							n++;
-							delta = val - mean;
-							delta_n = delta / n;
-							// delta_n2 = delta_n * delta_n;
-							term1 = delta * delta_n * n1;
-							mean = mean + delta_n;
-							// M4 = M4 + term1 * delta_n2 * (n*n - 3*n + 3) + 6
-							// * delta_n2 * M2 - 4 * delta_n * M3;
-							// M3 = M3 + term1 * delta_n * (n - 2) - 3 * delta_n
-							// * M2;
-							M2 = M2 + term1;
+							final double val = Math.sqrt( d2 ) / Math.abs( dt );
+							speeds[ n++ ] = val;
 						}
 
-						Util.quicksort( velocities, 0, track.size() - 1 );
-						final double median = velocities[ track.size() / 2 ];
-						final double min = velocities[ 0 ];
-						final double max = velocities[ track.size() - 1 ];
-						mean = sum / track.size();
-						final double variance = M2 / ( track.size() - 1 );
-						// double kurtosis = (n*M4) / (M2*M2) - 3;
-						// double skewness = Math.sqrt(n) * M3 / Math.pow(M2,
-						// 3/2.0) ;
+						Util.quicksort( speeds, 0, track.size() - 1 );
+						final double median = speeds[ track.size() / 2 ];
+						final double min = speeds[ 0 ];
+						final double max = speeds[ track.size() - 1 ];
+						final double mean = Util.average( speeds );
+						final double std = TMUtils.standardDeviation( speeds );
 
 						fm.putTrackFeature( trackID, TRACK_MEDIAN_SPEED, median );
 						fm.putTrackFeature( trackID, TRACK_MIN_SPEED, min );
 						fm.putTrackFeature( trackID, TRACK_MAX_SPEED, max );
 						fm.putTrackFeature( trackID, TRACK_MEAN_SPEED, mean );
-						fm.putTrackFeature( trackID, TRACK_STD_SPEED, Math.sqrt( variance ) );
-						// fm.putTrackFeature(index, TRACK_SPEED_KURTOSIS,
-						// kurtosis);
-						// fm.putTrackFeature(index, TRACK_SPEED_SKEWNESS,
-						// skewness);
-
+						fm.putTrackFeature( trackID, TRACK_STD_SPEED, std );
 					}
-
 				}
 			};
 		}
@@ -233,7 +169,6 @@ public class TrackSpeedStatisticsAnalyzer implements TrackAnalyzer
 	public void setNumThreads( final int numThreads )
 	{
 		this.numThreads = numThreads;
-
 	}
 
 	@Override
