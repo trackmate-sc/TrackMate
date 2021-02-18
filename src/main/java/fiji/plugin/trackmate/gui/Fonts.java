52746e1e9d981884c468f1f7519d6808b7572ba5
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2021-02-18 23:52:24 +0100

    Common class to centralize fonts and other GUI related constants.

---
+++ src/main/java/fiji/plugin/trackmate/gui/Fonts.java
@@ -0,0 +1,15 @@
+package fiji.plugin.trackmate.gui;
+
+import java.awt.Dimension;
+import java.awt.Font;
+
+public class Fonts
+{
+	public static final Font FONT = new Font( "Arial", Font.PLAIN, 10 );
+
+	public static final Font BIG_FONT = new Font( "Arial", Font.PLAIN, 14 );
+
+	public static final Font SMALL_FONT = FONT.deriveFont( 8 );
+
+	public static final Dimension TEXTFIELD_DIMENSION = new Dimension( 40, 18 );
+}
