68d47022564b9347635f3a60652a18bc8afba443
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2021-02-19 18:02:15 +0100

    Log the results of tracking and quick track statistics.

--- src/main/java/fiji/plugin/trackmate/gui/wizard/descriptors/ExecuteTrackingDescriptor.java
+++ src/main/java/fiji/plugin/trackmate/gui/wizard/descriptors/ExecuteTrackingDescriptor.java
@@ -1,8 +1,12 @@
 package fiji.plugin.trackmate.gui.wizard.descriptors;
 
+import java.util.IntSummaryStatistics;
+
 import org.scijava.Cancelable;
 
+import fiji.plugin.trackmate.Logger;
 import fiji.plugin.trackmate.TrackMate;
+import fiji.plugin.trackmate.TrackModel;
 import fiji.plugin.trackmate.gui.components.LogPanel;
 import fiji.plugin.trackmate.gui.wizard.WizardPanelDescriptor;
 
@@ -27,7 +31,18 @@ public class ExecuteTrackingDescriptor extends WizardPanelDescriptor
 			final long start = System.currentTimeMillis();
 			trackmate.execTracking();
 			final long end = System.currentTimeMillis();
-			trackmate.getModel().getLogger().log( String.format( "Tracking done in %.1f s.\n", ( end - start ) / 1e3f ) );
+
+			final Logger logger = trackmate.getModel().getLogger();
+			logger.log( String.format( "Tracking done in %.1f s.\n", ( end - start ) / 1e3f ) );
+			final TrackModel trackModel = trackmate.getModel().getTrackModel();
+			final int nTracks = trackModel.nTracks( false );
+			final IntSummaryStatistics stats = trackModel.unsortedTrackIDs( false ).stream()
+					.mapToInt( id -> trackModel.trackSpots( id ).size() )
+					.summaryStatistics();
+			logger.log( "Found " + nTracks + " tracks.\n" );
+			logger.log( String.format( "  - avg size: %.1f spots.\n", stats.getAverage() ) );
+			logger.log( String.format( "  - min size: %d spots.\n", stats.getMin() ) );
+			logger.log( String.format( "  - max size: %d spots.\n", stats.getMax() ) );
 		};
 	}
 
