a5a742cd77359e46444bde4799dc68bb510cfbcd
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2021-02-19 17:40:53 +0100

    Hack the SpotFilterDescriptor to show a message while we compute features.

--- src/main/java/fiji/plugin/trackmate/gui/wizard/descriptors/SpotFilterDescriptor.java
+++ src/main/java/fiji/plugin/trackmate/gui/wizard/descriptors/SpotFilterDescriptor.java
@@ -1,7 +1,11 @@
 package fiji.plugin.trackmate.gui.wizard.descriptors;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.swing.JLabel;
 
 import fiji.plugin.trackmate.Logger;
 import fiji.plugin.trackmate.Model;
@@ -16,6 +20,7 @@ import fiji.plugin.trackmate.gui.components.FilterGuiPanel;
 import fiji.plugin.trackmate.gui.displaysettings.DisplaySettings.TrackMateObject;
 import fiji.plugin.trackmate.gui.wizard.WizardPanelDescriptor;
 import fiji.plugin.trackmate.providers.SpotMorphologyAnalyzerProvider;
+import fiji.plugin.trackmate.util.EverythingDisablerAndReenabler;
 
 public class SpotFilterDescriptor extends WizardPanelDescriptor
 {
@@ -58,55 +63,76 @@ public class SpotFilterDescriptor extends WizardPanelDescriptor
 			@Override
 			public void run()
 			{
-				final Model model = trackmate.getModel();
-				final Logger logger = model.getLogger();
-				final String str = "Initial thresholding with a quality threshold above "
-						+ String.format( "%.1f", trackmate.getSettings().initialSpotFilterValue )
-						+ " ...\n";
-				logger.log( str, Logger.BLUE_COLOR );
-				final int ntotal = model.getSpots().getNSpots( false );
-				trackmate.execInitialSpotFiltering();
-				final int nselected = model.getSpots().getNSpots( false );
-				logger.log( String.format( "Retained %d spots out of %d.\n", nselected, ntotal ) );
-
-				/*
-				 * Should we add morphology feature analyzers?
-				 */
-
-				if ( trackmate.getSettings().detectorFactory != null
-						&& trackmate.getSettings().detectorFactory.has2Dsegmentation()
-						&& DetectionUtils.is2D( trackmate.getSettings().imp ) )
+				final EverythingDisablerAndReenabler disabler = new EverythingDisablerAndReenabler( ( Container ) targetPanel, new Class[] { JLabel.class } );
+				disabler.disable();
+				try
 				{
-					logger.log( "\nAdding morphology analyzers...\n", Logger.BLUE_COLOR );
-					final Settings settings = trackmate.getSettings();
-					final SpotMorphologyAnalyzerProvider spotMorphologyAnalyzerProvider = new SpotMorphologyAnalyzerProvider( settings.imp.getNChannels() );
-					@SuppressWarnings( "rawtypes" )
-					final List< SpotMorphologyAnalyzerFactory > factories = spotMorphologyAnalyzerProvider
-							.getKeys()
-							.stream()
-							.map( key -> spotMorphologyAnalyzerProvider.getFactory( key ) )
-							.collect( Collectors.toList() );
-					factories.forEach( settings::addSpotAnalyzerFactory );
-					final StringBuilder strb = new StringBuilder();
-					Settings.prettyPrintFeatureAnalyzer( factories, strb );
-					logger.log( strb.toString() );
+
+					final Model model = trackmate.getModel();
+					final Logger logger = model.getLogger();
+					final String str = "Initial thresholding with a quality threshold above "
+							+ String.format( "%.1f", trackmate.getSettings().initialSpotFilterValue )
+							+ " ...\n";
+					logger.log( str, Logger.BLUE_COLOR );
+					final int ntotal = model.getSpots().getNSpots( false );
+					trackmate.execInitialSpotFiltering();
+					final int nselected = model.getSpots().getNSpots( false );
+					logger.log( String.format( "Retained %d spots out of %d.\n", nselected, ntotal ) );
+
+					/*
+					 * Should we add morphology feature analyzers?
+					 */
+
+					if ( trackmate.getSettings().detectorFactory != null
+							&& trackmate.getSettings().detectorFactory.has2Dsegmentation()
+							&& DetectionUtils.is2D( trackmate.getSettings().imp ) )
+					{
+						logger.log( "\nAdding morphology analyzers...\n", Logger.BLUE_COLOR );
+						final Settings settings = trackmate.getSettings();
+						final SpotMorphologyAnalyzerProvider spotMorphologyAnalyzerProvider = new SpotMorphologyAnalyzerProvider( settings.imp.getNChannels() );
+						@SuppressWarnings( "rawtypes" )
+						final List< SpotMorphologyAnalyzerFactory > factories = spotMorphologyAnalyzerProvider
+								.getKeys()
+								.stream()
+								.map( key -> spotMorphologyAnalyzerProvider.getFactory( key ) )
+								.collect( Collectors.toList() );
+						factories.forEach( settings::addSpotAnalyzerFactory );
+						final StringBuilder strb = new StringBuilder();
+						Settings.prettyPrintFeatureAnalyzer( factories, strb );
+						logger.log( strb.toString() );
+					}
+
+					/*
+					 * Hack to show a message in the filter GUI panel.
+					 */
+
+					final FilterGuiPanel panel = ( FilterGuiPanel ) targetPanel;
+					final BorderLayout layout = ( BorderLayout ) panel.getLayout();
+					final JLabel labelTop = ( JLabel ) layout.getLayoutComponent( BorderLayout.NORTH );
+					final String originalText = labelTop.getText();
+					labelTop.setText( "  Please wait while computing spot features..." );
+
+					/*
+					 * We have some spots so we need to compute spot features
+					 * will we render them.
+					 */
+					logger.log( "\nCalculating spot features...\n", Logger.BLUE_COLOR );
+					// Calculate features
+					final long start = System.currentTimeMillis();
+					trackmate.computeSpotFeatures( true );
+					final long end = System.currentTimeMillis();
+					logger.log( String.format( "Calculating features done in %.1f s.\n", ( end - start ) / 1e3f ) );
+					labelTop.setText( originalText );
+
+					// Refresh component.
+					final FilterGuiPanel component = ( FilterGuiPanel ) targetPanel;
+					component.refreshValues();
+					filterSpots();
 				}
-
-				/*
-				 * We have some spots so we need to compute spot features will
-				 * we render them.
-				 */
-				logger.log( "\nCalculating spot features...\n", Logger.BLUE_COLOR );
-				// Calculate features
-				final long start = System.currentTimeMillis();
-				trackmate.computeSpotFeatures( true );
-				final long end = System.currentTimeMillis();
-				logger.log( String.format( "Calculating features done in %.1f s.\n", ( end - start ) / 1e3f ) );
-
-				// Refresh component.
-				final FilterGuiPanel component = ( FilterGuiPanel ) targetPanel;
-				component.refreshValues();
-				filterSpots();
+				finally
+				{
+					disabler.reenable();
+				}
 			}
 		};
 	}
