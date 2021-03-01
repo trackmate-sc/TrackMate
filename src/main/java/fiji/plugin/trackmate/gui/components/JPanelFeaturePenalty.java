ffb21c965b1486183b1686b755e2586cf4b3f97f
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2021-03-01 10:24:43 +0100

    Code style changes.

--- src/main/java/fiji/plugin/trackmate/gui/components/JPanelFeaturePenalty.java
+++ src/main/java/fiji/plugin/trackmate/gui/components/JPanelFeaturePenalty.java
@@ -3,6 +3,7 @@ package fiji.plugin.trackmate.gui.components;
 import static fiji.plugin.trackmate.gui.Fonts.SMALL_FONT;
 import static fiji.plugin.trackmate.gui.Fonts.TEXTFIELD_DIMENSION;
 
+import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Map;
 
@@ -17,23 +18,43 @@ import fiji.plugin.trackmate.util.TMUtils;
 public class JPanelFeaturePenalty extends javax.swing.JPanel
 {
 
-	private static final long serialVersionUID = 3848390144561204540L;
+	private static final long serialVersionUID = 1L;
 
-	private JComboBox< String > jComboBoxFeature;
+	private final JComboBox< String > cmbboxFeature;
 
-	private JFormattedTextField jTextFieldFeatureWeight;
+	private final JFormattedTextField txtfldFeatureWeight;
 
 	private final List< String > features;
 
-	private final Map< String, String > featureNames;
-
 	public JPanelFeaturePenalty( final List< String > features, final Map< String, String > featureNames, final int index )
 	{
-		super();
 		this.features = features;
-		this.featureNames = featureNames;
-		initGUI();
-		jComboBoxFeature.setSelectedIndex( index );
+
+		this.setPreferredSize( new java.awt.Dimension( 280, 40 ) );
+		this.setSize( 280, 40 );
+		this.setLayout( null );
+
+		final ComboBoxModel< String > jComboBoxFeatureModel = new DefaultComboBoxModel<>(
+				TMUtils.getArrayFromMaping( features, featureNames ).toArray( new String[] {} ) );
+		cmbboxFeature = new JComboBox<>();
+		this.add( cmbboxFeature );
+		cmbboxFeature.setModel( jComboBoxFeatureModel );
+		cmbboxFeature.setBounds( 2, 4, 205, 22 );
+		cmbboxFeature.setFont( SMALL_FONT );
+
+		txtfldFeatureWeight = new JFormattedTextField( new DecimalFormat( "0.0" ) );
+		txtfldFeatureWeight.setHorizontalAlignment( JFormattedTextField.CENTER );
+		txtfldFeatureWeight.setValue( Double.valueOf( 1. ) );
+		this.add( txtfldFeatureWeight );
+		txtfldFeatureWeight.setBounds( 220, 4, 30, 22 );
+		txtfldFeatureWeight.setSize( TEXTFIELD_DIMENSION );
+		txtfldFeatureWeight.setFont( SMALL_FONT );
+
+		// Select text-field content on focus.
+		GuiUtils.selectAllOnFocus( txtfldFeatureWeight );
+
+		// Select default.
+		cmbboxFeature.setSelectedIndex( index );
 	}
 
 	/*
@@ -43,63 +64,27 @@ public class JPanelFeaturePenalty extends javax.swing.JPanel
 	public void setSelectedFeature( final String feature, final double weight )
 	{
 		final int index = features.indexOf( feature );
-		if ( index < 0 ) { return; }
-		jComboBoxFeature.setSelectedIndex( index );
-		jTextFieldFeatureWeight.setValue( Double.valueOf( weight ) );
+		if ( index < 0 )
+		{ return; }
+		cmbboxFeature.setSelectedIndex( index );
+		txtfldFeatureWeight.setValue( Double.valueOf( weight ) );
 	}
 
 	public String getSelectedFeature()
 	{
-		return features.get( jComboBoxFeature.getSelectedIndex() );
+		return features.get( cmbboxFeature.getSelectedIndex() );
 	}
 
 	public double getPenaltyWeight()
 	{
-		return ( ( Number ) jTextFieldFeatureWeight.getValue() ).doubleValue();
+		return ( ( Number ) txtfldFeatureWeight.getValue() ).doubleValue();
 	}
 
 	@Override
 	public void setEnabled( final boolean enabled )
 	{
 		super.setEnabled( enabled );
-		jComboBoxFeature.setEnabled( enabled );
-		jTextFieldFeatureWeight.setEnabled( enabled );
-	}
-
-	/*
-	 * PRIVATE METHODS
-	 */
-
-	private void initGUI()
-	{
-		try
-		{
-			this.setPreferredSize( new java.awt.Dimension( 280, 40 ) );
-			this.setSize( 280, 40 );
-			this.setLayout( null );
-			{
-				final ComboBoxModel< String > jComboBoxFeatureModel = new DefaultComboBoxModel< >(
-						TMUtils.getArrayFromMaping( features, featureNames ).toArray( new String[] {} ) );
-				jComboBoxFeature = new JComboBox< >();
-				this.add( jComboBoxFeature );
-				jComboBoxFeature.setModel( jComboBoxFeatureModel );
-				jComboBoxFeature.setBounds( 2, 4, 205, 22 );
-				jComboBoxFeature.setFont( SMALL_FONT );
-			}
-			{
-				jTextFieldFeatureWeight = new JFormattedTextField( 1.0 );
-				this.add( jTextFieldFeatureWeight );
-				jTextFieldFeatureWeight.setBounds( 220, 4, 30, 22 );
-				jTextFieldFeatureWeight.setSize( TEXTFIELD_DIMENSION );
-				jTextFieldFeatureWeight.setFont( SMALL_FONT );
-			}
-
-			// Select text-field content on focus.
-			GuiUtils.selectAllOnFocus( jTextFieldFeatureWeight );
-		}
-		catch ( final Exception e )
-		{
-			e.printStackTrace();
-		}
+		cmbboxFeature.setEnabled( enabled );
+		txtfldFeatureWeight.setEnabled( enabled );
 	}
 }
