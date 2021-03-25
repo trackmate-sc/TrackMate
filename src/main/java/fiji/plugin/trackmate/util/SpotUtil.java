3b47e88613166e022dd9e3dde6d3c9b9819cd361
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2021-03-25 19:46:33 +0100

    Implement a specific iteration code for 2d polygons.
    I noticed than the repvious code (roi.sample) was really
    slow. I them tried to reimplement it in our special case (2D closed
    simple polygons). It get a 25x speed factor, but only provided I do
    not test the edge case when determining whether a point is inside the
    polygon. I still have to determine whether this is important for our
    case.

--- src/main/java/fiji/plugin/trackmate/util/SpotUtil.java
+++ src/main/java/fiji/plugin/trackmate/util/SpotUtil.java
@@ -1,13 +1,22 @@
 package fiji.plugin.trackmate.util;
 
+import java.util.Iterator;
+
 import fiji.plugin.trackmate.Spot;
 import fiji.plugin.trackmate.SpotRoi;
 import fiji.plugin.trackmate.detection.DetectionUtils;
 import net.imagej.ImgPlus;
+import net.imglib2.Cursor;
 import net.imglib2.FinalInterval;
 import net.imglib2.Interval;
 import net.imglib2.IterableInterval;
+import net.imglib2.Localizable;
+import net.imglib2.RandomAccess;
+import net.imglib2.Sampler;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Intervals;
+import net.imglib2.util.Util;
+import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 
 public class SpotUtil
@@ -21,7 +30,7 @@ public class SpotUtil
 		if ( null != roi && DetectionUtils.is2D( img ) )
 		{
 			// Operate on ROI only if we have one and the image is 2D.
-			neighborhood = roi.sample( spot, img );
+			neighborhood = new SpotRoiIterable<>( spot, img );
 		}
 		else
 		{
@@ -48,6 +57,247 @@ public class SpotUtil
 		else
 		{
 			return neighborhood;
+		}
+	}
+
+	private static final class SpotRoiIterable< T > implements IterableInterval< T >
+	{
+
+		private final FinalInterval interval;
+
+		private final Spot spot;
+
+		private final ImgPlus< T > img;
+
+		public SpotRoiIterable( final Spot spot, final ImgPlus< T > img )
+		{
+			this.spot = spot;
+			this.img = img;
+			final SpotRoi roi = spot.getRoi();
+			final double[] x = roi.toPolygonX( img.averageScale( 0 ), 0, spot.getDoublePosition( 0 ), 1. );
+			final double[] y = roi.toPolygonX( img.averageScale( 1 ), 0, spot.getDoublePosition( 1 ), 1. );
+			final long minX = ( long ) Math.floor( Util.min( x ) );
+			final long maxX = ( long ) Math.ceil( Util.max( x ) );
+			final long minY = ( long ) Math.floor( Util.min( y ) );
+			final long maxY = ( long ) Math.ceil( Util.max( y ) );
+			interval = Intervals.createMinMax( minX, minY, maxX, maxY );
+		}
+
+		@Override
+		public long size()
+		{
+			int n = 0;
+			final Cursor< T > cursor = cursor();
+			while ( cursor.hasNext() )
+			{
+				cursor.fwd();
+				n++;
+			}
+			return n;
+		}
+
+		@Override
+		public T firstElement()
+		{
+			return cursor().next();
+		}
+
+		@Override
+		public Object iterationOrder()
+		{
+			return this;
+		}
+
+		@Override
+		public double realMin( final int d )
+		{
+			return interval.realMin( d );
+		}
+
+		@Override
+		public double realMax( final int d )
+		{
+			return interval.realMax( d );
+		}
+
+		@Override
+		public int numDimensions()
+		{
+			return 2;
+		}
+
+		@Override
+		public long min( final int d )
+		{
+			return interval.min( d );
+		}
+
+		@Override
+		public long max( final int d )
+		{
+			return interval.max( d );
+		}
+
+		@Override
+		public Cursor< T > cursor()
+		{
+			return new MyCursor< T >( spot, img );
+		}
+
+		@Override
+		public Cursor< T > localizingCursor()
+		{
+			return cursor();
+		}
+
+		@Override
+		public Iterator< T > iterator()
+		{
+			return cursor();
+		}
+	}
+
+	private static final class MyCursor< T > implements Cursor< T >
+	{
+
+		private final Spot spot;
+
+		private final ImgPlus< T > img;
+
+		private final FinalInterval interval;
+
+		private Cursor< T > cursor;
+
+		private final double[] x;
+
+		private final double[] y;
+
+		private boolean hasNext;
+
+		private RandomAccess< T > ra;
+
+		public MyCursor( final Spot spot, final ImgPlus< T > img )
+		{
+			this.spot = spot;
+			this.img = img;
+			final SpotRoi roi = spot.getRoi();
+			x = roi.toPolygonX( img.averageScale( 0 ), 0, spot.getDoublePosition( 0 ), 1. );
+			y = roi.toPolygonY( img.averageScale( 1 ), 0, spot.getDoublePosition( 1 ), 1. );
+			final long minX = ( long ) Math.floor( Util.min( x ) );
+			final long maxX = ( long ) Math.ceil( Util.max( x ) );
+			final long minY = ( long ) Math.floor( Util.min( y ) );
+			final long maxY = ( long ) Math.ceil( Util.max( y ) );
+			interval = Intervals.createMinMax( minX, minY, maxX, maxY );
+			reset();
+		}
+
+		@Override
+		public T get()
+		{
+			return ra.get();
+		}
+
+		@Override
+		public void fwd()
+		{
+			ra.setPosition( cursor );
+			fetch();
+		}
+
+		private void fetch()
+		{
+			while ( cursor.hasNext() )
+			{
+				cursor.fwd();
+				if ( isInside( cursor, x, y ) )
+				{
+					hasNext = cursor.hasNext();
+					return;
+				}
+			}
+			hasNext = false;
+		}
+
+		private static final boolean isInside( final Localizable localizable, final double[] x, final double[] y )
+		{
+			// Taken from Imglib2-roi GeomMaths. No edge case.
+			final double xl = localizable.getDoublePosition( 0 );
+			final double yl = localizable.getDoublePosition( 1 );
+
+			int i;
+			int j;
+			boolean inside = false;
+			for ( i = 0, j = x.length - 1; i < x.length; j = i++ )
+			{
+				final double xj = x[ j ];
+				final double yj = y[ j ];
+
+				final double xi = x[ i ];
+				final double yi = y[ i ];
+
+				if ( ( yi > yl ) != ( yj > yl ) && ( xl < ( xj - xi ) * ( yl - yi ) / ( yj - yi ) + xi ) )
+					inside = !inside;
+			}
+			return inside;
+		}
+
+		@Override
+		public void reset()
+		{
+			final IntervalView< T > view = Views.interval( img, interval );
+			cursor = view.localizingCursor();
+			ra = view.randomAccess( interval );
+			fetch();
+		}
+
+		@Override
+		public double getDoublePosition( final int d )
+		{
+			return ra.getDoublePosition( d );
+		}
+
+		@Override
+		public int numDimensions()
+		{
+			return 2;
+		}
+
+		@Override
+		public void jumpFwd( final long steps )
+		{
+			for ( int i = 0; i < steps; i++ )
+				fwd();
+		}
+
+		@Override
+		public boolean hasNext()
+		{
+			return hasNext;
+		}
+
+		@Override
+		public T next()
+		{
+			fwd();
+			return get();
+		}
+
+		@Override
+		public long getLongPosition( final int d )
+		{
+			return ra.getLongPosition( d );
+		}
+
+		@Override
+		public Cursor< T > copyCursor()
+		{
+			return new MyCursor<>( spot, img );
+		}
+
+		@Override
+		public Sampler< T > copy()
+		{
+			return copyCursor();
 		}
 	}
 }
---
+++ src/test/java/fiji/plugin/trackmate/util/SpotRoiIterableTest.java
@@ -0,0 +1,101 @@
+package fiji.plugin.trackmate.util;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import fiji.plugin.trackmate.Spot;
+import fiji.plugin.trackmate.SpotRoi;
+import gnu.trove.list.array.TIntArrayList;
+import net.imagej.ImgPlus;
+import net.imglib2.Cursor;
+import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccess;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+
+public class SpotRoiIterableTest
+{
+
+	@Test
+	public void testIterationPolygon()
+	{
+		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 10, 10 );
+		int val = 0;
+		for ( final UnsignedByteType p : img )
+			p.set( val++ );
+
+		final double[] xp = new double[] { 4.5, 8.5, 4.5, 1.5 };
+		final double[] yp = new double[] { 1.5, 5, 8.8, 5 };
+		final Spot spot = SpotRoi.createSpot( xp, yp, 1. );
+
+		final int[] iteratedPixels = new int[] {
+				25, 34, 35, 36, 43, 44, 45, 46,
+				47, 52, 53, 54, 55, 56, 57, 58,
+				63, 64, 65, 66, 67, 74, 75, 76,
+				84, 85 };
+
+		final TIntArrayList vals = new TIntArrayList();
+		final IterableInterval< UnsignedByteType > iterable = SpotUtil.iterable( spot, new ImgPlus<>( img ) );
+		final Cursor< UnsignedByteType > cursor = iterable.cursor();
+		while ( cursor.hasNext() )
+		{
+			cursor.fwd();
+			vals.add( cursor.get().get() );
+		}
+		final int[] arr = vals.toArray();
+		Arrays.sort( arr );
+
+		assertArrayEquals( "Cursor did not iterate the expected area.", iteratedPixels, arr );
+	}
+
+	public static void main( final String[] args )
+	{
+		final Img< UnsignedByteType > img = ArrayImgs.unsignedBytes( 10, 10 );
+		int val = 0;
+		for ( final UnsignedByteType p : img )
+			p.set( val++ );
+
+		final double[] xp = new double[] { 4.5, 8.5, 4.5, 1.5 };
+		final double[] yp = new double[] { 1.5, 5, 8.8, 5 };
+		final Spot spot = SpotRoi.createSpot( xp, yp, 1. );
+
+		final IterableInterval< UnsignedByteType > iterable = SpotUtil.iterable( spot, new ImgPlus<>( img ) );
+		final Cursor< UnsignedByteType > cursor = iterable.cursor();
+		while ( cursor.hasNext() )
+		{
+			cursor.fwd();
+			System.out.println( String.format( "%d", cursor.get().get() ) );
+//			System.out.println( String.format( "  (%3d, %3d ) -> %.0f",
+//					cursor.getIntPosition( 0 ),
+//					cursor.getIntPosition( 1 ),
+//					cursor.get().getRealDouble() ) );
+			cursor.get().set( 0 );
+		}
+		System.out.println( printImg( img ) );
+
+	}
+
+	private static final < T extends RealType< T > > String printImg( final Img< T > img )
+	{
+		final StringBuilder str = new StringBuilder( '\n' );
+		final RandomAccess< T > ra = img.randomAccess( img );
+
+		for ( int y = 0; y < img.dimension( 0 ); y++ )
+		{
+			ra.setPosition( y, 1 );
+			for ( int x = 0; x < img.dimension( 0 ); x++ )
+			{
+				ra.setPosition( x, 0 );
+				str.append( String.format( "%4.0f ", ra.get().getRealDouble() ) );
+			}
+			str.append( '\n' );
+		}
+		return str.toString();
+	}
+
+}
