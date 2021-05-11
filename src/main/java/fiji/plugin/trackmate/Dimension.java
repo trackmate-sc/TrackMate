a30de625d2b09c45ab8fea392bd51097ebffb8d0
Jean-Yves Tinevez <jeanyves.tinevez@gmail.com>
2021-05-11 11:41:57 +0200

    Make future proof: Don't crash when we load a TrackMate v7 file.
    By incorporating the future feature dimensions we make it possible
    for TrackMate series 6 to load TrackMate v7 files, and have the
    features specific to v7 usable in v6.

--- src/main/java/fiji/plugin/trackmate/Dimension.java
+++ src/main/java/fiji/plugin/trackmate/Dimension.java
@@ -3,13 +3,16 @@ package fiji.plugin.trackmate;
 public enum Dimension {
 	NONE,
 	QUALITY,
+	COST,
 	INTENSITY,
 	INTENSITY_SQUARED,
 	POSITION,
 	VELOCITY,
 	LENGTH,   // we separate length and position so that x,y,z are plotted on a different graph from spot sizes
+	AREA,
 	TIME,
 	ANGLE,
 	RATE, // count per frames
+	ANGLE_RATE,
 	STRING; // for non-numeric features
 }
