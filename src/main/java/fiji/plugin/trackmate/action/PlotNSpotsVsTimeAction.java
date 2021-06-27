9a4f72b64f5c0cbab9e51e1a1bf433c099af952d
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2021-06-27 22:39:13 +0200

    Rework the N spots vs Time action.
    Reuse what we developed for the grapher framework.
    It is a bit of a hack though.

--- src/main/java/fiji/plugin/trackmate/action/PlotNSpotsVsTimeAction.java
+++ src/main/java/fiji/plugin/trackmate/action/PlotNSpotsVsTimeAction.java
@@ -21,86 +21,179 @@
  */
 package fiji.plugin.trackmate.action;
 
-import static fiji.plugin.trackmate.gui.Fonts.FONT;
-import static fiji.plugin.trackmate.gui.Fonts.SMALL_FONT;
 import static fiji.plugin.trackmate.gui.Icons.PLOT_ICON;
 
 import java.awt.Frame;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.xy.DefaultXYDataset;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.scijava.plugin.Plugin;
 
+import fiji.plugin.trackmate.Dimension;
 import fiji.plugin.trackmate.Model;
 import fiji.plugin.trackmate.SelectionModel;
 import fiji.plugin.trackmate.Settings;
 import fiji.plugin.trackmate.Spot;
 import fiji.plugin.trackmate.SpotCollection;
 import fiji.plugin.trackmate.TrackMate;
+import fiji.plugin.trackmate.features.AbstractFeatureGrapher;
+import fiji.plugin.trackmate.features.ModelDataset;
+import fiji.plugin.trackmate.gui.GuiUtils;
+import fiji.plugin.trackmate.gui.Icons;
 import fiji.plugin.trackmate.gui.displaysettings.DisplaySettings;
-import fiji.plugin.trackmate.util.ExportableChartPanel;
 
-public class PlotNSpotsVsTimeAction extends AbstractTMAction {
+public class PlotNSpotsVsTimeAction extends AbstractTMAction
+{
 
 	public static final String NAME = "Plot N spots vs time";
 
 	public static final String KEY = "PLOT_NSPOTS_VS_TIME";
-	public static final String INFO_TEXT =  "<html>" +
+
+	public static final String INFO_TEXT = "<html>" +
 			"Plot the number of spots in each frame as a function <br>" +
 			"of time. Only the filtered spots are taken into account. " +
 			"</html>";
 
 	@Override
-	public void execute( final TrackMate trackmate, final SelectionModel selectionModel, final DisplaySettings displaySettings, final Frame parent )
+	public void execute(
+			final TrackMate trackmate,
+			final SelectionModel selectionModel,
+			final DisplaySettings displaySettings,
+			final Frame parent )
 	{
 		// Collect data
 		final Model model = trackmate.getModel();
 		final Settings settings = trackmate.getSettings();
 		final SpotCollection spots = model.getSpots();
-		final int nFrames = spots.keySet().size();
-		final double[][] data = new double[2][nFrames];
-		int index = 0;
-		for (final int frame : spots.keySet()) {
-			data[1][index] = spots.getNSpots(frame, true);
-			if (data[1][index] > 0) {
-				data[0][index] = spots.iterator(frame, false).next().getFeature(Spot.POSITION_T);
-			} else {
-				data[0][index] = frame * settings.dt;
-			}
-			index++;
+
+		final int maxFrame = spots.keySet().stream().mapToInt( Integer::intValue ).max().getAsInt();
+		final int[] nSpots = new int[ maxFrame + 1 ];
+		final double[] time = new double[ maxFrame + 1 ];
+
+		for ( int frame = 0; frame <= maxFrame; frame++ )
+		{
+			nSpots[ frame ] = spots.getNSpots( frame, true );
+			time[ frame ] = frame * settings.dt;
+		}
+		final NSpotPerFrameDataset dataset = new NSpotPerFrameDataset( model, selectionModel, displaySettings, time, nSpots );
+		final String yFeature = "N spots";
+		final Map< String, Dimension > dimMap = new HashMap<>( 2 );
+		dimMap.put( yFeature, Dimension.NONE );
+		dimMap.put( Spot.POSITION_T, Dimension.TIME );
+		final Map< String, String > nameMap = new HashMap<>( 2 );
+		nameMap.put( yFeature, yFeature );
+		nameMap.put( Spot.POSITION_T, "T" );
+
+		final NSpotPerFrameGrapher grapher = new NSpotPerFrameGrapher(
+				Spot.POSITION_T,
+				Collections.singletonList( "N spots" ),
+				Dimension.TIME,
+				dimMap,
+				nameMap,
+				model.getSpaceUnits(),
+				model.getTimeUnits(), dataset );
+
+		final JFrame frame = grapher.render();
+		frame.setIconImage( Icons.PLOT_ICON.getImage() );
+		frame.setTitle( "N spots per time-point" );
+		GuiUtils.positionWindow( frame, SwingUtilities.getWindowAncestor( parent ) );
+		frame.setVisible( true );
+	}
+
+	private static class NSpotPerFrameGrapher extends AbstractFeatureGrapher
+	{
+
+		private final NSpotPerFrameDataset dataset;
+
+		public NSpotPerFrameGrapher(
+				final String xFeature,
+				final List< String > yFeatures,
+				final Dimension xDimension,
+				final Map< String, Dimension > yDimensions,
+				final Map< String, String > featureNames,
+				final String spaceUnits,
+				final String timeUnits,
+				final NSpotPerFrameDataset dataset )
+		{
+			super( xFeature, yFeatures, xDimension, yDimensions, featureNames, spaceUnits, timeUnits );
+			this.dataset = dataset;
 		}
 
-		// Plot data
-		final String xAxisLabel = "Time ("+trackmate.getModel().getTimeUnits()+")";
-		final String yAxisLabel = "N spots";
-		final String title = "Nspots vs Time for "+trackmate.getSettings().imp.getShortTitle();
-		final DefaultXYDataset dataset = new DefaultXYDataset();
-		dataset.addSeries("Nspots", data);
+		@Override
+		protected ModelDataset buildMainDataSet( final List< String > targetYFeatures )
+		{
+			return dataset;
+		}
+	}
 
-		final JFreeChart chart = ChartFactory.createXYLineChart(title, xAxisLabel, yAxisLabel, dataset, PlotOrientation.VERTICAL, true, true, false);
-		chart.getTitle().setFont(FONT);
-		chart.getLegend().setItemFont(SMALL_FONT);
+	private static class NSpotPerFrameDataset extends ModelDataset
+	{
 
-		// The plot
-		final XYPlot plot = chart.getXYPlot();
-//		plot.setRenderer(0, pointRenderer);
-		plot.getRangeAxis().setLabelFont(FONT);
-		plot.getRangeAxis().setTickLabelFont(SMALL_FONT);
-		plot.getDomainAxis().setLabelFont(FONT);
-		plot.getDomainAxis().setTickLabelFont(SMALL_FONT);
+		private static final long serialVersionUID = 1L;
 
-		final ExportableChartPanel panel = new ExportableChartPanel(chart);
+		private final double[] time;
 
-		final JFrame frame = new JFrame(title);
-		frame.setSize(500, 270);
-		frame.getContentPane().add(panel);
-		frame.setVisible(true);
+		private final int[] nspots;
+
+		public NSpotPerFrameDataset(
+				final Model model,
+				final SelectionModel selectionModel,
+				final DisplaySettings ds,
+				final double[] time,
+				final int[] nspots )
+		{
+			super( model, selectionModel, ds, Spot.POSITION_T, Collections.singletonList( "N spots" ) );
+			this.time = time;
+			this.nspots = nspots;
+		}
+
+		@Override
+		public int getItemCount( final int series )
+		{
+			return nspots.length;
+		}
+
+		@Override
+		public Number getX( final int series, final int item )
+		{
+			return Double.valueOf( time[ item ] );
+		}
+
+		@Override
+		public Number getY( final int series, final int item )
+		{
+			return Double.valueOf( nspots[ item ] );
+		}
+
+		@Override
+		public String getSeriesKey( final int series )
+		{
+			return yFeatures.get( series );
+		}
+
+		@Override
+		public String getItemLabel( final int item )
+		{
+			return "" + item;
+		}
+
+		@Override
+		public void setItemLabel( final int item, final String label )
+		{}
+
+		@Override
+		public XYItemRenderer getRenderer()
+		{
+			return new XYLineAndShapeRenderer( true, true );
+		}
 	}
 
 	@Plugin( type = TrackMateActionFactory.class )
