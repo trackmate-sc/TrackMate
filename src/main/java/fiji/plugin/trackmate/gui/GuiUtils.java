bf9da1bfee202dd0e205adfca8fc8070842b4277
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2021-07-05 09:34:28 +0200

    Color utility methods.
    - Convert to CIElab space.
    - Compute distance based on Lab.
    - Invert a color.

--- src/main/java/fiji/plugin/trackmate/gui/GuiUtils.java
+++ src/main/java/fiji/plugin/trackmate/gui/GuiUtils.java
@@ -29,6 +29,7 @@ import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.awt.Window;
+import java.awt.color.ColorSpace;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
@@ -84,6 +85,72 @@ public class GuiUtils
 			return Color.BLACK;
 		else
 			return Color.WHITE;
+	}
+
+	/**
+	 * Distance between two colors.
+	 * <p>
+	 * Adapted from
+	 * https://stackoverflow.com/questions/9018016/how-to-compare-two-colors-for-similarity-difference
+	 * and
+	 * https://stackoverflow.com/questions/4593469/java-how-to-convert-rgb-color-to-cie-lab
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static double colorDistance( final Color a, final Color b )
+	{
+		final float[] labA = toCIELab( a );
+		final float[] labB = toCIELab( b );
+		final float deltaL = labA[ 0 ] - labB[ 0 ];
+		final float deltaA = labA[ 1 ] - labB[ 1 ];
+		final float deltaB = labA[ 2 ] - labB[ 2 ];
+		final double c1 = Math.sqrt( labA[ 1 ] * labA[ 1 ] + labA[ 2 ] * labA[ 2 ] );
+		final double c2 = Math.sqrt( labB[ 1 ] * labB[ 1 ] + labB[ 2 ] * labB[ 2 ] );
+		final double deltaC = c1 - c2;
+		double deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+		deltaH = deltaH < 0 ? 0 : Math.sqrt( deltaH );
+		final double sc = 1.0 + 0.045 * c1;
+		final double sh = 1.0 + 0.015 * c1;
+		final double deltaLKlsl = deltaL / ( 1.0 );
+		final double deltaCkcsc = deltaC / ( sc );
+		final double deltaHkhsh = deltaH / ( sh );
+		final double i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+		return i < 0 ? 0 : Math.sqrt( i );
+	}
+
+	private static final float[] fromCIEXYZ( final float[] colorvalue )
+	{
+		final double l = f( colorvalue[ 1 ] );
+		final double L = 116.0 * l - 16.0;
+		final double a = 500.0 * ( f( colorvalue[ 0 ] ) - l );
+		final double b = 200.0 * ( l - f( colorvalue[ 2 ] ) );
+		return new float[] { ( float ) L, ( float ) a, ( float ) b };
+	}
+
+	private static double f( final double x )
+	{
+		final double N = 4.0 / 29.0;
+		if ( x > 216.0 / 24389.0 )
+			return Math.cbrt( x );
+		else
+			return ( 841.0 / 108.0 ) * x + N;
+	}
+
+	public static final float[] toCIELab( final Color color )
+	{
+		final float[] rgbvalue = color.getColorComponents( null );
+		final ColorSpace CIEXYZ = ColorSpace.getInstance( ColorSpace.CS_CIEXYZ );
+		final float[] xyz = CIEXYZ.fromRGB( rgbvalue );
+		return fromCIEXYZ( xyz );
+	}
+
+	public static final Color invert(final Color color)
+	{
+		return new Color( 255 - color.getRed(),
+				255 - color.getGreen(),
+				255 - color.getBlue() );
 	}
 
 	/**
