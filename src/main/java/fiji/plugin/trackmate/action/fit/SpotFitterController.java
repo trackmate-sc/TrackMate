6a3a3410534867f60a2898adec987bbfa7d9fac0
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2021-10-14 13:41:58 +0200

    GUI to perform spot fitting with a simply undo mechanism.

---
+++ src/main/java/fiji/plugin/trackmate/action/fit/SpotFitterController.java
@@ -0,0 +1,208 @@
+package fiji.plugin.trackmate.action.fit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import fiji.plugin.trackmate.Logger;
+import fiji.plugin.trackmate.SelectionModel;
+import fiji.plugin.trackmate.Settings;
+import fiji.plugin.trackmate.Spot;
+import fiji.plugin.trackmate.TrackMate;
+import fiji.plugin.trackmate.detection.DetectionUtils;
+import fiji.plugin.trackmate.gui.GuiUtils;
+import fiji.plugin.trackmate.gui.Icons;
+import fiji.plugin.trackmate.util.EverythingDisablerAndReenabler;
+import ij.ImagePlus;
+
+public class SpotFitterController
+{
+
+	private final TrackMate trackmate;
+
+	private final SelectionModel selectionModel;
+
+	private final SpotFitterPanel gui;
+
+	private final Map< Spot, double[] > undo;
+
+	public SpotFitterController( final TrackMate trackmate, final SelectionModel selectionModel )
+	{
+		this.trackmate = trackmate;
+		this.selectionModel = selectionModel;
+		this.undo = new HashMap<>();
+
+		final Settings settings = trackmate.getSettings();
+		final List< String > fits = getAvailableFits( DetectionUtils.is2D( settings.imp ) );
+		final List< String > docs = getDocs( DetectionUtils.is2D( settings.imp ) );
+		this.gui = new SpotFitterPanel( fits, docs, settings.imp.getNChannels() );
+
+		gui.btnFit.addActionListener( e -> fit() );
+		gui.btnUndo.addActionListener( e -> undo() );
+	}
+
+	private void undo()
+	{
+		final EverythingDisablerAndReenabler disabler = new EverythingDisablerAndReenabler( gui, new Class[] { JLabel.class } );
+		disabler.disable();
+		new Thread( "SpotFitterControllerUndoThread" )
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					final Logger logger = trackmate.getModel().getLogger();
+					logger.log( "Undoing last fit.\n" );
+					logger.setStatus( "Undoing" );
+
+					int progress = 0;
+					for ( final Spot spot : undo.keySet() )
+					{
+						final double[] arr = undo.get( spot );
+						spot.putFeature( Spot.POSITION_X, arr[ 0 ] );
+						spot.putFeature( Spot.POSITION_Y, arr[ 1 ] );
+						spot.putFeature( Spot.POSITION_Z, arr[ 2 ] );
+						spot.putFeature( Spot.RADIUS, arr[ 3 ] );
+						logger.setProgress( ( double ) progress++ / undo.size() );
+					}
+					logger.setProgress( 0. );
+					// Recompute features.
+					trackmate.computeSpotFeatures( true );
+					trackmate.computeEdgeFeatures( true );
+					trackmate.computeTrackFeatures( true );
+					logger.log( "Undoing done.\n" );
+				}
+				finally
+				{
+					disabler.reenable();
+				}
+			}
+		}.start();
+	}
+
+	private void fit()
+	{
+		final EverythingDisablerAndReenabler disabler = new EverythingDisablerAndReenabler( gui, new Class[] { JLabel.class } );
+		disabler.disable();
+		new Thread( "SpotFitterControllerFitterThread" )
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					final ImagePlus imp = trackmate.getSettings().imp;
+					// 1-based to 0-based.
+					final int channel = gui.getSelectedChannel();
+					final int index = gui.getSelectedFitIndex();
+					final SpotFitter fitter;
+					// Stupid and harsh:
+					if ( DetectionUtils.is2D( imp ) )
+					{
+						if ( index == 0 )
+							fitter = new SpotGaussianFitter2D( imp, channel );
+						else if ( index == 1 )
+							fitter = new SpotGaussianFitter2DFixedRadius( imp, channel );
+						else
+							throw new IllegalArgumentException( "Index points to an unknown fit model: " + index );
+					}
+					else
+					{
+						fitter = null; // FIXME
+					}
+					fitter.setNumThreads( trackmate.getNumThreads() );
+
+					// Get spots to fit.
+					final Iterable< Spot > spots;
+					if ( gui.rdbtnAll.isSelected() )
+						spots = trackmate.getModel().getSpots().iterable( true );
+					else if ( gui.rdbtnSelection.isSelected() )
+						spots = selectionModel.getSpotSelection();
+					else
+					{
+						selectionModel.selectTrack(
+								selectionModel.getSpotSelection(),
+								selectionModel.getEdgeSelection(), 0 );
+						spots = selectionModel.getSpotSelection();
+					}
+
+					// Prepare the undo array.
+					undo.clear();
+					for ( final Spot spot : spots )
+					{
+						undo.put( spot, new double[] {
+								spot.getDoublePosition( 0 ),
+								spot.getDoublePosition( 1 ),
+								spot.getDoublePosition( 2 ),
+								spot.getFeature( Spot.RADIUS ).doubleValue()
+						} );
+					}
+
+					// Perform fit.
+					fitter.process( spots, trackmate.getModel().getLogger() );
+
+					// Recompute features.
+					trackmate.computeSpotFeatures( true );
+					trackmate.computeEdgeFeatures( true );
+					trackmate.computeTrackFeatures( true );
+				}
+				finally
+				{
+					disabler.reenable();
+				}
+			}
+		}.start();
+	}
+
+	public void show()
+	{
+		if ( gui.getParent() != null && gui.getParent().isVisible() )
+			return;
+
+		final JFrame frame = new JFrame( "TrackMate spot fitting" );
+		frame.setIconImage( Icons.SPOT_ICON.getImage() );
+		frame.setSize( 300, 300 );
+		frame.getContentPane().add( gui );
+		GuiUtils.positionWindow( frame, trackmate.getSettings().imp.getCanvas() );
+		frame.setVisible( true );
+	}
+
+	private List< String > getDocs( final boolean is2d )
+	{
+		final List< String > docs = new ArrayList<>();
+		if ( is2d )
+		{
+			docs.add(
+					"<html>Fit a 2D circular Gaussian on each spot, "
+							+ "allowing for the radius to vary.</html>" );
+			docs.add( "<html>Fit a 2D circular Gaussian on each spot, "
+					+ "but blocking its sigma value. The radius of the spot"
+					+ "is not updated.</html>" );
+		}
+		else
+		{
+
+		}
+		return docs;
+	}
+
+	private List< String > getAvailableFits( final boolean is2d )
+	{
+		final List< String > fits = new ArrayList<>();
+		if ( is2d )
+		{
+			fits.add( "Gaussian 2D" );
+			fits.add( "Gaussian 2D with fixed radius" );
+		}
+		else
+		{
+
+		}
+		return fits;
+	}
+}
---
+++ src/main/java/fiji/plugin/trackmate/action/fit/SpotFitterPanel.java
@@ -0,0 +1,195 @@
+package fiji.plugin.trackmate.action.fit;
+
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.util.List;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+
+import fiji.plugin.trackmate.gui.Fonts;
+
+public class SpotFitterPanel extends JPanel
+{
+
+	private static final long serialVersionUID = 1L;
+
+	private final JComboBox< String > cmbboxModel;
+
+	final JButton btnUndo;
+
+	final JButton btnFit;
+
+	final JRadioButton rdbtnAll;
+
+	final JRadioButton rdbtnSelection;
+
+	final JRadioButton rdbtnTracks;
+
+	private final JSlider sliderChannel;
+
+	public SpotFitterPanel( final List< String > fits, final List< String > docs, final int nChannels )
+	{
+		final GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 0, 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE };
+		setLayout( gridBagLayout );
+
+		final JLabel lblTitle = new JLabel( "Refine spot position by fitting" );
+		lblTitle.setFont( Fonts.BIG_FONT );
+		lblTitle.setHorizontalAlignment( SwingConstants.CENTER );
+		final GridBagConstraints gbcLblTitle = new GridBagConstraints();
+		gbcLblTitle.gridwidth = 2;
+		gbcLblTitle.fill = GridBagConstraints.BOTH;
+		gbcLblTitle.insets = new Insets( 5, 5, 5, 5 );
+		gbcLblTitle.gridx = 0;
+		gbcLblTitle.gridy = 0;
+		add( lblTitle, gbcLblTitle );
+
+		final JLabel lblChannel = new JLabel( "Channel:" );
+		sliderChannel = new JSlider( 1, nChannels, 1 );
+
+		// Visible only if N channels > 1.
+		if ( nChannels > 1 )
+		{
+			final GridBagConstraints gbcLblChannel = new GridBagConstraints();
+			gbcLblChannel.anchor = GridBagConstraints.EAST;
+			gbcLblChannel.insets = new Insets( 5, 5, 5, 5 );
+			gbcLblChannel.gridx = 0;
+			gbcLblChannel.gridy = 1;
+			add( lblChannel, gbcLblChannel );
+
+			final JPanel panelSlider = new JPanel();
+			final GridBagConstraints gbc_panelSlider = new GridBagConstraints();
+			gbc_panelSlider.fill = GridBagConstraints.BOTH;
+			gbc_panelSlider.insets = new Insets( 5, 5, 5, 5 );
+			gbc_panelSlider.gridx = 1;
+			gbc_panelSlider.gridy = 1;
+			add( panelSlider, gbc_panelSlider );
+			panelSlider.setLayout( new BoxLayout( panelSlider, BoxLayout.X_AXIS ) );
+			panelSlider.add( sliderChannel );
+			sliderChannel.setPaintLabels( true );
+			sliderChannel.setSnapToTicks( true );
+			sliderChannel.setPaintTicks( true );
+			sliderChannel.setFont( Fonts.SMALL_FONT );
+
+			final JLabel lblChannelIndex = new JLabel( "1" );
+			lblChannelIndex.setFont( Fonts.FONT.deriveFont( Font.BOLD ) );
+			lblChannelIndex.setHorizontalAlignment( SwingConstants.CENTER );
+			panelSlider.add( lblChannelIndex );
+			sliderChannel.addChangeListener( e -> lblChannelIndex.setText( "" + sliderChannel.getValue() ) );
+		}
+
+		final JLabel lblFitType = new JLabel( "Fit type:" );
+		lblFitType.setFont( Fonts.FONT );
+		final GridBagConstraints gbcLblFitType = new GridBagConstraints();
+		gbcLblFitType.anchor = GridBagConstraints.EAST;
+		gbcLblFitType.insets = new Insets( 5, 5, 5, 5 );
+		gbcLblFitType.gridx = 0;
+		gbcLblFitType.gridy = 2;
+		add( lblFitType, gbcLblFitType );
+
+		final String[] arr = fits.toArray( new String[] {} );
+		this.cmbboxModel = new JComboBox<>( arr );
+		cmbboxModel.setFont( Fonts.FONT );
+		final GridBagConstraints gbcComboBox = new GridBagConstraints();
+		gbcComboBox.insets = new Insets( 5, 5, 5, 5 );
+		gbcComboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbcComboBox.gridx = 1;
+		gbcComboBox.gridy = 2;
+		add( cmbboxModel, gbcComboBox );
+
+		final JLabel lblDoc = new JLabel( " " );
+		lblDoc.setFont( Fonts.SMALL_FONT );
+		final GridBagConstraints gbcLblDoc = new GridBagConstraints();
+		gbcLblDoc.fill = GridBagConstraints.HORIZONTAL;
+		gbcLblDoc.gridwidth = 2;
+		gbcLblDoc.insets = new Insets( 5, 5, 5, 5 );
+		gbcLblDoc.gridx = 0;
+		gbcLblDoc.gridy = 3;
+		add( lblDoc, gbcLblDoc );
+
+		final JPanel panelSelection = new JPanel();
+		final GridBagConstraints gbcPanelSelection = new GridBagConstraints();
+		gbcPanelSelection.gridwidth = 2;
+		gbcPanelSelection.insets = new Insets( 5, 5, 5, 5 );
+		gbcPanelSelection.fill = GridBagConstraints.HORIZONTAL;
+		gbcPanelSelection.gridx = 0;
+		gbcPanelSelection.gridy = 4;
+		add( panelSelection, gbcPanelSelection );
+		panelSelection.setLayout( new BoxLayout( panelSelection, BoxLayout.X_AXIS ) );
+
+		final JLabel lblPerformOn = new JLabel( "Fit:" );
+		lblPerformOn.setFont( Fonts.FONT );
+		panelSelection.add( lblPerformOn );
+
+		rdbtnAll = new JRadioButton( "All spots" );
+		rdbtnSelection = new JRadioButton( "Selection" );
+		rdbtnTracks = new JRadioButton( "Tracks of selection" );
+		rdbtnAll.setFont( Fonts.FONT );
+		rdbtnSelection.setFont( Fonts.FONT );
+		rdbtnTracks.setFont( Fonts.FONT );
+
+		panelSelection.add( Box.createHorizontalGlue() );
+		panelSelection.add( rdbtnAll );
+		panelSelection.add( rdbtnSelection );
+		panelSelection.add( rdbtnTracks );
+
+		final JPanel panelButtons = new JPanel();
+		final GridBagConstraints gbcPanelButtons = new GridBagConstraints();
+		gbcPanelButtons.gridwidth = 2;
+		gbcPanelButtons.fill = GridBagConstraints.BOTH;
+		gbcPanelButtons.gridx = 0;
+		gbcPanelButtons.gridy = 5;
+		add( panelButtons, gbcPanelButtons );
+		panelButtons.setLayout( new BoxLayout( panelButtons, BoxLayout.X_AXIS ) );
+
+		btnUndo = new JButton( "Undo last fit" );
+		btnUndo.setFont( Fonts.FONT );
+		panelButtons.add( btnUndo );
+
+		final Component horizontalGlue = Box.createHorizontalGlue();
+		panelButtons.add( horizontalGlue );
+
+		btnFit = new JButton( "Fit" );
+		btnFit.setFont( Fonts.FONT );
+		panelButtons.add( btnFit );
+
+		final ButtonGroup btngroup = new ButtonGroup();
+		btngroup.add( rdbtnAll );
+		btngroup.add( rdbtnSelection );
+		btngroup.add( rdbtnTracks );
+		rdbtnSelection.setSelected( true );
+
+		cmbboxModel.addActionListener( e -> lblDoc.setText( docs.get( cmbboxModel.getSelectedIndex() ) ) );
+		cmbboxModel.setSelectedIndex( 0 );
+	}
+
+	/**
+	 * 1-based.
+	 * 
+	 * @return
+	 */
+	public int getSelectedChannel()
+	{
+		return sliderChannel.getValue();
+	}
+
+	public int getSelectedFitIndex()
+	{
+		return cmbboxModel.getSelectedIndex();
+	}
+}
