0e97ad58828d9374d69099d232063392972bfcd0
Jean-Yves Tinevez <tinevez@pasteur.fr>
2021-12-16 16:32:53 +0100

    Code style changes.

--- src/main/java/fiji/plugin/trackmate/util/JLabelLogger.java
+++ src/main/java/fiji/plugin/trackmate/util/JLabelLogger.java
@@ -29,54 +29,61 @@ import javax.swing.JLabel;
 
 import fiji.plugin.trackmate.Logger;
 
-public class JLabelLogger extends JLabel {
-	
+public class JLabelLogger extends JLabel
+{
+
 	private static final long serialVersionUID = 1L;
+
 	private final MyLogger logger;
 
-	public JLabelLogger() {
-		this.logger = new MyLogger(this);
+	public JLabelLogger()
+	{
+		this.logger = new MyLogger( this );
 		setFont( SMALL_FONT );
 	}
-	
-	public Logger getLogger() {
+
+	public Logger getLogger()
+	{
 		return logger;
 	}
 
-	
-	
-	
 	/*
 	 * INNER CLASS
 	 */
-	
-	private class MyLogger extends Logger {
+
+	private class MyLogger extends Logger
+	{
 
 		private final JLabelLogger label;
 
-		public MyLogger(final JLabelLogger logger) {
+		public MyLogger( final JLabelLogger logger )
+		{
 			this.label = logger;
 		}
 
 		@Override
-		public void log(final String message, final Color color) {
-			label.setText(message);
-			label.setForeground(color);
+		public void log( final String message, final Color color )
+		{
+			label.setText( message );
+			label.setForeground( color );
 		}
 
 		@Override
-		public void error(final String message) {
-			log(message, Logger.ERROR_COLOR);
+		public void error( final String message )
+		{
+			log( message, Logger.ERROR_COLOR );
 		}
 
 		/** Ignored. */
 		@Override
-		public void setProgress(final double val) {}
+		public void setProgress( final double val )
+		{}
 
 		@Override
-		public void setStatus(final String status) {
-			log(status, Logger.BLUE_COLOR);
+		public void setStatus( final String status )
+		{
+			log( status, Logger.BLUE_COLOR );
 		}
-		
+
 	}
 }
