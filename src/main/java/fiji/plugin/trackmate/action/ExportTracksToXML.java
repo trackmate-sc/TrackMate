01815b74fc2998103683050f77a3b4c9cfdf27ac
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2022-02-19 18:58:25 +0100

    Code style changes.

--- src/main/java/fiji/plugin/trackmate/action/ExportTracksToXML.java
+++ src/main/java/fiji/plugin/trackmate/action/ExportTracksToXML.java
@@ -49,24 +49,26 @@ import fiji.plugin.trackmate.gui.displaysettings.DisplaySettings;
 import fiji.plugin.trackmate.io.IOUtils;
 import fiji.plugin.trackmate.util.TMUtils;
 
-public class ExportTracksToXML extends AbstractTMAction {
+public class ExportTracksToXML extends AbstractTMAction
+{
 
 	public static final String NAME = "Export tracks to XML file";
 
 	public static final String KEY = "EXPORT_TRACKS_TO_XML_SIMPLE";
+
 	public static final String INFO_TEXT = "<html>" +
-				"Export the tracks in the current model content to a XML " +
-				"file in a simple format. " +
-				"<p> " +
-				"The file will have one element per track, and each track " +
-				"contains several spot elements. These spots are " +
-				"sorted by frame number, and have 4 numerical attributes: " +
-				"the frame number this spot is in, and its X, Y, Z position in " +
-				"physical units as specified in the image properties. " +
-				"<p>" +
-				"As such, this format <u>cannot</u> handle track merging and " +
-				"splitting properly, and is suited only for non-branching tracks." +
-				"</html>";
+			"Export the tracks in the current model content to a XML " +
+			"file in a simple format. " +
+			"<p> " +
+			"The file will have one element per track, and each track " +
+			"contains several spot elements. These spots are " +
+			"sorted by frame number, and have 4 numerical attributes: " +
+			"the frame number this spot is in, and its X, Y, Z position in " +
+			"physical units as specified in the image properties. " +
+			"<p>" +
+			"As such, this format <u>cannot</u> handle track merging and " +
+			"splitting properly, and is suited only for non-branching tracks." +
+			"</html>";
 
 	/**
 	 * Static utility that silently exports tracks in a simplified xml format,
@@ -84,104 +86,119 @@ public class ExportTracksToXML extends AbstractTMAction {
 	 * @throws IOException
 	 *             if there is a problem writing the file.
 	 */
-	public static void export(final Model model, final Settings settings, final File file) throws FileNotFoundException, IOException {
-		final Element root = marshall(model, settings, Logger.VOID_LOGGER);
-		final Document document = new Document(root);
-		final XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
-		outputter.output(document, new FileOutputStream(file));
+	public static void export( final Model model, final Settings settings, final File file ) throws FileNotFoundException, IOException
+	{
+		final Element root = marshall( model, settings, Logger.VOID_LOGGER );
+		final Document document = new Document( root );
+		final XMLOutputter outputter = new XMLOutputter( Format.getPrettyFormat() );
+		outputter.output( document, new FileOutputStream( file ) );
 	}
 
 	@Override
 	public void execute( final TrackMate trackmate, final SelectionModel selectionModel, final DisplaySettings displaySettings, final Frame parent )
 	{
-		logger.log("Exporting tracks to simple XML format.\n");
+		logger.log( "Exporting tracks to simple XML format.\n" );
 		final Model model = trackmate.getModel();
-		final int ntracks = model.getTrackModel().nTracks(true);
-		if (ntracks == 0) {
-			logger.log("No visible track found. Aborting.\n");
+		final int ntracks = model.getTrackModel().nTracks( true );
+		if ( ntracks == 0 )
+		{
+			logger.log( "No visible track found. Aborting.\n" );
 			return;
 		}
 
-		logger.log("  Preparing XML data.\n");
-		final Element root = marshall(model, trackmate.getSettings(), logger);
+		logger.log( "  Preparing XML data.\n" );
+		final Element root = marshall( model, trackmate.getSettings(), logger );
 
 		File folder;
-		try {
-			folder = new File(trackmate.getSettings().imp.getOriginalFileInfo().directory);
-		} catch (final NullPointerException npe) {
-			folder = new File(System.getProperty("user.dir")).getParentFile().getParentFile();
+		try
+		{
+			folder = new File( trackmate.getSettings().imp.getOriginalFileInfo().directory );
+		}
+		catch ( final NullPointerException npe )
+		{
+			folder = new File( System.getProperty( "user.dir" ) ).getParentFile().getParentFile();
 		}
 
 		File file;
-		try {
+		try
+		{
 			String filename = trackmate.getSettings().imageFileName;
-			final int dot = filename.indexOf(".");
-			filename = dot < 0 ? filename : filename.substring(0, dot);
-			file = new File(folder.getPath() + File.separator + filename +"_Tracks.xml");
-		} catch (final NullPointerException npe) {
-			file = new File(folder.getPath() + File.separator + "Tracks.xml");
+			final int dot = filename.indexOf( "." );
+			filename = dot < 0 ? filename : filename.substring( 0, dot );
+			file = new File( folder.getPath() + File.separator + filename + "_Tracks.xml" );
+		}
+		catch ( final NullPointerException npe )
+		{
+			file = new File( folder.getPath() + File.separator + "Tracks.xml" );
 		}
 		file = IOUtils.askForFileForSaving( file, parent, logger );
-		if (null == file) {
-			return;
-		}
+		if ( null == file )
+		{ return; }
 
-		logger.log("  Writing to file.\n");
-		final Document document = new Document(root);
-		final XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
-		try {
-			outputter.output(document, new FileOutputStream(file));
-		} catch (final FileNotFoundException e) {
-			logger.error("Trouble writing to "+file+":\n" + e.getMessage());
-		} catch (final IOException e) {
-			logger.error("Trouble writing to "+file+":\n" + e.getMessage());
+		logger.log( "  Writing to file.\n" );
+		final Document document = new Document( root );
+		final XMLOutputter outputter = new XMLOutputter( Format.getPrettyFormat() );
+		try
+		{
+			outputter.output( document, new FileOutputStream( file ) );
 		}
-		logger.log("Done.\n");
+		catch ( final FileNotFoundException e )
+		{
+			logger.error( "Trouble writing to " + file + ":\n" + e.getMessage() );
+		}
+		catch ( final IOException e )
+		{
+			logger.error( "Trouble writing to " + file + ":\n" + e.getMessage() );
+		}
+		logger.log( "Done.\n" );
 	}
 
-	private static Element marshall(final Model model, final Settings settings, final Logger logger) {
-		logger.setStatus("Marshalling...");
-		final Element content = new Element(CONTENT_KEY);
+	private static Element marshall( final Model model, final Settings settings, final Logger logger )
+	{
+		logger.setStatus( "Marshalling..." );
+		final Element content = new Element( CONTENT_KEY );
 
-		content.setAttribute(NTRACKS_ATT, ""+model.getTrackModel().nTracks(true));
-		content.setAttribute(PHYSUNIT_ATT, model.getSpaceUnits());
-		content.setAttribute(FRAMEINTERVAL_ATT, ""+settings.dt);
-		content.setAttribute(FRAMEINTERVALUNIT_ATT, ""+model.getTimeUnits());
-		content.setAttribute(DATE_ATT, TMUtils.getCurrentTimeString());
-		content.setAttribute(FROM_ATT, TrackMate.PLUGIN_NAME_STR + " v" + TrackMate.PLUGIN_NAME_VERSION);
+		content.setAttribute( NTRACKS_ATT, "" + model.getTrackModel().nTracks( true ) );
+		content.setAttribute( PHYSUNIT_ATT, model.getSpaceUnits() );
+		content.setAttribute( FRAMEINTERVAL_ATT, "" + settings.dt );
+		content.setAttribute( FRAMEINTERVALUNIT_ATT, "" + model.getTimeUnits() );
+		content.setAttribute( DATE_ATT, TMUtils.getCurrentTimeString() );
+		content.setAttribute( FROM_ATT, TrackMate.PLUGIN_NAME_STR + " v" + TrackMate.PLUGIN_NAME_VERSION );
 
-		final Set<Integer> trackIDs = model.getTrackModel().trackIDs(true);
+		final Set< Integer > trackIDs = model.getTrackModel().trackIDs( true );
 		int i = 0;
-		for (final Integer trackID : trackIDs) {
+		for ( final Integer trackID : trackIDs )
+		{
 
-			final Set<Spot> track = model.getTrackModel().trackSpots(trackID);
+			final Set< Spot > track = model.getTrackModel().trackSpots( trackID );
 
-			final Element trackElement = new Element(TRACK_KEY);
-			trackElement.setAttribute(NSPOTS_ATT, ""+track.size());
+			final Element trackElement = new Element( TRACK_KEY );
+			trackElement.setAttribute( NSPOTS_ATT, "" + track.size() );
 
 			// Sort them by time
-			final TreeSet<Spot> sortedTrack = new TreeSet<>(Spot.timeComparator);
-			sortedTrack.addAll(track);
+			final TreeSet< Spot > sortedTrack = new TreeSet<>( Spot.timeComparator );
+			sortedTrack.addAll( track );
 
-			for (final Spot spot : sortedTrack) {
-				final int frame = spot.getFeature(Spot.FRAME).intValue();
-				final double x = spot.getFeature(Spot.POSITION_X);
-				final double y = spot.getFeature(Spot.POSITION_Y);
-				final double z = spot.getFeature(Spot.POSITION_Z);
+			for ( final Spot spot : sortedTrack )
+			{
+				final int frame = spot.getFeature( Spot.FRAME ).intValue();
+				final double x = spot.getFeature( Spot.POSITION_X );
+				final double y = spot.getFeature( Spot.POSITION_Y );
+				final double z = spot.getFeature( Spot.POSITION_Z );
 
-				final Element spotElement = new Element(SPOT_KEY);
-				spotElement.setAttribute(T_ATT, ""+frame);
-				spotElement.setAttribute(X_ATT, ""+x);
-				spotElement.setAttribute(Y_ATT, ""+y);
-				spotElement.setAttribute(Z_ATT, ""+z);
-				trackElement.addContent(spotElement);
+				final Element spotElement = new Element( SPOT_KEY );
+				spotElement.setAttribute( T_ATT, "" + frame );
+				spotElement.setAttribute( X_ATT, "" + x );
+				spotElement.setAttribute( Y_ATT, "" + y );
+				spotElement.setAttribute( Z_ATT, "" + z );
+				trackElement.addContent( spotElement );
 			}
-			content.addContent(trackElement);
-			logger.setProgress(i++ / (0d + model.getTrackModel().nTracks(true)));
+			content.addContent( trackElement );
+			logger.setProgress( i++ / ( 0d + model.getTrackModel().nTracks( true ) ) );
 		}
 
-		logger.setStatus("");
-		logger.setProgress(1);
+		logger.setStatus( "" );
+		logger.setProgress( 1 );
 		return content;
 	}
 
@@ -189,20 +206,33 @@ public class ExportTracksToXML extends AbstractTMAction {
 	 * XML KEYS
 	 */
 
-	private static final String CONTENT_KEY 			= "Tracks";
-	private static final String DATE_ATT 				= "generationDateTime";
-	private static final String PHYSUNIT_ATT 			= "spaceUnits";
-	private static final String FRAMEINTERVAL_ATT 		= "frameInterval";
-	private static final String FRAMEINTERVALUNIT_ATT 	= "timeUnits";
-	private static final String FROM_ATT 				= "from";
-	private static final String NTRACKS_ATT				= "nTracks";
-	private static final String NSPOTS_ATT				= "nSpots";
-	private static final String TRACK_KEY 				= "particle";
-	private static final String SPOT_KEY 				= "detection";
-	private static final String X_ATT 					= "x";
-	private static final String Y_ATT 					= "y";
-	private static final String Z_ATT 					= "z";
-	private static final String T_ATT 					= "t";
+	private static final String CONTENT_KEY = "Tracks";
+
+	private static final String DATE_ATT = "generationDateTime";
+
+	private static final String PHYSUNIT_ATT = "spaceUnits";
+
+	private static final String FRAMEINTERVAL_ATT = "frameInterval";
+
+	private static final String FRAMEINTERVALUNIT_ATT = "timeUnits";
+
+	private static final String FROM_ATT = "from";
+
+	private static final String NTRACKS_ATT = "nTracks";
+
+	private static final String NSPOTS_ATT = "nSpots";
+
+	private static final String TRACK_KEY = "particle";
+
+	private static final String SPOT_KEY = "detection";
+
+	private static final String X_ATT = "x";
+
+	private static final String Y_ATT = "y";
+
+	private static final String Z_ATT = "z";
+
+	private static final String T_ATT = "t";
 
 	@Plugin( type = TrackMateActionFactory.class )
 	public static class Factory implements TrackMateActionFactory
--- src/main/java/fiji/plugin/trackmate/action/ISBIChallengeExporter.java
+++ src/main/java/fiji/plugin/trackmate/action/ISBIChallengeExporter.java
@@ -51,32 +51,37 @@ import fiji.plugin.trackmate.TrackMate;
 import fiji.plugin.trackmate.gui.displaysettings.DisplaySettings;
 import fiji.plugin.trackmate.io.IOUtils;
 
-public class ISBIChallengeExporter extends AbstractTMAction {
+public class ISBIChallengeExporter extends AbstractTMAction
+{
 
 	public static final String NAME = "Export to ISBI challenge format";
 
 	public static final String KEY = "EXPORT_TO_ISBI_CHALLENGE_FORMAT";
+
 	public static final String INFO_TEXT = "<html>" +
-				"Export the current model content to a XML file following the " +
-				"ISBI 2012 particle tracking challenge format, as specified on " +
-				"<a href='http://bioimageanalysis.org/track/'></a>. " +
-				"<p> " +
-				"Only tracks are exported. If there is no track, this action " +
-				"does nothing. " +
-				"</html>";
+			"Export the current model content to a XML file following the " +
+			"ISBI 2012 particle tracking challenge format, as specified on " +
+			"<a href='http://bioimageanalysis.org/track/'></a>. " +
+			"<p> " +
+			"Only tracks are exported. If there is no track, this action " +
+			"does nothing. " +
+			"</html>";
 
 	@Override
 	public void execute( final TrackMate trackmate, final SelectionModel selectionModel, final DisplaySettings displaySettings, final Frame parent )
 	{
 		final Model model = trackmate.getModel();
 		File file;
-		final File folder = new File(System.getProperty("user.dir")).getParentFile().getParentFile();
-		try {
+		final File folder = new File( System.getProperty( "user.dir" ) ).getParentFile().getParentFile();
+		try
+		{
 			String filename = trackmate.getSettings().imageFileName;
-			filename = filename.substring(0, filename.indexOf("."));
-			file = new File(folder.getPath() + File.separator + filename +"_ISBI.xml");
-		} catch (final NullPointerException npe) {
-			file = new File(folder.getPath() + File.separator + "ISBIChallenge2012Result.xml");
+			filename = filename.substring( 0, filename.indexOf( "." ) );
+			file = new File( folder.getPath() + File.separator + filename + "_ISBI.xml" );
+		}
+		catch ( final NullPointerException npe )
+		{
+			file = new File( folder.getPath() + File.separator + "ISBIChallenge2012Result.xml" );
 		}
 		file = IOUtils.askForFileForSaving( file, parent, logger );
 
@@ -90,88 +95,101 @@ public class ISBIChallengeExporter extends AbstractTMAction {
 
 	public static void exportToFile( final Model model, final Settings settings, final File file, final Logger logger )
 	{
-		logger.log("Exporting to ISBI 2012 particle tracking challenge format.\n");
-		final int ntracks = model.getTrackModel().nTracks(true);
-		if (ntracks == 0) {
-			logger.log("No visible track found. Aborting.\n");
+		logger.log( "Exporting to ISBI 2012 particle tracking challenge format.\n" );
+		final int ntracks = model.getTrackModel().nTracks( true );
+		if ( ntracks == 0 )
+		{
+			logger.log( "No visible track found. Aborting.\n" );
 			return;
 		}
 
-		logger.log("  Preparing XML data.\n");
-		final Element root = marshall(model, settings);
+		logger.log( "  Preparing XML data.\n" );
+		final Element root = marshall( model, settings );
 
-		logger.log("  Writing to file.\n");
-		final Document document = new Document(root);
-		final XMLOutputter outputter = new XMLOutputter(Format.getPrettyFormat());
-		try {
-			outputter.output(document, new FileOutputStream(file));
-		} catch (final FileNotFoundException e) {
-			logger.error("Trouble writing to "+file+":\n" + e.getMessage());
-		} catch (final IOException e) {
-			logger.error("Trouble writing to "+file+":\n" + e.getMessage());
+		logger.log( "  Writing to file.\n" );
+		final Document document = new Document( root );
+		final XMLOutputter outputter = new XMLOutputter( Format.getPrettyFormat() );
+		try
+		{
+			outputter.output( document, new FileOutputStream( file ) );
 		}
-		logger.log("Done.\n");
+		catch ( final FileNotFoundException e )
+		{
+			logger.error( "Trouble writing to " + file + ":\n" + e.getMessage() );
+		}
+		catch ( final IOException e )
+		{
+			logger.error( "Trouble writing to " + file + ":\n" + e.getMessage() );
+		}
+		logger.log( "Done.\n" );
 	}
 
-	private static final Element marshall(final Model model, final Settings settings) {
+	private static final Element marshall( final Model model, final Settings settings )
+	{
 		final Logger logger = model.getLogger();
 
-		final Element root = new Element("root");
-		final Element content = new Element(CONTENT_KEY);
+		final Element root = new Element( "root" );
+		final Element content = new Element( CONTENT_KEY );
 
 		// Extract from file name
-		final String filename = settings.imageFileName; // VIRUS snr 7 density mid.tif
-		final String pattern = "^(\\w+) " + SNR_ATT +" (\\d+) " + DENSITY_ATT + " (\\w+)\\.";
-		final Pattern r = Pattern.compile(pattern);
-		final Matcher m = r.matcher(filename);
+		final String filename = settings.imageFileName; // VIRUS snr 7 density
+														// mid.tif
+		final String pattern = "^(\\w+) " + SNR_ATT + " (\\d+) " + DENSITY_ATT + " (\\w+)\\.";
+		final Pattern r = Pattern.compile( pattern );
+		final Matcher m = r.matcher( filename );
 		String snr_val;
 		String density_val;
 		String scenario_val;
-		if (m.find()) {
-			scenario_val 	= m.group(1);
-			snr_val 		= m.group(2);
-			density_val 	= m.group(3);
-		} else {
+		if ( m.find() )
+		{
+			scenario_val = m.group( 1 );
+			snr_val = m.group( 2 );
+			density_val = m.group( 3 );
+		}
+		else
+		{
 			scenario_val = filename;
 			snr_val = "?";
 			density_val = "?";
 		}
-		content.setAttribute(SNR_ATT, snr_val);
-		content.setAttribute(DENSITY_ATT, density_val);
-		content.setAttribute(SCENARIO_ATT, scenario_val);
-		content.setAttribute(DATE_ATT, new Date().toString());
+		content.setAttribute( SNR_ATT, snr_val );
+		content.setAttribute( DENSITY_ATT, density_val );
+		content.setAttribute( SCENARIO_ATT, scenario_val );
+		content.setAttribute( DATE_ATT, new Date().toString() );
 
-		logger.setStatus("Marshalling...");
-		final Integer[] visibleTracks = model.getTrackModel().trackIDs(true).toArray(new Integer[] {});
-		for (int i = 0 ; i < model.getTrackModel().nTracks(true) ; i++) {
+		logger.setStatus( "Marshalling..." );
+		final Integer[] visibleTracks = model.getTrackModel().trackIDs( true ).toArray( new Integer[] {} );
+		for ( int i = 0; i < model.getTrackModel().nTracks( true ); i++ )
+		{
 
-			final Element trackElement = new Element(TRACK_KEY);
-			final int trackindex = visibleTracks[i];
-			final Set<Spot> track = model.getTrackModel().trackSpots(trackindex);
+			final Element trackElement = new Element( TRACK_KEY );
+			final int trackindex = visibleTracks[ i ];
+			final Set< Spot > track = model.getTrackModel().trackSpots( trackindex );
 			// Sort them by time
-			final TreeSet<Spot> sortedTrack = new TreeSet<>(Spot.timeComparator);
-			sortedTrack.addAll(track);
+			final TreeSet< Spot > sortedTrack = new TreeSet<>( Spot.timeComparator );
+			sortedTrack.addAll( track );
 
-			for (final Spot spot : sortedTrack) {
-				final int t = spot.getFeature(Spot.FRAME).intValue();
-				final double x = spot.getFeature(Spot.POSITION_X);
-				final double y = spot.getFeature(Spot.POSITION_Y);
-				final double z = spot.getFeature(Spot.POSITION_Z);
+			for ( final Spot spot : sortedTrack )
+			{
+				final int t = spot.getFeature( Spot.FRAME ).intValue();
+				final double x = spot.getFeature( Spot.POSITION_X );
+				final double y = spot.getFeature( Spot.POSITION_Y );
+				final double z = spot.getFeature( Spot.POSITION_Z );
 
-				final Element spotElement = new Element(SPOT_KEY);
-				spotElement.setAttribute(T_ATT, ""+t);
-				spotElement.setAttribute(X_ATT, ""+x);
-				spotElement.setAttribute(Y_ATT, ""+y);
-				spotElement.setAttribute(Z_ATT, ""+z);
-				trackElement.addContent(spotElement);
+				final Element spotElement = new Element( SPOT_KEY );
+				spotElement.setAttribute( T_ATT, "" + t );
+				spotElement.setAttribute( X_ATT, "" + x );
+				spotElement.setAttribute( Y_ATT, "" + y );
+				spotElement.setAttribute( Z_ATT, "" + z );
+				trackElement.addContent( spotElement );
 			}
-			content.addContent(trackElement);
-			logger.setProgress(i / (0d + model.getTrackModel().nTracks(true)));
+			content.addContent( trackElement );
+			logger.setProgress( i / ( 0d + model.getTrackModel().nTracks( true ) ) );
 		}
 
-		logger.setStatus("");
-		logger.setProgress(1);
-		root.addContent(content);
+		logger.setStatus( "" );
+		logger.setProgress( 1 );
+		root.addContent( content );
 		return root;
 	}
 
@@ -179,17 +197,27 @@ public class ISBIChallengeExporter extends AbstractTMAction {
 	 * XML KEYS
 	 */
 
-	private static final String CONTENT_KEY 	= "TrackContestISBI2012";
-	private static final String DATE_ATT 		= "generationDateTime";
-	private static final String SNR_ATT 		= "snr";
-	private static final String DENSITY_ATT 	= "density";
-	private static final String SCENARIO_ATT 	= "scenario";
-	private static final String TRACK_KEY 		= "particle";
-	private static final String SPOT_KEY 		= "detection";
-	private static final String X_ATT 			= "x";
-	private static final String Y_ATT 			= "y";
-	private static final String Z_ATT 			= "z";
-	private static final String T_ATT 			= "t";
+	private static final String CONTENT_KEY = "TrackContestISBI2012";
+
+	private static final String DATE_ATT = "generationDateTime";
+
+	private static final String SNR_ATT = "snr";
+
+	private static final String DENSITY_ATT = "density";
+
+	private static final String SCENARIO_ATT = "scenario";
+
+	private static final String TRACK_KEY = "particle";
+
+	private static final String SPOT_KEY = "detection";
+
+	private static final String X_ATT = "x";
+
+	private static final String Y_ATT = "y";
+
+	private static final String Z_ATT = "z";
+
+	private static final String T_ATT = "t";
 
 	@Plugin( type = TrackMateActionFactory.class, visible = true )
 	public static class Factory implements TrackMateActionFactory
