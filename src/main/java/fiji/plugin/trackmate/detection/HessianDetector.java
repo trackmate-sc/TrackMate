d6af4de354b7ec92e6a8fa0a19e87b0fc02fbacf
Jean-Yves Tinevez <tinevez@pasteur.fr>
2022-03-07 16:19:20 +0100

    A new detector based on the determinant of the Hessian matrix.

---
+++ src/main/java/fiji/plugin/trackmate/detection/HessianDetector.java
@@ -0,0 +1,270 @@
+package fiji.plugin.trackmate.detection;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.ToDoubleFunction;
+
+import fiji.plugin.trackmate.Spot;
+import net.imglib2.Dimensions;
+import net.imglib2.FinalDimensions;
+import net.imglib2.Interval;
+import net.imglib2.RandomAccessible;
+import net.imglib2.algorithm.MultiThreaded;
+import net.imglib2.algorithm.gradient.HessianMatrix;
+import net.imglib2.exception.IncompatibleTypeException;
+import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.loops.LoopBuilder;
+import net.imglib2.outofbounds.OutOfBoundsBorderFactory;
+import net.imglib2.parallel.TaskExecutor;
+import net.imglib2.parallel.TaskExecutors;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.util.Util;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
+import net.imglib2.view.composite.CompositeIntervalView;
+import net.imglib2.view.composite.RealComposite;
+
+public class HessianDetector< T extends RealType< T > & NativeType< T > > implements SpotDetector< T >, MultiThreaded
+{
+
+	/*
+	 * FIELDS
+	 */
+
+	private final static String BASE_ERROR_MESSAGE = "HessianDetector: ";
+
+	private final RandomAccessible< T > img;
+
+	private final Interval interval;
+
+	private final double[] calibration;
+
+	private final double radiusXY;
+
+	private final double radiusZ;
+
+	private final double threshold;
+
+	private final boolean doSubPixelLocalization;
+
+	private String errorMessage;
+
+	private List< Spot > spots;
+
+	private long processingTime;
+
+	private int numThreads;
+
+	private final boolean normalize;
+
+	/*
+	 * CONSTRUCTOR
+	 */
+
+	public HessianDetector(
+			final RandomAccessible< T > img,
+			final Interval interval,
+			final double[] calibration,
+			final double radiusXY,
+			final double radiusZ,
+			final double threshold,
+			final boolean normalize,
+			final boolean doSubPixelLocalization )
+	{
+		this.img = img;
+		this.interval = interval;
+		this.calibration = calibration;
+		this.radiusXY = radiusXY;
+		this.radiusZ = radiusZ;
+		this.threshold = threshold;
+		this.normalize = normalize;
+		this.doSubPixelLocalization = doSubPixelLocalization;
+		setNumThreads();
+	}
+
+	/*
+	 * METHODS
+	 */
+
+	@Override
+	public boolean checkInput()
+	{
+		if ( null == img )
+		{
+			errorMessage = BASE_ERROR_MESSAGE + "Image is null.";
+			return false;
+		}
+		if ( img.numDimensions() > 3 || img.numDimensions() < 2 )
+		{
+			errorMessage = BASE_ERROR_MESSAGE + "Image must be 2D or 3D, got " + img.numDimensions() + "D.";
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean process()
+	{
+		spots = null;
+		errorMessage = null;
+
+		final long start = System.currentTimeMillis();
+		try
+		{
+			final Img< DoubleType > det = computeHessianDeterminant();
+			if ( normalize )
+			{
+				// Min & Max.
+				double max = Double.NEGATIVE_INFINITY;
+				double min = Double.POSITIVE_INFINITY;
+				for ( final DoubleType d : det )
+				{
+					final double val = d.get();
+					if ( val > max )
+						max = val;
+					if ( val < min )
+						min = val;
+				}
+				for ( final DoubleType d : det )
+				{
+					final double val = d.get();
+					d.set( ( val - min ) / ( max - min ) );
+				}
+			}
+			spots = DetectionUtils.findLocalMaxima( det, threshold, calibration, radiusXY, doSubPixelLocalization, numThreads );
+		}
+		catch ( final IncompatibleTypeException | InterruptedException | ExecutionException e )
+		{
+			errorMessage = BASE_ERROR_MESSAGE + e.getMessage();
+			e.printStackTrace();
+			return false;
+		}
+
+		final long end = System.currentTimeMillis();
+		this.processingTime = end - start;
+		return true;
+	}
+
+	private final Img< DoubleType > computeHessianDeterminant() throws IncompatibleTypeException, InterruptedException, ExecutionException
+	{
+		final int n = img.numDimensions();
+
+		// Sigmas in pixel units.
+		final double[] radius = new double[] { radiusXY, radiusXY, radiusZ };
+		final double[] sigmas = new double[ n ];
+		for ( int d = 0; d < n; d++ )
+		{
+			final double cal = d < calibration.length ? calibration[ d ] : 1;
+			sigmas[ d ] = radius[ d ] / cal / Math.sqrt( n );
+		}
+
+		// Get a suitable image factory.
+		final long[] gradientDims = new long[ n + 1 ];
+		final long[] hessianDims = new long[ n + 1 ];
+		for ( int d = 0; d < n; d++ )
+		{
+			hessianDims[ d ] = interval.dimension( d );
+			gradientDims[ d ] = interval.dimension( d );
+		}
+		hessianDims[ n ] = n * ( n + 1 ) / 2;
+		gradientDims[ n ] = n;
+		final Dimensions hessianDimensions = FinalDimensions.wrap( hessianDims );
+		final FinalDimensions gradientDimensions = FinalDimensions.wrap( gradientDims );
+		final ImgFactory< DoubleType > factory = Util.getArrayOrCellImgFactory( hessianDimensions, new DoubleType() );
+		final Img< DoubleType > hessian = factory.create( hessianDimensions );
+		final Img< DoubleType > gradient = factory.create( gradientDimensions );
+		final Img< DoubleType > gaussian = factory.create( interval );
+
+		// Handle multithreading.
+		final ExecutorService es = Executors.newFixedThreadPool( numThreads );
+		// Hessian calculation.
+		HessianMatrix.calculateMatrix( img, gaussian,
+				gradient, hessian, new OutOfBoundsBorderFactory<>(), numThreads, es,
+				sigmas );
+
+		// Normalize for pixel size.
+		final IntervalView< DoubleType > H = HessianMatrix.scaleHessianMatrix( hessian, sigmas );
+
+		// Compute determinant.
+		final ToDoubleFunction< RealComposite< DoubleType > > detcalc;
+		if ( n == 2 )
+		{
+			detcalc = ( c ) -> {
+				final double a00 = c.get( 0 ).get();
+				final double a01 = c.get( 1 ).get();
+				final double a11 = c.get( 2 ).get();
+				final double det = a00 * a11 - a01 * a01;
+				return det;
+			};
+		}
+		else
+		{
+			detcalc = ( c ) -> {
+				final double a00 = c.get( 0 ).get();
+				final double a01 = c.get( 1 ).get();
+				final double a02 = c.get( 2 ).get();
+				final double a11 = c.get( 3 ).get();
+				final double a12 = c.get( 4 ).get();
+				final double a22 = c.get( 5 ).get();
+
+				final double x = a11 * a22 - a12 * a12;
+				final double y = a01 * a22 - a02 * a12;
+				final double z = a01 * a12 - a02 * a11;
+
+				final double det = a00 * x - a01 * y + a02 * z;
+				return -det;
+				// Change sign so that bright detections have positive values.
+			};
+		}
+
+		final CompositeIntervalView< DoubleType, RealComposite< DoubleType > > composite = Views.collapseReal( H );
+		final Img< DoubleType > det = factory.create( interval );
+
+		final TaskExecutor taskExecutor = TaskExecutors.forExecutorServiceAndNumTasks( es, numThreads );
+		LoopBuilder.setImages( composite, det ).multiThreaded( taskExecutor ).forEachPixel(
+				( c, d ) -> d.set( detcalc.applyAsDouble( c ) ) );
+
+		return det;
+	}
+
+	@Override
+	public List< Spot > getResult()
+	{
+		return spots;
+	}
+
+	@Override
+	public String getErrorMessage()
+	{
+		return errorMessage;
+	}
+
+	@Override
+	public long getProcessingTime()
+	{
+		return processingTime;
+	}
+
+	@Override
+	public void setNumThreads()
+	{
+		this.numThreads = Runtime.getRuntime().availableProcessors() / 2;
+	}
+
+	@Override
+	public void setNumThreads( final int numThreads )
+	{
+		this.numThreads = numThreads;
+	}
+
+	@Override
+	public int getNumThreads()
+	{
+		return numThreads;
+	}
+}
---
+++ src/main/java/fiji/plugin/trackmate/detection/HessianDetectorFactory.java
@@ -0,0 +1,206 @@
+/*-
+ * #%L
+ * Fiji distribution of ImageJ for the life sciences.
+ * %%
+ * Copyright (C) 2010 - 2022 Fiji developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package fiji.plugin.trackmate.detection;
+
+import static fiji.plugin.trackmate.detection.DetectorKeys.DEFAULT_NORMALIZE;
+import static fiji.plugin.trackmate.detection.DetectorKeys.DEFAULT_RADIUS_Z;
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_DO_MEDIAN_FILTERING;
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_DO_SUBPIXEL_LOCALIZATION;
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_NORMALIZE;
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_RADIUS;
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_RADIUS_Z;
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_TARGET_CHANNEL;
+import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_THRESHOLD;
+import static fiji.plugin.trackmate.io.IOUtils.readBooleanAttribute;
+import static fiji.plugin.trackmate.io.IOUtils.readDoubleAttribute;
+import static fiji.plugin.trackmate.io.IOUtils.readIntegerAttribute;
+import static fiji.plugin.trackmate.io.IOUtils.writeAttribute;
+import static fiji.plugin.trackmate.io.IOUtils.writeDoSubPixel;
+import static fiji.plugin.trackmate.io.IOUtils.writeRadius;
+import static fiji.plugin.trackmate.io.IOUtils.writeTargetChannel;
+import static fiji.plugin.trackmate.io.IOUtils.writeThreshold;
+import static fiji.plugin.trackmate.util.TMUtils.checkMapKeys;
+import static fiji.plugin.trackmate.util.TMUtils.checkParameter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.jdom2.Element;
+import org.scijava.plugin.Plugin;
+
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.Settings;
+import fiji.plugin.trackmate.gui.components.ConfigurationPanel;
+import fiji.plugin.trackmate.gui.components.detector.HessianDetectorConfigurationPanel;
+import fiji.plugin.trackmate.util.TMUtils;
+import net.imglib2.Interval;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.view.Views;
+
+@Plugin( type = SpotDetectorFactory.class )
+public class HessianDetectorFactory< T extends RealType< T > & NativeType< T > > extends LogDetectorFactory< T >
+{
+
+	public static final String DETECTOR_KEY = "HESSIAN_DETECTOR";
+
+	public static final String NAME = "Hessian detector";
+
+	public static final String INFO_TEXT = "<html>"
+			+ "This detector is based on computing the determinant of the <br>"
+			+ "Hessian matrix of the image to detector bright blobs.  "
+			+ "<p>"
+			+ "It can be configured with a different spots size in XY and Z. <br>"
+			+ "It can also return a normalized quality value, scaled from 0 <br>"
+			+ "to 1 for the spots of each time-point."
+			+ "<p>"
+			+ "As discussed in Mikolajczyk et al.(2005), this detector has <br>"
+			+ "a better edge response elimination than the LoG detector and is <br>"
+			+ "suitable for detect spots in images with many strong edges."
+			+ "</html>";
+
+	@Override
+	public SpotDetector< T > getDetector( final Interval interval, final int frame )
+	{
+		final double radiusXY = ( Double ) settings.get( KEY_RADIUS );
+		final double radiusZ = ( Double ) settings.get( KEY_RADIUS_Z );
+		final double thresholdQuality = ( Double ) settings.get( KEY_THRESHOLD );
+		final boolean doSubpixel = ( Boolean ) settings.get( KEY_DO_SUBPIXEL_LOCALIZATION );
+		final boolean normalize = ( Boolean ) settings.get( KEY_NORMALIZE );
+
+		final double[] calibration = TMUtils.getSpatialCalibration( img );
+		final RandomAccessibleInterval< T > imFrame = prepareFrameImg( frame );
+
+		final HessianDetector< T > detector = new HessianDetector<>(
+				Views.extendMirrorDouble( imFrame ),
+				interval,
+				calibration,
+				radiusXY,
+				radiusZ,
+				thresholdQuality,
+				normalize,
+				doSubpixel );
+		detector.setNumThreads( 1 );
+		return detector;
+	}
+
+	@Override
+	public String getKey()
+	{
+		return DETECTOR_KEY;
+	}
+
+	@Override
+	public boolean checkSettings( final Map< String, Object > lSettings )
+	{
+		boolean ok = true;
+		final StringBuilder errorHolder = new StringBuilder();
+		ok = ok & checkParameter( lSettings, KEY_TARGET_CHANNEL, Integer.class, errorHolder );
+		ok = ok & checkParameter( lSettings, KEY_RADIUS, Double.class, errorHolder );
+		ok = ok & checkParameter( lSettings, KEY_RADIUS_Z, Double.class, errorHolder );
+		ok = ok & checkParameter( lSettings, KEY_THRESHOLD, Double.class, errorHolder );
+		ok = ok & checkParameter( lSettings, KEY_DO_SUBPIXEL_LOCALIZATION, Boolean.class, errorHolder );
+		ok = ok & checkParameter( lSettings, KEY_NORMALIZE, Boolean.class, errorHolder );
+		final List< String > mandatoryKeys = new ArrayList<>();
+		mandatoryKeys.add( KEY_TARGET_CHANNEL );
+		mandatoryKeys.add( KEY_RADIUS );
+		mandatoryKeys.add( KEY_RADIUS_Z );
+		mandatoryKeys.add( KEY_THRESHOLD );
+		mandatoryKeys.add( KEY_DO_SUBPIXEL_LOCALIZATION );
+		mandatoryKeys.add( KEY_NORMALIZE );
+		ok = ok & checkMapKeys( lSettings, mandatoryKeys, null, errorHolder );
+		if ( !ok )
+			errorMessage = errorHolder.toString();
+		return ok;
+	}
+
+	@Override
+	public boolean marshall( final Map< String, Object > lSettings, final Element element )
+	{
+		final StringBuilder errorHolder = new StringBuilder();
+		final boolean ok = writeTargetChannel( lSettings, element, errorHolder )
+				&& writeRadius( lSettings, element, errorHolder )
+				&& writeAttribute( lSettings, element, KEY_RADIUS_Z, Double.class, errorHolder )
+				&& writeThreshold( lSettings, element, errorHolder )
+				&& writeAttribute( lSettings, element, KEY_NORMALIZE, Boolean.class, errorHolder )
+				&& writeDoSubPixel( lSettings, element, errorHolder );
+		if ( !ok )
+			errorMessage = errorHolder.toString();
+		return ok;
+	}
+
+	@Override
+	public boolean unmarshall( final Element element, final Map< String, Object > lSettings )
+	{
+		lSettings.clear();
+		final StringBuilder errorHolder = new StringBuilder();
+		boolean ok = true;
+		ok = ok & readDoubleAttribute( element, lSettings, KEY_RADIUS, errorHolder );
+		ok = ok & readDoubleAttribute( element, lSettings, KEY_RADIUS_Z, errorHolder );
+		ok = ok & readDoubleAttribute( element, lSettings, KEY_THRESHOLD, errorHolder );
+		ok = ok & readBooleanAttribute( element, lSettings, KEY_DO_SUBPIXEL_LOCALIZATION, errorHolder );
+		ok = ok & readBooleanAttribute( element, lSettings, KEY_NORMALIZE, errorHolder );
+		ok = ok & readIntegerAttribute( element, lSettings, KEY_TARGET_CHANNEL, errorHolder );
+		if ( !ok )
+		{
+			errorMessage = errorHolder.toString();
+			return false;
+		}
+		return checkSettings( lSettings );
+	}
+
+	@Override
+	public ConfigurationPanel getDetectorConfigurationPanel( final Settings lSettings, final Model model )
+	{
+		return new HessianDetectorConfigurationPanel( lSettings, model, INFO_TEXT, NAME );
+	}
+
+	@Override
+	public String getInfoText()
+	{
+		return INFO_TEXT;
+	}
+
+	@Override
+	public String getName()
+	{
+		return NAME;
+	}
+
+	@Override
+	public Map< String, Object > getDefaultSettings()
+	{
+		final Map< String, Object > lSettings = super.getDefaultSettings();
+		lSettings.remove( KEY_DO_MEDIAN_FILTERING );
+		lSettings.put( KEY_RADIUS_Z, DEFAULT_RADIUS_Z );
+		lSettings.put( KEY_NORMALIZE, DEFAULT_NORMALIZE );
+		return lSettings;
+	}
+
+	@Override
+	public HessianDetectorFactory< T > copy()
+	{
+		return new HessianDetectorFactory< >();
+	}
+}
