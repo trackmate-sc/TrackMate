9f314c095f7977999aea5f0ee44bfe5762113c46
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2022-07-22 17:45:45 +0200

    Code style changes.

--- src/main/java/fiji/plugin/trackmate/util/SpotNeighborhood.java
+++ src/main/java/fiji/plugin/trackmate/util/SpotNeighborhood.java
@@ -36,7 +36,7 @@ import net.imglib2.algorithm.region.localneighborhood.RectangleNeighborhoodGPL;
 import net.imglib2.outofbounds.OutOfBoundsMirrorExpWindowingFactory;
 import net.imglib2.type.numeric.RealType;
 
-public class SpotNeighborhood< T extends RealType< T >> implements Neighborhood< T >
+public class SpotNeighborhood< T extends RealType< T > > implements Neighborhood< T >
 {
 
 	/*
@@ -59,52 +59,44 @@ public class SpotNeighborhood< T extends RealType< T >> implements Neighborhood<
 		// Center
 		this.center = new long[ img.numDimensions() ];
 		for ( int d = 0; d < center.length; d++ )
-		{
 			center[ d ] = Math.round( spot.getFeature( Spot.POSITION_FEATURES[ d ] ).doubleValue() / calibration[ d ] );
-		}
+
 		// Span
 		final long[] span = new long[ img.numDimensions() ];
 		for ( int d = 0; d < span.length; d++ )
-		{
 			span[ d ] = Math.round( spot.getFeature( Spot.RADIUS ) / calibration[ d ] );
-		}
 
 		// Neighborhood
 
 		/*
 		 * We have to detect here whether we were given a 1D image. Trouble is,
-		 * since it is an ImgPlus, it will always be of fim at least 2. So we
+		 * since it is an ImgPlus, it will always be of dim at least 2. So we
 		 * have to test pedantically.
 		 */
 
-		final OutOfBoundsMirrorExpWindowingFactory< T, RandomAccessibleInterval< T >> oob = new OutOfBoundsMirrorExpWindowingFactory<>();
+		final OutOfBoundsMirrorExpWindowingFactory< T, RandomAccessibleInterval< T > > oob = new OutOfBoundsMirrorExpWindowingFactory<>();
 		if ( img.numDimensions() == 2 && img.dimension( 0 ) < 2 || img.dimension( 1 ) < 2 )
 		{
 			if ( img.dimension( 0 ) < 2 )
-			{
 				span[ 0 ] = 0;
-			}
 			else
-			{
 				span[ 1 ] = 0;
-			}
-			this.neighborhood = new RectangleNeighborhoodGPL< >( img, oob );
+			this.neighborhood = new RectangleNeighborhoodGPL<>( img, oob );
 			neighborhood.setPosition( center );
 			neighborhood.setSpan( span );
 		}
 		else if ( img.numDimensions() == 2 )
 		{
-			this.neighborhood = new EllipseNeighborhood< >( img, center, span, oob );
+			this.neighborhood = new EllipseNeighborhood<>( img, center, span, oob );
 		}
 		else if ( img.numDimensions() == 3 )
 		{
-			this.neighborhood = new EllipsoidNeighborhood< >( img, center, span, oob );
+			this.neighborhood = new EllipsoidNeighborhood<>( img, center, span, oob );
 		}
 		else
 		{
 			throw new IllegalArgumentException( "Source input must be 1D, 2D or 3D, got nDims = " + img.numDimensions() );
 		}
-
 	}
 
 	/*
@@ -114,7 +106,7 @@ public class SpotNeighborhood< T extends RealType< T >> implements Neighborhood<
 	@Override
 	public final SpotNeighborhoodCursor< T > cursor()
 	{
-		return new SpotNeighborhoodCursor< >( this );
+		return new SpotNeighborhoodCursor<>( this );
 	}
 
 	@Override
@@ -151,14 +143,12 @@ public class SpotNeighborhood< T extends RealType< T >> implements Neighborhood<
 	public void realMin( final double[] min )
 	{
 		neighborhood.realMin( min );
-
 	}
 
 	@Override
 	public void realMin( final RealPositionable min )
 	{
 		neighborhood.realMin( min );
-
 	}
 
 	@Override
@@ -243,18 +233,14 @@ public class SpotNeighborhood< T extends RealType< T >> implements Neighborhood<
 	public void localize( final int[] position )
 	{
 		for ( int d = 0; d < position.length; d++ )
-		{
 			position[ d ] = ( int ) center[ d ];
-		}
 	}
 
 	@Override
 	public void localize( final long[] position )
 	{
 		for ( int d = 0; d < position.length; d++ )
-		{
 			position[ d ] = center[ d ];
-		}
 	}
 
 	@Override
@@ -273,18 +259,14 @@ public class SpotNeighborhood< T extends RealType< T >> implements Neighborhood<
 	public void localize( final float[] position )
 	{
 		for ( int d = 0; d < position.length; d++ )
-		{
 			position[ d ] = center[ d ];
-		}
 	}
 
 	@Override
 	public void localize( final double[] position )
 	{
 		for ( int d = 0; d < position.length; d++ )
-		{
 			position[ d ] = center[ d ];
-		}
 	}
 
 	@Override
@@ -309,5 +291,4 @@ public class SpotNeighborhood< T extends RealType< T >> implements Neighborhood<
 		final FinalInterval interval = new FinalInterval( min, max );
 		return interval;
 	}
-
 }
