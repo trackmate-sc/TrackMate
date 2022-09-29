7bcdc3a4e1bd8a296c4f7c9cbcaedee41678dfea
Jean-Yves Tinevez <tinevez@pasteur.fr>
2022-09-29 13:59:09 +0200

    Code style changes.

--- src/main/java/fiji/plugin/trackmate/gui/components/XYTextSimpleAnnotation.java
+++ src/main/java/fiji/plugin/trackmate/gui/components/XYTextSimpleAnnotation.java
@@ -34,58 +34,77 @@ import org.jfree.chart.event.AnnotationChangeEvent;
 import org.jfree.chart.plot.PlotRenderingInfo;
 import org.jfree.chart.plot.XYPlot;
 
-public class XYTextSimpleAnnotation extends AbstractXYAnnotation {
-	
+public class XYTextSimpleAnnotation extends AbstractXYAnnotation
+{
+
 	private static final long serialVersionUID = 1L;
+
 	private float x, y;
+
 	private String text;
+
 	private Font font;
+
 	private Color color;
-	private ChartPanel chartPanel;
-	
-	
-	public XYTextSimpleAnnotation(ChartPanel chartPanel) {
+
+	private final ChartPanel chartPanel;
+
+	public XYTextSimpleAnnotation( final ChartPanel chartPanel )
+	{
 		this.chartPanel = chartPanel;
 	}
-	
+
 	/*
 	 * PUBLIC METHOD
 	 */
-	
-	
+
 	@Override
-	public void draw(Graphics2D g2, XYPlot plot, Rectangle2D dataArea,
-			ValueAxis domainAxis, ValueAxis rangeAxis, int rendererIndex,
-			PlotRenderingInfo info) {
-		
-		Rectangle2D box = chartPanel.getScreenDataArea();
-		float sx = (float) plot.getDomainAxis().valueToJava2D(x, box, plot.getDomainAxisEdge());
-		float maxXLim = (float) box.getWidth() - g2.getFontMetrics().stringWidth(text); 
-		if (sx > maxXLim) {
+	public void draw( 
+			final Graphics2D g2, 
+			final XYPlot plot, 
+			final Rectangle2D dataArea,
+			final ValueAxis domainAxis, 
+			final ValueAxis rangeAxis,
+			final int rendererIndex,
+			final PlotRenderingInfo info )
+	{
+
+		final Rectangle2D box = chartPanel.getScreenDataArea();
+		float sx = ( float ) plot.getDomainAxis().valueToJava2D( x, box, plot.getDomainAxisEdge() );
+		final float maxXLim = ( float ) box.getWidth() - g2.getFontMetrics().stringWidth( text );
+		if ( sx > maxXLim )
 			sx = maxXLim;
-		}
-		if (sx < box.getMinX()) {
-			sx = (float) box.getMinX();
-		}
-		
-		float sy = (float) plot.getRangeAxis().valueToJava2D(y, chartPanel.getScreenDataArea(), plot.getRangeAxisEdge());
-		g2.setTransform(new AffineTransform());
-		g2.setColor(color);
-		g2.setFont(font);
-		g2.drawString(text, sx, sy);
-	}
-	
-	public void setLocation(float x, float y) {
-		this.x = x;
-		this.y = y;
-		notifyListeners(new AnnotationChangeEvent(this, this));
-	}
-	
-	public void setText(String text) { this.text = text; }
-	public void setFont(Font font) { this.font = font;	}
-	public void setColor(Color color) {
-		this.color = color; 
-		notifyListeners(new AnnotationChangeEvent(this, this));
+		if ( sx < box.getMinX() )
+			sx = ( float ) box.getMinX();
+
+		final float sy = ( float ) plot.getRangeAxis().valueToJava2D(
+				y, chartPanel.getScreenDataArea(), plot.getRangeAxisEdge() );
+		g2.setTransform( new AffineTransform() );
+		g2.setColor( color );
+		g2.setFont( font );
+		g2.drawString( text, sx, sy );
 	}
 
+	public void setLocation( final float x, final float y )
+	{
+		this.x = x;
+		this.y = y;
+		notifyListeners( new AnnotationChangeEvent( this, this ) );
+	}
+
+	public void setText( final String text )
+	{
+		this.text = text;
+	}
+
+	public void setFont( final Font font )
+	{
+		this.font = font;
+	}
+
+	public void setColor( final Color color )
+	{
+		this.color = color;
+		notifyListeners( new AnnotationChangeEvent( this, this ) );
+	}
 }
