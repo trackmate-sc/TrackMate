ed7f487f2a183fbace2b461071034dc5adab3296
Jean-Yves Tinevez <tinevez@pasteur.fr>
2022-09-29 16:47:37 +0200

    Use the detection preview util in the mask, threshold and label detector config panels.

--- src/main/java/fiji/plugin/trackmate/gui/components/detector/ThresholdDetectorConfigurationPanel.java
+++ src/main/java/fiji/plugin/trackmate/gui/components/detector/ThresholdDetectorConfigurationPanel.java
@@ -25,10 +25,8 @@ import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_TARGET_CHANNEL;
 import static fiji.plugin.trackmate.gui.Fonts.BIG_FONT;
 import static fiji.plugin.trackmate.gui.Fonts.FONT;
 import static fiji.plugin.trackmate.gui.Fonts.SMALL_FONT;
-import static fiji.plugin.trackmate.gui.Icons.PREVIEW_ICON;
 
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -45,7 +43,6 @@ import javax.swing.JSlider;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
-import fiji.plugin.trackmate.Logger;
 import fiji.plugin.trackmate.Model;
 import fiji.plugin.trackmate.Settings;
 import fiji.plugin.trackmate.detection.DetectionUtils;
@@ -54,7 +51,7 @@ import fiji.plugin.trackmate.detection.SpotDetectorFactory;
 import fiji.plugin.trackmate.detection.ThresholdDetectorFactory;
 import fiji.plugin.trackmate.gui.GuiUtils;
 import fiji.plugin.trackmate.gui.components.ConfigurationPanel;
-import fiji.plugin.trackmate.util.JLabelLogger;
+import fiji.plugin.trackmate.util.DetectionPreview;
 import fiji.plugin.trackmate.util.TMUtils;
 import ij.ImagePlus;
 import net.imagej.ImgPlus;
@@ -76,15 +73,6 @@ public class ThresholdDetectorConfigurationPanel extends ConfigurationPanel
 	private static final long serialVersionUID = 1L;
 
 	private static final NumberFormat THRESHOLD_FORMAT = new DecimalFormat( "#.###" );
-
-	private static final String TOOLTIP_PREVIEW = "<html>"
-			+ "Preview the current settings on the current frame."
-			+ "<p>"
-			+ "Advice: change the settings until you get at least <br>"
-			+ "<b>all</b> the spots you want, and do not mind the <br>"
-			+ "spurious spots too much. You will get a chance to <br>"
-			+ "get rid of them later."
-			+ "</html>";
 
 	protected final Settings settings;
 
@@ -137,46 +125,30 @@ public class ThresholdDetectorConfigurationPanel extends ConfigurationPanel
 
 		setPreferredSize( new Dimension( 300, 511 ) );
 		final GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 47 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 47 };
 		gridBagLayout.columnWidths = new int[] { 0, 0, 20 };
 		gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0 };
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 };
+		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, };
 		setLayout( gridBagLayout );
 
-		final JLabel jLabelPreTitle = new JLabel();
-		final GridBagConstraints gbcLabel1 = new GridBagConstraints();
-		gbcLabel1.anchor = GridBagConstraints.NORTHWEST;
-		gbcLabel1.insets = new Insets( 5, 5, 5, 5 );
-		gbcLabel1.gridwidth = 3;
-		gbcLabel1.gridx = 0;
-		gbcLabel1.gridy = 0;
-		this.add( jLabelPreTitle, gbcLabel1 );
-		jLabelPreTitle.setText( "Settings for detector:" );
-		jLabelPreTitle.setFont( FONT );
-
-		final JLabel jLabelDetectorName = new JLabel();
+		final JLabel jLabelDetectorName = new JLabel( detectorName, JLabel.CENTER );
 		jLabelDetectorName.setFont( BIG_FONT );
 		final GridBagConstraints gbclblSegmenterName = new GridBagConstraints();
 		gbclblSegmenterName.anchor = GridBagConstraints.NORTHWEST;
 		gbclblSegmenterName.insets = new Insets( 5, 5, 5, 5 );
 		gbclblSegmenterName.gridwidth = 3;
 		gbclblSegmenterName.gridx = 0;
-		gbclblSegmenterName.gridy = 1;
+		gbclblSegmenterName.gridy = 0;
 		this.add( jLabelDetectorName, gbclblSegmenterName );
-		jLabelDetectorName.setFont( BIG_FONT );
-		jLabelDetectorName.setText( detectorName );
 
-		final JLabel jLabelHelpText = new JLabel();
 		final GridBagConstraints gbcLblHelpText = new GridBagConstraints();
 		gbcLblHelpText.anchor = GridBagConstraints.WEST;
 		gbcLblHelpText.fill = GridBagConstraints.BOTH;
 		gbcLblHelpText.insets = new Insets( 5, 5, 5, 5 );
 		gbcLblHelpText.gridwidth = 3;
 		gbcLblHelpText.gridx = 0;
-		gbcLblHelpText.gridy = 2;
-		this.add( jLabelHelpText, gbcLblHelpText );
-		jLabelHelpText.setFont( FONT.deriveFont( Font.ITALIC ) );
-		jLabelHelpText.setText( infoText.replace( "<br>", "" ).replace( "<p>", "<p align=\"justify\">" ).replace( "<html>", "<html><p align=\"justify\">" ) );
+		gbcLblHelpText.gridy = 1;
+		this.add( GuiUtils.textInScrollPanel( GuiUtils.infoDisplay( infoText ) ), gbcLblHelpText );
 
 		final JLabel labelChannel = new JLabel( "1" );
 		labelChannel.setHorizontalAlignment( SwingConstants.CENTER );
@@ -185,7 +157,7 @@ public class ThresholdDetectorConfigurationPanel extends ConfigurationPanel
 		gbcLblChannel.fill = GridBagConstraints.VERTICAL;
 		gbcLblChannel.insets = new Insets( 5, 5, 5, 5 );
 		gbcLblChannel.gridx = 2;
-		gbcLblChannel.gridy = 4;
+		gbcLblChannel.gridy = 3;
 		add( labelChannel, gbcLblChannel );
 
 		sliderChannel = new JSlider();
@@ -197,14 +169,14 @@ public class ThresholdDetectorConfigurationPanel extends ConfigurationPanel
 		gbcLblDetectInChannel.anchor = GridBagConstraints.EAST;
 		gbcLblDetectInChannel.insets = new Insets( 5, 5, 5, 5 );
 		gbcLblDetectInChannel.gridx = 0;
-		gbcLblDetectInChannel.gridy = 4;
+		gbcLblDetectInChannel.gridy = 3;
 		add( lblDetectInChannel, gbcLblDetectInChannel );
 
 		final GridBagConstraints gbcSliderChannel = new GridBagConstraints();
 		gbcSliderChannel.fill = GridBagConstraints.BOTH;
 		gbcSliderChannel.insets = new Insets( 5, 5, 5, 5 );
 		gbcSliderChannel.gridx = 1;
-		gbcSliderChannel.gridy = 4;
+		gbcSliderChannel.gridy = 3;
 		add( sliderChannel, gbcSliderChannel );
 
 		lblIntensityThreshold = new JLabel( "Intensity threshold:" );
@@ -213,7 +185,7 @@ public class ThresholdDetectorConfigurationPanel extends ConfigurationPanel
 		gbcLblNewLabel.anchor = GridBagConstraints.EAST;
 		gbcLblNewLabel.insets = new Insets( 5, 5, 5, 5 );
 		gbcLblNewLabel.gridx = 0;
-		gbcLblNewLabel.gridy = 5;
+		gbcLblNewLabel.gridy = 4;
 		add( lblIntensityThreshold, gbcLblNewLabel );
 
 		ftfIntensityThreshold = new JFormattedTextField( THRESHOLD_FORMAT );
@@ -225,7 +197,7 @@ public class ThresholdDetectorConfigurationPanel extends ConfigurationPanel
 		gbcFtfIntensityThreshold.fill = GridBagConstraints.HORIZONTAL;
 		gbcFtfIntensityThreshold.insets = new Insets( 5, 5, 5, 5 );
 		gbcFtfIntensityThreshold.gridx = 1;
-		gbcFtfIntensityThreshold.gridy = 5;
+		gbcFtfIntensityThreshold.gridy = 4;
 		add( ftfIntensityThreshold, gbcFtfIntensityThreshold );
 
 		btnAutoThreshold = new JButton( "Auto" );
@@ -233,7 +205,7 @@ public class ThresholdDetectorConfigurationPanel extends ConfigurationPanel
 		final GridBagConstraints gbcBtnAutoThreshold = new GridBagConstraints();
 		gbcBtnAutoThreshold.insets = new Insets( 5, 5, 5, 5 );
 		gbcBtnAutoThreshold.gridx = 2;
-		gbcBtnAutoThreshold.gridy = 5;
+		gbcBtnAutoThreshold.gridy = 4;
 		add( btnAutoThreshold, gbcBtnAutoThreshold );
 
 		chkboxSimplify = new JCheckBox();
@@ -242,33 +214,27 @@ public class ThresholdDetectorConfigurationPanel extends ConfigurationPanel
 		gbChkboxSimplify.insets = new Insets( 5, 5, 5, 5 );
 		gbChkboxSimplify.gridwidth = 3;
 		gbChkboxSimplify.gridx = 0;
-		gbChkboxSimplify.gridy = 6;
+		gbChkboxSimplify.gridy = 5;
 		this.add( chkboxSimplify, gbChkboxSimplify );
 		chkboxSimplify.setText( "Simplify contours." );
 		chkboxSimplify.setFont( FONT );
 
-		final JButton btnPreview = new JButton( "Preview", PREVIEW_ICON );
-		btnPreview.setToolTipText( TOOLTIP_PREVIEW );
+		final DetectionPreview detectionPreview = new DetectionPreview(
+				model,
+				settings,
+				getDetectorFactory(),
+				() -> getSettings(),
+				() -> ( imp.getFrame() - 1 ),
+				null,
+				DetectionUtils.is2D( settings.imp ) ? "Area histogram" : "Volume histogram" );
+
 		final GridBagConstraints gbcBtnPreview = new GridBagConstraints();
-		gbcBtnPreview.anchor = GridBagConstraints.NORTHEAST;
+		gbcBtnPreview.fill = GridBagConstraints.BOTH;
 		gbcBtnPreview.insets = new Insets( 5, 5, 5, 5 );
 		gbcBtnPreview.gridwidth = 3;
 		gbcBtnPreview.gridx = 0;
-		gbcBtnPreview.gridy = 8;
-		this.add( btnPreview, gbcBtnPreview );
-		btnPreview.setFont( SMALL_FONT );
-
-		final JLabelLogger labelLogger = new JLabelLogger();
-		labelLogger.setText( "    " );
-		final GridBagConstraints gbcLblLogger = new GridBagConstraints();
-		gbcLblLogger.insets = new Insets( 5, 5, 5, 5 );
-		gbcLblLogger.anchor = GridBagConstraints.NORTH;
-		gbcLblLogger.fill = GridBagConstraints.HORIZONTAL;
-		gbcLblLogger.gridwidth = 3;
-		gbcLblLogger.gridx = 0;
-		gbcLblLogger.gridy = 9;
-		add( labelLogger, gbcLblLogger );
-		final Logger localLogger = labelLogger.getLogger();
+		gbcBtnPreview.gridy = 7;
+		this.add( detectionPreview.getPanel(), gbcBtnPreview );
 
 		/*
 		 * Deal with channels: the slider and channel labels are only visible if
@@ -291,15 +257,6 @@ public class ThresholdDetectorConfigurationPanel extends ConfigurationPanel
 		}
 
 		btnAutoThreshold.addActionListener( e -> autoThreshold() );
-
-		btnPreview.addActionListener( e -> DetectionUtils.preview(
-				model,
-				settings,
-				getDetectorFactory(),
-				getSettings(),
-				imp.getFrame() - 1,
-				localLogger,
-				b -> btnPreview.setEnabled( b ) ) );
 	}
 
 	/*
