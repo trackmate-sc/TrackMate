cd569765c089387bac4aa0ee6763be3f6fb2f9d0
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-04-16 21:16:21 +0200

    Rework the demo of mesh intersection.
    Much cleaner. Still does not work for not simple meshes.
    Probably because of border cases where we have vertices that lie
    exactly on the plane we are interesecting with.

--- src/test/java/fiji/plugin/trackmate/mesh/MeshPlaneIntersection.java
+++ src/test/java/fiji/plugin/trackmate/mesh/MeshPlaneIntersection.java
@@ -2,181 +2,202 @@ package fiji.plugin.trackmate.mesh;
 
 import java.util.Arrays;
 
-import gnu.trove.iterator.TLongIterator;
 import gnu.trove.list.array.TDoubleArrayList;
 import gnu.trove.list.array.TLongArrayList;
-import gnu.trove.map.hash.TLongObjectHashMap;
-import gnu.trove.set.hash.TLongHashSet;
+import net.imagej.mesh.Edges;
 import net.imagej.mesh.Mesh;
+import net.imagej.mesh.Meshes;
 import net.imagej.mesh.Triangles;
 import net.imagej.mesh.Vertices;
 
 public class MeshPlaneIntersection
 {
 
-	public static double[][] intersect( final Mesh mesh, final double z )
+	/**
+	 * Only works if the {@link Mesh} supports {@link Mesh#edges()}.
+	 *
+	 * @param mesh
+	 * @param z
+	 * @return
+	 */
+	public static void intersect(
+			final Mesh mesh,
+			final double z,
+			final TDoubleArrayList cx,
+			final TDoubleArrayList cy )
 	{
 		/*
-		 * Build the edge and face maps. This could be more efficiently
-		 * implemented in a read-only winged-edge mesh class.
+		 * Clear contour holders.
 		 */
+		cx.resetQuick();
+		cy.resetQuick();
 
-		final Vertices vertices = mesh.vertices();
-		final Triangles triangles = mesh.triangles();
-
-		// Map of edge (va -> vb with always va < vb) to face pair (fa, fb).
-		// They are stored as a paired integers using Szudzik pairing.
-		// So it won't work if the index exceeds a few 100s of millions.
-		final TLongObjectHashMap< long[] > edgeList = new TLongObjectHashMap< long[] >();
-
-		// Iterate through all the faces.
-		final long startTime = System.currentTimeMillis();
-		final long[] vs = new long[ 3 ];
-		final int[] pairHolder = new int[ 2 ];
-		for ( long face = 0; face < triangles.size(); face++ )
-		{
-			vs[ 0 ] = triangles.vertex0( face );
-			vs[ 1 ] = triangles.vertex1( face );
-			vs[ 2 ] = triangles.vertex2( face );
-			Arrays.sort( vs );
-
-			// Deal with the 3 edges.
-			insertEdge( vs[ 0 ], vs[ 1 ], face, edgeList, pairHolder );
-			insertEdge( vs[ 0 ], vs[ 2 ], face, edgeList, pairHolder );
-			insertEdge( vs[ 1 ], vs[ 2 ], face, edgeList, pairHolder );
-		}
-		final long endTime = System.currentTimeMillis();
-		System.out.println( "Built edge and face lists for " + triangles.size() + " faces in " + ( endTime - startTime ) + " ms." );
-
-//		edgeList.forEachEntry( new TLongObjectProcedure< long[] >()
-//		{
-//			private final int[] phK = new int[ 2 ];
-//			
-//			@Override
-//			public boolean execute( final long k, final long[] v )
-//			{
-//				unpair( k, phK );
-//				System.out.println( String.format( "%d, %d -> %d, %d", phK[ 0 ], phK[ 1 ], v[ 0 ], v[ 1 ] ) );
-//				return true;
-//			}
-//		} );
+		/*
+		 * Check if bounding-box intersect. TODO: use a data structure where the
+		 * bounding-box is a field, calculated once.
+		 */
+		final float[] bb = Meshes.boundingBox( mesh );
+		if ( bb[ 2 ] > z )
+			return;
+		if ( bb[ 5 ] < z )
+			return;
 
 		/*
 		 * Find one edge that crosses the Z plane.
 		 */
 
-		final TLongIterator edgeIt = edgeList.keySet().iterator();
+		final Edges edges = mesh.edges();
+		final Vertices vertices = mesh.vertices();
+		final long nEdges = edges.size();
+
 		long start = -1;
-		while ( edgeIt.hasNext() )
+		for ( long e = 0; e < nEdges; e++ )
 		{
-			final long edge = edgeIt.next();
-			unpair( edge, pairHolder );
-			final long va = pairHolder[ 0 ];
-			final long vb = pairHolder[ 1 ];
-			if ( testLineIntersectPlane( vertices, va, vb, z ) )
+			if ( edgeCrossPlane( vertices, edges, e, z ) )
 			{
-				start = edge;
-				break;
+				// Edge is part of a face?
+				final long f0 = edges.f0( e );
+				if ( f0 >= 0 )
+				{
+					start = e;
+					break;
+				}
+				// This edge has no face, we need another one.
 			}
 		}
+		// Cannot build contour based on edge with no faces.
 		if ( start < 0 )
-		{
-			System.out.println( "Could not find an edge that intersects with Z = " + z );
-			return null;
-		}
+			return;
 
-		/*
-		 * Iterate from it, selecting faces that an edge that crosses the plane.
-		 */
+		// Holder for the vertices of a triangle.
+		final long[] vs = new long[ 3 ];
+		// Holder for the 3 edges of a triangle.
+		final long[] es = new long[ 3 ];
 
-		final TDoubleArrayList intersectionX = new TDoubleArrayList();
-		final TDoubleArrayList intersectionY = new TDoubleArrayList();
 		long current = start;
-		long previousFace = -1;
-		final long[][] edges = new long[ 3 ][ 2 ];
-		final TLongHashSet visited = new TLongHashSet();
+		final long startTriangle = edges.f0( start );
+		long previousTriangle = startTriangle;
+		final TLongArrayList visited = new TLongArrayList();
+//		final TLongHashSet visited = new TLongHashSet();
+		visited.add( startTriangle );
 		while ( true )
 		{
-			addEdgeToContour( vertices, current, z, intersectionX, intersectionY, pairHolder );
-			final long face = getNextFace( edgeList, current, previousFace );
-			if ( visited.contains( face ) )
-				break;
+			addEdgeToContour( vertices, edges, current, z, cx, cy );
 
-			final long next = getNextEdge( mesh, edgeList, face, current, z, previousFace, edges, vs );
-			visited.add( face );
-			previousFace = face;
+			final long triangle = getNextTriangle( edges, current, previousTriangle );
+			System.out.println( "At triangle: " + toString( mesh, triangle ) );
+
+			if ( triangle < 0 || visited.contains( triangle ) )
+				return;
+
+			visited.add( triangle );
+			final long next = getNextEdge( mesh, triangle, current, z, vs, es );
+
+			if ( next < 0 || next == start )
+				return;
+
+			previousTriangle = triangle;
 			current = next;
 		}
-
-		return new double[][] { intersectionX.toArray(), intersectionY.toArray() };
 	}
 
-	private static long getNextFace( final TLongObjectHashMap< long[] > edgeList, final long current, final long previousFace )
+	private static String toString( final Mesh mesh, final long triangle )
 	{
-		// Get the faces of this edge.
-		final long[] faces = edgeList.get( current );
-		// Retain the one we have not been visiting.
-		long face;
-		if ( faces[ 0 ] == previousFace )
-			face = faces[ 1 ];
-		else
-			face = faces[ 0 ];
-		return face;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	private static long getNextEdge( final Mesh mesh, final TLongObjectHashMap< long[] > edgeList, final long face, final long current, final double z, final long previousFace, final long[][] edges, final long[] vs )
+	private static boolean edgeCrossPlane( final Vertices vertices, final Edges edges, final long e, final double z )
+	{
+		final double z0 = vertices.z( edges.v0( e ) );
+		final double z1 = vertices.z( edges.v1( e ) );
+		if ( z0 > z && z1 > z )
+			return false;
+		if ( z0 < z && z1 < z )
+			return false;
+		return true;
+	}
+
+	private static long getNextTriangle( final Edges edges, final long e, final long previousFace )
+	{
+		final long f0 = edges.f0( e );
+		if ( f0 == previousFace )
+			return edges.f1( e );
+		return f0;
+	}
+
+	/**
+	 * Returns the index of the edge in the specified triangle that crosses the
+	 * plane with the specified z, and that is different from the specified
+	 * current edge. Returns -1 is such an edge cannot be found for the
+	 * specified triangle.
+	 *
+	 * @param mesh
+	 *            the mesh structure.
+	 * @param face
+	 *            the triangle to inspect.
+	 * @param current
+	 *            the current edge, that should not be returned.
+	 * @param z
+	 *            the value of the Z plane.
+	 * @param vs
+	 *            holder for the vertices of the triangle (size at least 3).
+	 * @param es
+	 *            holder for the edges of the triangle (size at least 3).
+	 * @return the index of the next edge.
+	 */
+	private static long getNextEdge(
+			final Mesh mesh,
+			final long face,
+			final long current,
+			final double z,
+			final long[] vs,
+			final long[] es )
 	{
 		final Triangles triangles = mesh.triangles();
 		final Vertices vertices = mesh.vertices();
+		final Edges edges = mesh.edges();
 
 		// Get the edges of this face.
 		vs[ 0 ] = triangles.vertex0( face );
 		vs[ 1 ] = triangles.vertex1( face );
 		vs[ 2 ] = triangles.vertex2( face );
 		Arrays.sort( vs );
-		edges[ 0 ][ 0 ] = vs[ 0 ];
-		edges[ 0 ][ 1 ] = vs[ 1 ];
-		edges[ 1 ][ 0 ] = vs[ 0 ];
-		edges[ 1 ][ 1 ] = vs[ 2 ];
-		edges[ 2 ][ 0 ] = vs[ 1 ];
-		edges[ 2 ][ 1 ] = vs[ 2 ];
-		for ( final long[] edge : edges )
+		es[ 0 ] = edges.indexOf( vs[ 0 ], vs[ 1 ] );
+		es[ 1 ] = edges.indexOf( vs[ 0 ], vs[ 2 ] );
+		es[ 2 ] = edges.indexOf( vs[ 1 ], vs[ 2 ] );
+		for ( final long e : es )
 		{
-			final long e = pair( edge[ 0 ], edge[ 1 ] );
 			if ( e == current )
 				continue;
 
-			if ( testLineIntersectPlane( vertices, edge[ 0 ], edge[ 1 ], z ) )
-					return e;
+			if ( edgeCrossPlane( vertices, edges, e, z ) )
+				return e;
 		}
 		return -1;
 
 	}
 
-	private static void addEdgeToContour( final Vertices vertices, final long edge, final double z, final TDoubleArrayList cx, final TDoubleArrayList cy, final int[] pairHolder )
+	private static void addEdgeToContour(
+			final Vertices vertices,
+			final Edges edges,
+			final long e,
+			final double z,
+			final TDoubleArrayList cx,
+			final TDoubleArrayList cy )
 	{
-		unpair( edge, pairHolder );
-		final int sv = pairHolder[ 0 ];
-		final int tv = pairHolder[ 1 ];
+		final long sv = edges.v0( e );
+		final long tv = edges.v1( e );
 		final double xs = vertices.x( sv );
 		final double ys = vertices.y( sv );
 		final double zs = vertices.z( sv );
 		final double xt = vertices.x( tv );
 		final double yt = vertices.y( tv );
 		final double zt = vertices.z( tv );
-		double x;
-		double y;
-		if ( zs == zt )
-		{
-			x = 0.5 * ( xs + xt );
-			y = 0.5 * ( ys + yt );
-		}
-		else
-		{
-			final double t = ( z - zs ) / ( zt - zs );
-			x = xs + t * ( xt - xs );
-			y = ys + t * ( yt - ys );
-		}
+		final double t = ( zs == zt )
+				? 0.5 : ( z - zs ) / ( zt - zs );
+		final double x = xs + t * ( xt - xs );
+		final double y = ys + t * ( yt - ys );
 		final int np = cx.size();
 		if ( np > 1 && cx.getQuick( np - 1 ) == x && cy.getQuick( np - 1 ) == y )
 			return; // Don't add duplicate.
@@ -184,98 +205,4 @@ public class MeshPlaneIntersection
 		cx.add( x );
 		cy.add( y );
 	}
-
-	private static void insertEdge( final long va, final long vb, final long face, final TLongObjectHashMap< long[] > edgeList, final int[] pairHolder )
-	{
-		assert va < vb;
-		final long edge = pair( va, vb );
-		final long[] faces = edgeList.get( edge );
-		if ( faces == null )
-		{
-			edgeList.put( edge, new long[] { face, -1 } );
-			return;
-		}
-		faces[ 1 ] = face;
-	}
-
-	private static void insertFaceIntoVertexList( final long vertex, final long face, final TLongObjectHashMap< TLongArrayList > vertexList )
-	{
-		TLongArrayList faceList = vertexList.get( vertex );
-		if ( faceList == null )
-		{
-			faceList = new TLongArrayList();
-			vertexList.put( vertex, faceList );
-		}
-		faceList.add( face );
-
-	}
-
-	private static boolean testLineIntersectPlane( final Vertices vertices, final long source, final long target, final double z )
-	{
-		final double z0 = vertices.z( source );
-		final double z1 = vertices.z( target );
-		if ( ( z0 > z && z1 > z ) || ( z0 < z && z1 < z ) )
-			return false;
-		return true;
-	}
-
-	/**
-	 * Szudzik pairing.
-	 * 
-	 * @param x
-	 *            the 1st int to pair.
-	 * @param y
-	 *            the 2nd int to pair.
-	 * @return Szudzik pairing.
-	 */
-	public static long pair( final double x, final double y )
-	{
-		return ( long ) ( x >= y ? x * x + x + y : y * y + x );
-	}
-
-	/**
-	 * Szudzik unpairing.
-	 * 
-	 * @param z
-	 *            the factor to unpair.
-	 * @param out
-	 *            where to write the results in.
-	 */
-	public static void unpair( final long z, final int[] out )
-	{
-		final long b = ( long ) Math.sqrt( z );
-		final int a = ( int ) ( z - b * b );
-		if ( a < b )
-		{
-			out[ 0 ] = a;
-			out[ 1 ] = ( int ) b;
-		}
-		else
-		{
-			out[ 0 ] = ( int ) b;
-			out[ 1 ] = ( int ) ( a - b );
-		}
-	}
-
-	public static void main( final String[] args )
-	{
-		final int[][] tests = new int[][] {
-				{ 1, 2 },
-				{ 100, 5 },
-				{ 5, 100 },
-				{ 0, 500 },
-				{ 9, 0 },
-				{ 120, 12345678 },
-				{ -1, 50 },
-				{ 20, -1 }
-		};
-		final int[] out = new int[ 2 ];
-		for ( final int[] test : tests )
-		{
-			final long z = pair( test[ 0 ], test[ 1 ] );
-			unpair( z, out );
-			System.out.println( String.format( "%d, %d -> %d -> %d, %d", test[ 0 ], test[ 1 ], z, out[ 0 ], out[ 1 ] ) );
-		}
-	}
-
 }
