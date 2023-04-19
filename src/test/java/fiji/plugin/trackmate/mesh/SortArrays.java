0486b5a9cd8af9f58738fa048d74204166829fbf
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-04-19 22:57:25 +0200

    Utility to sort Trove arrays.

---
+++ src/test/java/fiji/plugin/trackmate/mesh/SortArrays.java
@@ -0,0 +1,143 @@
+package fiji.plugin.trackmate.mesh;
+
+import java.util.BitSet;
+import java.util.Random;
+
+import gnu.trove.list.array.TDoubleArrayList;
+
+/**
+ * Utilities to sort an array and return the sorting index.
+ */
+public class SortArrays
+{
+
+	public static void reorder( final TDoubleArrayList data, final int[] ind )
+	{
+		final BitSet done = new BitSet( data.size() );
+		for ( int i = 0; i < data.size() && done.cardinality() < data.size(); i++ )
+		{
+			int ia = i;
+			int ib = ind[ ia ];
+			if ( done.get( ia ) )
+			{ // index is already done
+				continue;
+			}
+			if ( ia == ib )
+			{ // element is at the right place
+				done.set( ia );
+				continue;
+			}
+			final int x = ia; // start a loop at x = ia
+			// some next index will be x again eventually
+			final double a = data.getQuick( ia );
+			// keep element a as the last value after the loop
+			while ( ib != x && !done.get( ia ) )
+			{
+				final double b = data.getQuick( ib );
+				// element from index b must go to index a
+				data.setQuick( ia, b );
+				done.set( ia );
+				ia = ib;
+				ib = ind[ ia ]; // get next index
+			}
+			data.setQuick( ia, a ); // set value a to last index
+			done.set( ia );
+		}
+	}
+
+	public static int[] quicksort( final TDoubleArrayList main )
+	{
+		final int[] index = new int[ main.size() ];
+		for ( int i = 0; i < index.length; i++ )
+			index[ i ] = i;
+		quicksort( main, index );
+		return index;
+	}
+
+	public static void quicksort( final TDoubleArrayList main, final int[] index )
+	{
+		quicksort( main, index, 0, index.length - 1 );
+	}
+
+	// quicksort a[left] to a[right]
+	public static void quicksort( final TDoubleArrayList a, final int[] index, final int left, final int right )
+	{
+		if ( right <= left )
+			return;
+		final int i = partition( a, index, left, right );
+		quicksort( a, index, left, i - 1 );
+		quicksort( a, index, i + 1, right );
+	}
+
+	// partition a[left] to a[right], assumes left < right
+	private static int partition( final TDoubleArrayList a, final int[] index,
+			final int left, final int right )
+	{
+		int i = left - 1;
+		int j = right;
+		while ( true )
+		{
+			while ( less( a.getQuick( ++i ), a.getQuick( right ) ) )
+				;
+			while ( less( a.getQuick( right ), a.getQuick( --j ) ) )
+				if ( j == left )
+					break; // don't go out-of-bounds
+			if ( i >= j )
+				break; // check if pointers cross
+			exch( a, index, i, j ); // swap two elements into place
+		}
+		exch( a, index, i, right ); // swap with partition element
+		return i;
+	}
+
+	// is x < y ?
+	private static boolean less( final double x, final double y )
+	{
+		return ( x < y );
+	}
+
+	// exchange a[i] and a[j]
+	private static void exch( final TDoubleArrayList a, final int[] index, final int i, final int j )
+	{
+		final double swap = a.getQuick( i );
+		a.setQuick( i, a.getQuick( j ) );
+		a.setQuick( j, swap );
+		final int b = index[ i ];
+		index[ i ] = index[ j ];
+		index[ j ] = b;
+	}
+
+	public static void main( final String[] args )
+	{
+		final Random ran = new Random( 1l );
+		final int n = 10;
+		final TDoubleArrayList arr = new TDoubleArrayList();
+		for ( int i = 0; i < n; i++ )
+			arr.add( ran.nextDouble() );
+
+		final TDoubleArrayList copy = new TDoubleArrayList( arr );
+
+		System.out.print( String.format( "Before sorting: %4.2f", arr.get( 0 ) ) );
+		for ( int i = 1; i < arr.size(); i++ )
+			System.out.print( String.format( ", %4.2f", arr.get( i ) ) );
+		System.out.println();
+
+		final int[] index = quicksort( arr );
+		System.out.print( String.format( "After sorting:  %4.2f", arr.get( 0 ) ) );
+		for ( int i = 1; i < arr.size(); i++ )
+			System.out.print( String.format( ", %4.2f", arr.get( i ) ) );
+		System.out.println();
+
+		System.out.print( String.format( "Index:          %4d", index[ 0 ] ) );
+		for ( int i = 1; i < arr.size(); i++ )
+			System.out.print( String.format( ", %4d", index[ i ] ) );
+		System.out.println();
+
+		reorder( arr, index );
+		System.out.print( String.format( "Reorder copy:   %4.2f", copy.get( 0 ) ) );
+		for ( int i = 1; i < copy.size(); i++ )
+			System.out.print( String.format( ", %4.2f", copy.get( i ) ) );
+		System.out.println();
+	}
+
+}
