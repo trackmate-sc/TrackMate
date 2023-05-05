42bf5dffee485178df41e94ba961cd13e4e12130
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-05-05 17:57:12 +0200

    Rename 2D morphology analyzers.
    To make room for the 3D analyzers.

--- src/main/java/fiji/plugin/trackmate/features/spot/Spot2DMorphologyAnalyzerFactory.java
+++ src/main/java/fiji/plugin/trackmate/features/spot/Spot2DMorphologyAnalyzerFactory.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -26,9 +26,9 @@ import net.imglib2.type.numeric.RealType;
 
 /**
  * Special interface for spot analyzers that can compute feature values based on
- * the contour of spots.
+ * the 2D contour of spots.
  *
  * @author Jean-Yves Tinevez - 2020
  */
-public interface SpotMorphologyAnalyzerFactory< T extends RealType< T > & NativeType< T > > extends SpotAnalyzerFactoryBase< T >
+public interface Spot2DMorphologyAnalyzerFactory< T extends RealType< T > & NativeType< T > > extends SpotAnalyzerFactoryBase< T >
 {}
