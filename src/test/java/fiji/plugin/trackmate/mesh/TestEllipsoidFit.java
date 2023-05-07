27850e960ae5f5206181ab8ca33fd99ac07c8ae4
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2023-05-07 11:53:27 +0200

    JUnit test for the ellipsoid fitter.
    Contains a routine to generate the mesh of an ellipsoid
    programmatically.

---
+++ src/test/java/fiji/plugin/trackmate/mesh/TestEllipsoidFit.java
@@ -0,0 +1,101 @@
+package fiji.plugin.trackmate.mesh;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import fiji.plugin.trackmate.util.mesh.EllipsoidFitter;
+import fiji.plugin.trackmate.util.mesh.EllipsoidFitter.EllipsoidFit;
+import net.imagej.mesh.Mesh;
+import net.imagej.mesh.naive.NaiveDoubleMesh;
+
+public class TestEllipsoidFit
+{
+
+	@Test
+	public void testSimpleEllipsoids()
+	{
+		final double TOLERANCE = 1e-6;
+		
+		final double ra = 1.;
+		final double rb = 2.;
+		for ( double rc = 3.; rc < 10.; rc++ )
+		{
+			final Mesh mesh = generateEllipsoidMesh( ra, rb, rc, -1, -1 );
+			final EllipsoidFit fit = EllipsoidFitter.fit( mesh );
+
+			final double[] arr = new double[ 3 ];
+
+			// Center on 0.
+			fit.center.localize( arr );
+			assertArrayEquals( "Ellipsoid center should be close to 0.", arr, new double[] { 0., 0., 0. }, TOLERANCE );
+
+			// Proper radius, ordered by increasing absolute value.
+			assertEquals( "Smallest radius has unexpected value.", ra, fit.r1, TOLERANCE );
+			assertEquals( "Mid radius has unexpected value.", rb, fit.r2, TOLERANCE );
+			assertEquals( "Largest radius has unexpected value.", rc, fit.r3, TOLERANCE );
+
+			// Vectors, aligned with axes.
+			fit.ev1.localize( arr );
+			for ( int d = 0; d < arr.length; d++ )
+				arr[ d ] = Math.abs( arr[ d ] );
+			assertArrayEquals( "Smallest eigenvector should be aligned with X axis.", arr, new double[] { 1., 0., 0. }, TOLERANCE );
+
+			fit.ev2.localize( arr );
+			for ( int d = 0; d < arr.length; d++ )
+				arr[ d ] = Math.abs( arr[ d ] );
+			assertArrayEquals( "Mid eigenvector should be aligned with Y axis.", arr, new double[] { 0., 1., 0. }, TOLERANCE );
+
+			fit.ev3.localize( arr );
+			for ( int d = 0; d < arr.length; d++ )
+				arr[ d ] = Math.abs( arr[ d ] );
+			assertArrayEquals( "Largest eigenvector should be aligned with Z axis.", arr, new double[] { 0., 0., 1. }, TOLERANCE );
+		}
+	}
+
+	private static Mesh generateEllipsoidMesh( final double ra, final double rb, final double rc, int numLongitudes, int numLatitudes )
+	{
+		if ( numLongitudes < 4 )
+			numLongitudes = 36; // Number of longitudinal divisions
+		if ( numLatitudes < 4 )
+			numLatitudes = 18; // Number of latitudinal divisions
+
+		final NaiveDoubleMesh mesh = new NaiveDoubleMesh();
+		for ( int lat = 0; lat < numLatitudes; lat++ )
+		{
+			final double theta1 = ( double ) lat / numLatitudes * Math.PI;
+			final double theta2 = ( double ) ( lat + 1 ) / numLatitudes * Math.PI;
+
+			for ( int lon = 0; lon < numLongitudes; lon++ )
+			{
+				final double phi1 = ( double ) lon / numLongitudes * 2 * Math.PI;
+				final double phi2 = ( double ) ( lon + 1 ) / numLongitudes * 2 * Math.PI;
+
+				// Calculate the vertices of each triangle
+				final long p1 = addVertex( mesh, ra, rb, rc, theta1, phi1 );
+				final long p2 = addVertex( mesh, ra, rb, rc, theta1, phi2 );
+				final long p3 = addVertex( mesh, ra, rb, rc, theta2, phi1 );
+				final long p4 = addVertex( mesh, ra, rb, rc, theta2, phi2 );
+
+				// Draw the triangles
+				addTriangle( mesh, p1, p3, p2 );
+				addTriangle( mesh, p2, p3, p4 );
+			}
+		}
+		return mesh;
+	}
+
+	private static long addVertex( final Mesh mesh, final double ra, final double rb, final double rc, final double theta, final double phi )
+	{
+		final double x = ra * Math.sin( theta ) * Math.cos( phi );
+		final double y = rb * Math.sin( theta ) * Math.sin( phi );
+		final double z = rc * Math.cos( theta );
+		return mesh.vertices().add( x, y, z );
+	}
+
+	private static long addTriangle( final Mesh mesh, final long p1, final long p2, final long p3 )
+	{
+		return mesh.triangles().add( p1, p2, p3 );
+	}
+}
