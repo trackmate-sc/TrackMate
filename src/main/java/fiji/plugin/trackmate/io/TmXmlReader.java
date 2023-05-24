20265b2a19d5ed684cf6096d8ca72345dea7ea0f
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-05-24 13:34:40 +0200

    Don't crash when loading faulty meshes.

--- src/main/java/fiji/plugin/trackmate/io/TmXmlReader.java
+++ src/main/java/fiji/plugin/trackmate/io/TmXmlReader.java
@@ -988,7 +988,7 @@ public class TmXmlReader
 							spots.remove( spot );
 							spots.add( spotMesh );
 						}
-						catch ( final IOException e )
+						catch ( final Exception e )
 						{
 							ok = false;
 							logger.error( "Problem reading mesh for spot " + id + ":\n"
