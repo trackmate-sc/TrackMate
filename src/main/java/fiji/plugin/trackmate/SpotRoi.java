2682bef2d8435c987349fcbc94741202223289f6
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-09-11 17:03:26 +0200

    Use again the custom 2D iteration routine for SpotRoi.
    I was using it before and trash it in the overhaul. But this one
    is about 30x faster. So let's put it back, with some adapting.
    Noticed by @Mini-Miette

--- src/main/java/fiji/plugin/trackmate/SpotRoi.java
+++ src/main/java/fiji/plugin/trackmate/SpotRoi.java
@@ -22,18 +22,20 @@
 package fiji.plugin.trackmate;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 import gnu.trove.list.array.TDoubleArrayList;
+import net.imglib2.Cursor;
+import net.imglib2.FinalInterval;
 import net.imglib2.IterableInterval;
+import net.imglib2.Localizable;
+import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessible;
-import net.imglib2.roi.IterableRegion;
-import net.imglib2.roi.Masks;
-import net.imglib2.roi.Regions;
-import net.imglib2.roi.geom.GeomMasks;
-import net.imglib2.roi.geom.real.WritablePolygon2D;
-import net.imglib2.type.logic.BoolType;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Intervals;
 import net.imglib2.util.Util;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
 
 public class SpotRoi extends SpotBase
 {
@@ -230,10 +232,7 @@ public class SpotRoi extends SpotBase
 	@Override
 	public < T extends RealType< T > > IterableInterval< T > iterable( final RandomAccessible< T > ra, final double[] calibration )
 	{
-		final double[][] out = toArray( 0., 0., 1 / calibration[ 0 ], 1 / calibration[ 1 ] );
-		final WritablePolygon2D polygon = GeomMasks.closedPolygon2D( out[ 0 ], out[ 1 ] );
-		final IterableRegion< BoolType > region = Masks.toIterableRegion( polygon );
-		return Regions.sample( region, ra );
+		return new SpotRoiIterable<>( this, ra, calibration );
 	}
 
 	private static double radius( final double[] x, final double[] y )
@@ -312,5 +311,255 @@ public class SpotRoi extends SpotBase
 			a += x[ j ] * y[ i ] - x[ i ] * y[ j ];
 
 		return a / 2.;
+	}
+
+	/*
+	 * ITERABLE and ITERATOR.
+	 */
+
+	private static final class SpotRoiIterable< T extends RealType< T > > implements IterableInterval< T >
+	{
+
+		private final FinalInterval interval;
+
+		/** Polygon X coords in pixel units. */
+		private final double[] x;
+
+		/** Polygon Y coords in pixel units. */
+		private final double[] y;
+
+		private final RandomAccessible< T > ra;
+
+		public SpotRoiIterable( final SpotRoi roi, final RandomAccessible< T > ra, final double[] calibration )
+		{
+			this.ra = ra;
+			final double[][] xy = roi.toArray( 0., 0., 1 / calibration[ 0 ], 1 / calibration[ 1 ] );
+			this.x = xy[ 0 ];
+			this.y = xy[ 1 ];
+			final long minX = ( long ) Math.floor( Util.min( x ) );
+			final long maxX = ( long ) Math.ceil( Util.max( x ) );
+			final long minY = ( long ) Math.floor( Util.min( y ) );
+			final long maxY = ( long ) Math.ceil( Util.max( y ) );
+			interval = Intervals.createMinMax( minX, minY, maxX, maxY );
+		}
+
+		@Override
+		public long size()
+		{
+			int n = 0;
+			final Cursor< T > cursor = cursor();
+			while ( cursor.hasNext() )
+			{
+				cursor.fwd();
+				n++;
+			}
+			return n;
+		}
+
+		@Override
+		public T firstElement()
+		{
+			return cursor().next();
+		}
+
+		@Override
+		public Object iterationOrder()
+		{
+			return this;
+		}
+
+		@Override
+		public double realMin( final int d )
+		{
+			return interval.realMin( d );
+		}
+
+		@Override
+		public double realMax( final int d )
+		{
+			return interval.realMax( d );
+		}
+
+		@Override
+		public int numDimensions()
+		{
+			return 2;
+		}
+
+		@Override
+		public long min( final int d )
+		{
+			return interval.min( d );
+		}
+
+		@Override
+		public long max( final int d )
+		{
+			return interval.max( d );
+		}
+
+		@Override
+		public Cursor< T > cursor()
+		{
+			return new MyCursor< T >( x, y, ra );
+		}
+
+		@Override
+		public Cursor< T > localizingCursor()
+		{
+			return cursor();
+		}
+
+		@Override
+		public Iterator< T > iterator()
+		{
+			return cursor();
+		}
+	}
+
+	/**
+	 * Iterates inside a close polygon given by X & Y in pixel coordinates.
+	 * 
+	 * @param <T>
+	 *            the type of pixel in the image.
+	 */
+	private static final class MyCursor< T extends RealType< T > > implements Cursor< T >
+	{
+
+		private final FinalInterval interval;
+
+		private Cursor< T > cursor;
+
+		private final double[] x;
+
+		private final double[] y;
+
+		private boolean hasNext;
+
+		private final RandomAccessible< T > rae;
+
+		private RandomAccess< T > ra;
+
+		public MyCursor( final double[] x, final double[] y, final RandomAccessible< T > rae )
+		{
+			this.x = x;
+			this.y = y;
+			this.rae = rae;
+			final long minX = ( long ) Math.floor( Util.min( x ) );
+			final long maxX = ( long ) Math.ceil( Util.max( x ) );
+			final long minY = ( long ) Math.floor( Util.min( y ) );
+			final long maxY = ( long ) Math.ceil( Util.max( y ) );
+			interval = Intervals.createMinMax( minX, minY, maxX, maxY );
+			reset();
+		}
+
+		@Override
+		public T get()
+		{
+			return ra.get();
+		}
+
+		@Override
+		public void fwd()
+		{
+			ra.setPosition( cursor );
+			fetch();
+		}
+
+		private void fetch()
+		{
+			while ( cursor.hasNext() )
+			{
+				cursor.fwd();
+				if ( isInside( cursor, x, y ) )
+				{
+					hasNext = cursor.hasNext();
+					return;
+				}
+			}
+			hasNext = false;
+		}
+
+		private static final boolean isInside( final Localizable localizable, final double[] x, final double[] y )
+		{
+			// Taken from Imglib2-roi GeomMaths. No edge case.
+			final double xl = localizable.getDoublePosition( 0 );
+			final double yl = localizable.getDoublePosition( 1 );
+
+			int i;
+			int j;
+			boolean inside = false;
+			for ( i = 0, j = x.length - 1; i < x.length; j = i++ )
+			{
+				final double xj = x[ j ];
+				final double yj = y[ j ];
+
+				final double xi = x[ i ];
+				final double yi = y[ i ];
+
+				if ( ( yi > yl ) != ( yj > yl ) && ( xl < ( xj - xi ) * ( yl - yi ) / ( yj - yi ) + xi ) )
+					inside = !inside;
+			}
+			return inside;
+		}
+
+		@Override
+		public void reset()
+		{
+			final IntervalView< T > view = Views.interval( rae, interval );
+			cursor = view.localizingCursor();
+			ra = rae.randomAccess( interval );
+			fetch();
+		}
+
+		@Override
+		public double getDoublePosition( final int d )
+		{
+			return ra.getDoublePosition( d );
+		}
+
+		@Override
+		public int numDimensions()
+		{
+			return 2;
+		}
+
+		@Override
+		public void jumpFwd( final long steps )
+		{
+			for ( int i = 0; i < steps; i++ )
+				fwd();
+		}
+
+		@Override
+		public boolean hasNext()
+		{
+			return hasNext;
+		}
+
+		@Override
+		public T next()
+		{
+			fwd();
+			return get();
+		}
+
+		@Override
+		public long getLongPosition( final int d )
+		{
+			return ra.getLongPosition( d );
+		}
+
+		@Override
+		public Cursor< T > copy()
+		{
+			return new MyCursor<>( x, y, rae );
+		}
+
+		@Override
+		public Cursor< T > copyCursor()
+		{
+			return copy();
+		}
 	}
 }
