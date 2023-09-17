e263f83863e3a50172acba12b50b43137b2af46b
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2023-09-17 13:22:30 +0200

    The Labkit editor can be launched on all time-points at once.
    Shift-click on the button.

--- src/main/java/fiji/plugin/trackmate/gui/editor/LabkitLauncher.java
+++ src/main/java/fiji/plugin/trackmate/gui/editor/LabkitLauncher.java
@@ -2,30 +2,20 @@ package fiji.plugin.trackmate.gui.editor;
 
 import java.awt.Component;
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JRootPane;
 import javax.swing.SwingUtilities;
 
-import org.jgrapht.graph.DefaultWeightedEdge;
 import org.scijava.Context;
 import org.scijava.ui.behaviour.util.AbstractNamedAction;
 
-import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.Logger;
 import fiji.plugin.trackmate.Spot;
 import fiji.plugin.trackmate.SpotCollection;
 import fiji.plugin.trackmate.TrackMate;
-import fiji.plugin.trackmate.detection.DetectionUtils;
-import fiji.plugin.trackmate.detection.MaskUtils;
 import fiji.plugin.trackmate.gui.Icons;
 import fiji.plugin.trackmate.util.EverythingDisablerAndReenabler;
 import fiji.plugin.trackmate.util.SpotUtil;
@@ -39,14 +29,12 @@ import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.display.imagej.ImgPlusViews;
 import net.imglib2.loops.LoopBuilder;
-import net.imglib2.roi.labeling.ImgLabeling;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.view.Views;
 import sc.fiji.labkit.ui.LabkitFrame;
 import sc.fiji.labkit.ui.inputimage.DatasetInputImage;
 import sc.fiji.labkit.ui.labeling.Labeling;
 import sc.fiji.labkit.ui.models.DefaultSegmentationModel;
-import sc.fiji.labkit.ui.models.ImageLabelingModel;
 
 public class LabkitLauncher
 {
@@ -57,13 +45,7 @@ public class LabkitLauncher
 
 	private final EverythingDisablerAndReenabler disabler;
 
-	private ImgPlus< UnsignedShortType > previousIndexImg;
-
 	private int currentTimePoint;
-
-	private final boolean is2D;
-
-	private final double dt;
 
 	public LabkitLauncher( final TrackMate trackmate, final EverythingDisablerAndReenabler disabler )
 	{
@@ -71,36 +53,21 @@ public class LabkitLauncher
 		this.disabler = disabler;
 		final ImagePlus imp = trackmate.getSettings().imp;
 		this.calibration = TMUtils.getSpatialCalibration( imp );
-		this.is2D = DetectionUtils.is2D( imp );
-		this.dt = imp.getCalibration().frameInterval;
 	}
 
-	@SuppressWarnings( { "rawtypes", "unchecked" } )
-	protected void launch()
+	/**
+	 * Launches the Labkit editor.
+	 * 
+	 * @param singleTimePoint
+	 *            if <code>true</code>, will launch the editor using only the
+	 *            time-point currently displayed in the main view. Otherwise,
+	 *            will edit all time-points.
+	 */
+	protected void launch( final boolean singleTimePoint )
 	{
 		final ImagePlus imp = trackmate.getSettings().imp;
-		final ImgPlus src = TMUtils.rawWraps( imp );
-		final int timeAxis = src.dimensionIndex( Axes.TIME );
-
-		// Reslice for current time-point.
-		this.currentTimePoint = imp.getFrame() - 1;
-		final ImgPlus frame = ImgPlusViews.hyperSlice( src, timeAxis, currentTimePoint );
-		final ImpBdvShowable showable = ImpBdvShowable.fromImp( frame, imp );
-		final DatasetInputImage input = new DatasetInputImage( frame, showable );
-
-		// Prepare label image.
-		final AxisType[] axes = new AxisType[] { Axes.X, Axes.Y, Axes.Z, };
-		final long[] dims = new long[ is2D ? 2 : 3 ];
-		for ( int d = 0; d < dims.length; d++ )
-			dims[ d ] = src.dimension( src.dimensionIndex( axes[ d ] ) );
-		final Img< UnsignedShortType > lblImg = ArrayImgs.unsignedShorts( dims );
-		final double[] calibration = TMUtils.getSpatialCalibration( imp );
-		final ImgPlus< UnsignedShortType > lblImgPlus = new ImgPlus<>( lblImg, "LblImg", axes, calibration );
-
-		// Write spots in it with index = id + 1
-		final Iterable< Spot > spotsThisFrame = trackmate.getModel().getSpots().iterable( currentTimePoint, true );
-		for ( final Spot spot : spotsThisFrame )
-			SpotUtil.iterable( spot, lblImgPlus ).forEach( p -> p.set( spot.ID() + 1 ) );
+		final DatasetInputImage input = makeInput( imp, singleTimePoint );
+		final ImgPlus< UnsignedShortType > lblImgPlus = makeLblImage( imp, trackmate.getModel().getSpots(), singleTimePoint );
 
 		// Make a labeling model from it.
 		final Context context = TMUtils.getContext();
@@ -108,243 +75,193 @@ public class LabkitLauncher
 		model.imageLabelingModel().labeling().set( Labeling.fromImg( lblImgPlus ) );
 
 		// Store a copy.
-		this.previousIndexImg = lblImgPlus.copy();
+		final ImgPlus< UnsignedShortType > previousIndexImg = lblImgPlus.copy();
 
 		// Show LabKit.
 		final LabkitFrame labkit = LabkitFrame.show( model, "Edit TrackMate data frame " + ( currentTimePoint + 1 ) );
-		labkit.onCloseListeners().addListener( () -> reimportData( model.imageLabelingModel(), currentTimePoint ) );
-	}
 
-	@SuppressWarnings( "unchecked" )
-	private void reimportData( final ImageLabelingModel lm, final int currentTimePoint )
-	{
-		try
-		{
-			/*
-			 * We will update the spots using a comparison based on only the
-			 * index images.
-			 */
-			final Labeling labeling = lm.labeling().get();
-			final RandomAccessibleInterval< UnsignedShortType > novelIndexImg = ( RandomAccessibleInterval< UnsignedShortType > ) labeling.getIndexImg();
-
-			// Collect ids of spots that have been modified. id = index - 1
-			final Set< Integer > modifiedIDs = getModifiedIDs( novelIndexImg );
-			final int nModified = modifiedIDs.size();
-
-			if ( nModified == 0 )
-				return;
-
-			// Message the user.
-			final String msg =
-					"Commit the changes made to the\n"
-							+ "segmentation in frame " + ( currentTimePoint + 1 ) + "?";
-			final String title = "Commit edits to TrackMate";
-			final int returnedValue = JOptionPane.showConfirmDialog(
-					null,
-					msg,
-					title,
-					JOptionPane.YES_NO_OPTION,
-					JOptionPane.QUESTION_MESSAGE,
-					Icons.TRACKMATE_ICON );
-			if ( returnedValue != JOptionPane.YES_OPTION )
-				return;
-
-			final Model model = trackmate.getModel();
-			model.beginUpdate();
+		// Prepare re-importer.
+		final double dt = imp.getCalibration().frameInterval;
+		final LabkitImporter reimporter = new LabkitImporter( trackmate.getModel(), calibration, dt );
+		labkit.onCloseListeners().addListener( () -> {
 			try
 			{
-				// Map of previous spots against their ID:
-				final SpotCollection spots = model.getSpots();
-				final Map< Integer, Spot > previousSpotIDs = new HashMap<>();
-				spots.iterable( currentTimePoint, true ).forEach( s -> previousSpotIDs.put( Integer.valueOf( s.ID() ), s ) );
+				@SuppressWarnings( "unchecked" )
+				final RandomAccessibleInterval< UnsignedShortType > indexImg = ( RandomAccessibleInterval< UnsignedShortType > ) model.imageLabelingModel().labeling().get().getIndexImg();
 
-				/*
-				 * Get all the spots present in the new image. Because we
-				 * specified the novel label image as 'quality' image, they have
-				 * a quality value equal to the index in the label image (id+1).
-				 */
-				final List< Spot > novelSpots = getSpots( novelIndexImg );
+				// Do we have something to reimport?
+				final AtomicBoolean modified = new AtomicBoolean( false );
+				LoopBuilder.setImages( previousIndexImg, indexImg )
+						.multiThreaded()
+						.forEachChunk( chunk -> {
+							if ( modified.get() )
+								return null;
+							chunk.forEachPixel( ( p1, p2 ) -> {
+								if ( p1.get() != p2.get() )
+								{
+									modified.set( true );
+									return;
+								}
+							} );
+							return null;
+						} );
+				if ( !modified.get() )
+					return;
 
-				/*
-				 * Map of novel spots against the ID taken from the index of the
-				 * novel label image. Normally, this index, and hence the novel
-				 * id, corresponds to the id of previous spots. If one of the
-				 * novel spot has an id we cannot find in the previous spot
-				 * list, it means that it is a new one.
-				 *
-				 * Careful! The user might have created several connected
-				 * components with the same label in LabKit, which will result
-				 * in having several spots with the same quality value. We don't
-				 * want to loose them, so the map is that of a id to a list of
-				 * spots.
-				 */
-				final Map< Integer, List< Spot > > novelSpotIDs = new HashMap<>();
-				novelSpots.forEach( s -> {
-					final int id = Integer.valueOf( s.getFeature( Spot.QUALITY ).intValue() - 1 );
-					final List< Spot > list = novelSpotIDs.computeIfAbsent( Integer.valueOf( id ), ( i ) -> new ArrayList<>() );
-					list.add( s );
-				} );
+				// Message the user.
+				final String msg = ( currentTimePoint < 0 )
+						? "Commit the changes made to the\n"
+								+ "segmentation in whole movie?"
+						: "Commit the changes made to the\n"
+								+ "segmentation in frame " + ( currentTimePoint + 1 ) + "?";
+				final String title = "Commit edits to TrackMate";
+				final int returnedValue = JOptionPane.showConfirmDialog(
+						null,
+						msg,
+						title,
+						JOptionPane.YES_NO_OPTION,
+						JOptionPane.QUESTION_MESSAGE,
+						Icons.TRACKMATE_ICON );
+				if ( returnedValue != JOptionPane.YES_OPTION )
+					return;
 
-				// Update model for those spots.
-				for ( final int id : modifiedIDs )
+				final int timeDim = 2;
+				if ( currentTimePoint < 0 )
 				{
-					final Spot previousSpot = previousSpotIDs.get( Integer.valueOf( id ) );
-					final List< Spot > novelSpotList = novelSpotIDs.get( Integer.valueOf( id ) );
-					if ( previousSpot == null )
+					// All time-points.
+					final Logger log = Logger.IJ_LOGGER;
+					log.setStatus( "Re-importing from Labkit..." );
+					for ( int t = 0; t < imp.getNFrames(); t++ )
 					{
-						/*
-						 * A new one (possible several) I cannot find in the
-						 * previous list -> add as a new spot.
-						 */
-						addNewSpot( novelSpotList );
+						final RandomAccessibleInterval< UnsignedShortType > novelIndexImgThisFrame = Views.hyperSlice( indexImg, timeDim, t );
+						final RandomAccessibleInterval< UnsignedShortType > previousIndexImgThisFrame = Views.hyperSlice( previousIndexImg, timeDim, t );
+						reimporter.reimport( novelIndexImgThisFrame, previousIndexImgThisFrame, t );
+						log.setProgress( ++t / ( double ) imp.getNFrames() );
 					}
-					else if ( novelSpotList == null || novelSpotList.isEmpty() )
-					{
-						/*
-						 * One I add in the previous spot list, but that has
-						 * disappeared. Remove it.
-						 */
-						model.removeSpot( previousSpot );
-					}
-					else
-					{
-						/*
-						 * I know of them both. Treat the case as if the
-						 * previous spot was modified.
-						 */
-						modifySpot( novelSpotList, previousSpot );
-					}
+					log.setStatus( "" );
+					log.setProgress( 0. );
 				}
+				else
+				{
+					// Only one.
+					reimporter.reimport( indexImg, previousIndexImg, currentTimePoint );
+				}
+			}
+			catch ( final Exception e )
+			{
+				e.printStackTrace();
 			}
 			finally
 			{
-				model.endUpdate();
+				disabler.reenable();
 			}
-		}
-		catch ( final Exception e )
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			disabler.reenable();
-		}
+		} );
 	}
 
-	private void modifySpot( final List< Spot > novelSpotList, final Spot previousSpot )
+	/**
+	 * Creates a new {@link DatasetInputImage} from the specified
+	 * {@link ImagePlus}. The embedded label image is empty.
+	 * 
+	 * @param imp
+	 *            the input {@link ImagePlus}.
+	 * @param singleTimePoint
+	 *            if <code>true</code>, then the dataset will be created only
+	 *            for the time-point currently displayed in the
+	 *            {@link ImagePlus}. This time-point is then stored in the
+	 *            {@link #currentTimePoint} field. If <code>false</code>, the
+	 *            dataset is created for the whole movie and the
+	 *            {@link #currentTimePoint} takes the value -1.
+	 * @return a new {@link DatasetInputImage}.
+	 */
+	@SuppressWarnings( { "rawtypes", "unchecked" } )
+	private final DatasetInputImage makeInput( final ImagePlus imp, final boolean singleTimePoint )
 	{
-		final Model model = trackmate.getModel();
-
-		/*
-		 * Hopefully there is only one spot in the novel spot list. If not we
-		 * privilege the one closest to the previous spots.
-		 */
-		final Spot mainNovelSpot;
-		if ( novelSpotList.size() == 1 )
+		final ImgPlus src = TMUtils.rawWraps( imp );
+		// Possibly reslice for current time-point.
+		final ImpBdvShowable showable;
+		final ImgPlus inputImg;
+		if ( singleTimePoint )
 		{
-			mainNovelSpot = novelSpotList.get( 0 );
+			this.currentTimePoint = imp.getFrame() - 1;
+			final int timeAxis = src.dimensionIndex( Axes.TIME );
+			inputImg = ImgPlusViews.hyperSlice( src, timeAxis, currentTimePoint );
+			showable = ImpBdvShowable.fromImp( inputImg, imp );
 		}
 		else
 		{
-			Spot closest = null;
-			double minD2 = Double.POSITIVE_INFINITY;
-			for ( final Spot s : novelSpotList )
+			this.currentTimePoint = -1;
+			showable = ImpBdvShowable.fromImp( imp );
+			inputImg = src;
+		}
+		return new DatasetInputImage( inputImg, showable );
+	}
+
+	/**
+	 * Prepare the label image for annotation.
+	 * 
+	 * @param imp
+	 *            the source image plus.
+	 * @param spots
+	 *            the spot collection.
+	 * @param singleTimePoint
+	 *            if <code>true</code> we only annotate one time-point.
+	 * @return a new {@link ImgPlus}.
+	 */
+	private ImgPlus< UnsignedShortType > makeLblImage( final ImagePlus imp, final SpotCollection spots, final boolean singleTimePoint )
+	{
+		// Careful: Only works for 2D images! FIXME
+
+		// Axes.
+		final AxisType[] axes = ( singleTimePoint )
+				? new AxisType[] { Axes.X, Axes.Y }
+				: new AxisType[] { Axes.X, Axes.Y, Axes.TIME };
+
+		// N dimensions.
+		final int timeDim = 2;
+		final int nDims = singleTimePoint ? 2 : 3;
+
+		// Dimensions.
+		final long[] dims = new long[ nDims ];
+		dims[ 0 ] = imp.getWidth();
+		dims[ 1 ] = imp.getHeight();
+		if ( !singleTimePoint )
+			dims[ timeDim ] = imp.getNFrames();
+
+		// Raw image.
+		final Img< UnsignedShortType > lblImg = ArrayImgs.unsignedShorts( dims );
+
+		// Calibration.
+		final double[] c = TMUtils.getSpatialCalibration( imp );
+		final double[] calibration = new double[ nDims ];
+		for ( int i = 0; i < 2; i++ )
+			calibration[ i ] = c[ i ];
+		if ( !singleTimePoint )
+			calibration[ timeDim ] = 1.;
+
+		// Label image holder.
+		final ImgPlus< UnsignedShortType > lblImgPlus = new ImgPlus<>( lblImg, "LblImg", axes, calibration );
+
+		// Write spots in it with index = id + 1
+		if ( singleTimePoint )
+		{
+			processFrame( lblImgPlus, spots, currentTimePoint );
+		}
+		else
+		{
+			for ( int t = 0; t < imp.getNFrames(); t++ )
 			{
-				final double d2 = s.squareDistanceTo( previousSpot );
-				if ( d2 < minD2 )
-				{
-					minD2 = d2;
-					closest = s;
-				}
+				final ImgPlus< UnsignedShortType > lblImgPlusThisFrame = ImgPlusViews.hyperSlice( lblImgPlus, timeDim, t );
+				processFrame( lblImgPlusThisFrame, spots, t );
 			}
-			mainNovelSpot = closest;
 		}
-
-		// Add it properly.
-		mainNovelSpot.setName( previousSpot.getName() );
-		mainNovelSpot.putFeature( Spot.POSITION_T, currentTimePoint * dt );
-		mainNovelSpot.putFeature( Spot.QUALITY, -1. );
-		model.addSpotTo( mainNovelSpot, Integer.valueOf( currentTimePoint ) );
-		// Recreate links.
-		final Set< DefaultWeightedEdge > edges = model.getTrackModel().edgesOf( previousSpot );
-		for ( final DefaultWeightedEdge e : edges )
-		{
-			final double weight = model.getTrackModel().getEdgeWeight( e );
-			final Spot source = model.getTrackModel().getEdgeSource( e );
-			final Spot target = model.getTrackModel().getEdgeTarget( e );
-			if ( source == previousSpot )
-				model.addEdge( mainNovelSpot, target, weight );
-			else if ( target == previousSpot )
-				model.addEdge( source, mainNovelSpot, weight );
-			else
-				throw new IllegalArgumentException( "The edge of a spot does not have the spot as source or target?!?" );
-		}
-		model.removeSpot( previousSpot );
-
-		// Deal with the other ones.
-		final HashSet< Spot > extraSpots = new HashSet<>( novelSpotList );
-		extraSpots.remove( mainNovelSpot );
-		int i = 1;
-		for ( final Spot s : extraSpots )
-		{
-			s.setName( previousSpot.getName() + "_" + i++ );
-			s.putFeature( Spot.POSITION_T, currentTimePoint * dt );
-			s.putFeature( Spot.QUALITY, -1. );
-			model.addSpotTo( s, Integer.valueOf( currentTimePoint ) );
-		}
+		return lblImgPlus;
 	}
 
-	private void addNewSpot( final List< Spot > novelSpotList )
+	private static final void processFrame( final ImgPlus< UnsignedShortType > lblImgPlus, final SpotCollection spots, final int t )
 	{
-		for ( final Spot spot : novelSpotList )
-		{
-			spot.putFeature( Spot.POSITION_T, currentTimePoint * dt );
-			spot.putFeature( Spot.QUALITY, -1. );
-			trackmate.getModel().addSpotTo( spot, Integer.valueOf( currentTimePoint ) );
-		}
-	}
-
-	private final Set< Integer > getModifiedIDs( final RandomAccessibleInterval< UnsignedShortType > novelIndexImg )
-	{
-		final ConcurrentSkipListSet< Integer > modifiedIDs = new ConcurrentSkipListSet<>();
-		LoopBuilder.setImages( novelIndexImg, previousIndexImg )
-				.multiThreaded( false )
-				.forEachPixel( ( c, p ) -> {
-					final int ci = c.get();
-					final int pi = p.get();
-					if ( ci == 0 && pi == 0 )
-						return;
-					if ( ci != pi )
-					{
-						modifiedIDs.add( Integer.valueOf( pi - 1 ) );
-						modifiedIDs.add( Integer.valueOf( ci - 1 ) );
-					}
-				} );
-		modifiedIDs.remove( Integer.valueOf( -1 ) );
-		return modifiedIDs;
-	}
-
-	private List< Spot > getSpots( final RandomAccessibleInterval< UnsignedShortType > rai )
-	{
-		// Get all labels.
-		final AtomicInteger max = new AtomicInteger( 0 );
-		Views.iterable( rai ).forEach( p -> {
-			final int val = p.getInteger();
-			if ( val != 0 && val > max.get() )
-				max.set( val );
-		} );
-		final List< Integer > indices = new ArrayList<>( max.get() );
-		for ( int i = 0; i < max.get(); i++ )
-			indices.add( Integer.valueOf( i + 1 ) );
-
-		final ImgLabeling< Integer, ? > labeling = ImgLabeling.fromImageAndLabels( rai, indices );
-		final boolean simplify = true;
-		return MaskUtils.fromLabelingWithROI(
-				labeling,
-				labeling,
-				calibration,
-				simplify,
-				rai );
+		final Iterable< Spot > spotsThisFrame = spots.iterable( t, true );
+		for ( final Spot spot : spotsThisFrame )
+			SpotUtil.iterable( spot, lblImgPlus ).forEach( p -> p.set( spot.ID() + 1 ) );
 	}
 
 	public static final AbstractNamedAction getLaunchAction( final TrackMate trackmate )
@@ -362,13 +279,18 @@ public class LabkitLauncher
 					@Override
 					public void run()
 					{
+						// Is shift pressed?
+						final int mod = ae.getModifiers();
+						final boolean shiftPressed = ( mod & ActionEvent.SHIFT_MASK ) > 0;
+						final boolean singleTimepoint = !shiftPressed;
+
 						final JRootPane parent = SwingUtilities.getRootPane( ( Component ) ae.getSource() );
 						final EverythingDisablerAndReenabler disabler = new EverythingDisablerAndReenabler( parent, new Class[] { JLabel.class } );
 						disabler.disable();
 						try
 						{
 							final LabkitLauncher launcher = new LabkitLauncher( trackmate, disabler );
-							launcher.launch();
+							launcher.launch( singleTimepoint );
 						}
 						catch ( final Exception e )
 						{
