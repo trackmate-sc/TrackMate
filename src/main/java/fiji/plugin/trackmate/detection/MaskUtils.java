bb97d01e6891b9478d9ca032267d1dd24e14eafc
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2023-11-22 18:12:56 +0100

    WIP: Smooth mask or label before generating mesh to smooth it.
    Incomplete: miss the 2D case.

--- src/main/java/fiji/plugin/trackmate/detection/MaskUtils.java
+++ src/main/java/fiji/plugin/trackmate/detection/MaskUtils.java
@@ -409,6 +409,11 @@ public class MaskUtils
 	 *            smoother and contain less points.
 	 * @param numThreads
 	 *            how many threads to use for multithreaded computation.
+	 * @param smoothingScale
+	 *            if strictly larger than 0, the mask will be smoothed before
+	 *            creating the mesh, resulting in smoother meshes. The scale
+	 *            value sets the (Gaussian) filter radius and is specified in
+	 *            physical units. If 0 or lower than 0, no smoothing is applied.
 	 * @param qualityImage
 	 *            the image in which to read the quality value.
 	 * @return a list of spots, with ROI.
@@ -418,6 +423,7 @@ public class MaskUtils
 			final Interval interval, 
 			final double[] calibration, 
 			final boolean simplify, 
+			final double smoothingScale,
 			final int numThreads, 
 			final RandomAccessibleInterval< S > qualityImage )
 	{
@@ -442,6 +448,7 @@ public class MaskUtils
 					interval,
 					calibration,
 					simplify,
+					smoothingScale,
 					qualityImage );
 		}
 		else
