2810fa372129853426e0f7250a41db9516760116
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2023-11-23 19:59:57 +0100

    Tweak the slider panels.
    - Mouse wheel listener.
    - Method to dis/enable all sub component.

--- src/main/java/fiji/plugin/trackmate/gui/displaysettings/SliderPanelDouble.java
+++ src/main/java/fiji/plugin/trackmate/gui/displaysettings/SliderPanelDouble.java
@@ -25,6 +25,8 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -131,6 +133,22 @@ public class SliderPanelDouble extends JPanel implements BoundedValueDouble.Upda
 		add( slider, BorderLayout.CENTER );
 		add( spinner, BorderLayout.EAST );
 
+		final MouseWheelListener mouseWheelListener = new MouseWheelListener()
+		{
+
+			@Override
+			public void mouseWheelMoved( final MouseWheelEvent e )
+			{
+				if ( !slider.isEnabled() )
+					return;
+				final int notches = e.getWheelRotation();
+				final int step = notches < 0 ? 1 : -1;
+				slider.setValue( slider.getValue() + step );
+			}
+		};
+		slider.addMouseWheelListener( mouseWheelListener );
+		spinner.addMouseWheelListener( mouseWheelListener );
+
 		this.model = model;
 		model.setUpdateListener( this );
 	}
@@ -152,6 +170,14 @@ public class SliderPanelDouble extends JPanel implements BoundedValueDouble.Upda
 	public void setNumColummns( final int cols )
 	{
 		( ( JSpinner.NumberEditor ) spinner.getEditor() ).getTextField().setColumns( cols );
+	}
+
+	@Override
+	public void setEnabled( final boolean enabled )
+	{
+		spinner.setEnabled( enabled );
+		slider.setEnabled( enabled );
+		super.setEnabled( enabled );
 	}
 
 	@Override
