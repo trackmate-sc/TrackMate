e8efb1be7a10c4a356aa57b368877f035cf67b5f
Jean-Yves Tinevez <tinevez@pasteur.fr>
2023-12-04 11:27:47 +0100

    Fix compile error due to casting error.

--- src/main/java/fiji/plugin/trackmate/detection/MaskUtils.java
+++ src/main/java/fiji/plugin/trackmate/detection/MaskUtils.java
@@ -489,7 +489,7 @@ public class MaskUtils
 	 *            the image in which to read the quality value.
 	 * @return a list of spots, with ROI.
 	 */
-	@SuppressWarnings( "unchecked" )
+	@SuppressWarnings( { "unchecked", "rawtypes" } )
 	public static final < T extends RealType< T > & NativeType< T >, S extends RealType< S > > List< Spot > fromThresholdWithROI(
 			final RandomAccessible< T > input,
 			final Interval interval,
@@ -516,7 +516,7 @@ public class MaskUtils
 			for ( int d = 0; d < sigmas.length; d++ )
 				sigmas[ d ] = smoothingScale / Math.sqrt( in.numDimensions() ) / calibration[ d ];
 
-			filtered = ( RandomAccessibleInterval< T > ) Util.getArrayOrCellImgFactory( in, new FloatType() ).create( in );
+			filtered = ( RandomAccessibleInterval ) Util.getArrayOrCellImgFactory( in, new FloatType() ).create( in );
 			Parallelization.runWithNumThreads( numThreads,
 					() -> Gauss3.gauss( sigmas, Views.extendMirrorDouble( in ), filtered ) );
 		}
