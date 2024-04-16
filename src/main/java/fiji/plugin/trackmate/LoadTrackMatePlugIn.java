158025f5ea1c2f4e195c5434ea5376cfb712ee3e
Jean-Yves Tinevez <tinevez@pasteur.fr>
2024-04-16 17:55:44 +0200

    Make it possible to open TrackMate files without a settings element.

--- src/main/java/fiji/plugin/trackmate/LoadTrackMatePlugIn.java
+++ src/main/java/fiji/plugin/trackmate/LoadTrackMatePlugIn.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -139,9 +139,15 @@ public class LoadTrackMatePlugIn extends TrackMatePlugIn
 		 * Read settings.
 		 */
 
-		final Settings settings = reader.readSettings( imp );
+		Settings settings = reader.readSettings( imp );
 		if ( !reader.isReadingOk() )
 			logger.error( "Problem reading the settings:\n" + reader.getErrorMessage() );
+
+		if ( settings == null )
+		{
+			logger.error( "Problem reading the settings:\n" + "The file did not contain a settings element. Using default values." );
+			settings = new Settings( imp );
+		}
 
 		/*
 		 * Declare the analyzers that are in the settings to the model. This is
