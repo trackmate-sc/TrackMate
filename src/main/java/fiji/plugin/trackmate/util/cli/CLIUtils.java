7169304f16b1c6cc2642d42e39323650f2e4163d
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2024-06-23 18:23:45 +0200

    Utility to read the list of conda envs.

--- src/main/java/fiji/plugin/trackmate/util/cli/CLIUtils.java
+++ src/main/java/fiji/plugin/trackmate/util/cli/CLIUtils.java
@@ -1,12 +1,18 @@
 package fiji.plugin.trackmate.util.cli;
 
 import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,7 +21,65 @@ import org.apache.commons.io.input.TailerListenerAdapter;
 import fiji.plugin.trackmate.Logger;
 
 public class CLIUtils
+
 {
+
+	public static String CONDA_COMMAND = "conda";
+
+	public static List< String > getEnvList()
+	{
+		final List< String > envs = new ArrayList<>();
+		try
+		{
+			// Create tmp files.
+			final Path maskTmpFolder = Files.createTempDirectory( "CLIUtils_" );
+			final File outputFile = maskTmpFolder.resolve( "output.txt" ).toFile();
+			final File errorFile = maskTmpFolder.resolve( "error.txt" ).toFile();
+			final List< String > cmd = Arrays.asList( "cmd.exe", "/c", CONDA_COMMAND, "env", "list" );
+			final ProcessBuilder pb = new ProcessBuilder( cmd );
+			pb.redirectOutput( ProcessBuilder.Redirect.appendTo( outputFile ) );
+			pb.redirectError( ProcessBuilder.Redirect.appendTo( errorFile ) );
+			final Process process = pb.start();
+			final int exitValue = process.waitFor();
+			if ( exitValue == 0 )
+			{
+				String line;
+				try (BufferedReader reader = new BufferedReader( new FileReader( outputFile ) ))
+				{
+					while ( ( line = reader.readLine() ) != null )
+					{
+						if ( !line.startsWith( "#" ) )
+						{ // skip lines that start with #
+							final int indexOfFirstSpace = line.indexOf( ' ' );
+							if ( indexOfFirstSpace != -1 )
+							{ // check if there is a space in the line
+								final String firstColumn = line.substring( 0, indexOfFirstSpace );
+								envs.add( firstColumn );
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				String line;
+				try (BufferedReader reader = new BufferedReader( new FileReader( errorFile ) ))
+				{
+					while ( ( line = reader.readLine() ) != null )
+						System.err.println( line );
+				}
+			}
+		}
+		catch ( final IOException e )
+		{
+			e.printStackTrace();
+		}
+		catch ( final InterruptedException e )
+		{
+			e.printStackTrace();
+		}
+		return envs;
+	}
 
 	/**
 	 * Add a hook to delete the content of given path when Fiji quits. Taken
@@ -106,4 +170,8 @@ public class CLIUtils
 		}
 	}
 
+	public static void main( final String[] args )
+	{
+		System.out.println( getEnvList() );
+	}
 }
