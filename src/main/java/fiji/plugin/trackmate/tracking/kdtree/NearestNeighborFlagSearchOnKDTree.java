6eb47f6956dc1c02034378fbe3f8f61fe547de58
Jean-Yves Tinevez <tinevez@pasteur.fr>
2024-11-06 10:04:41 +0100

    Fix compile error with new version of imglib2.
    Focuses on the rewrite of KDtree. The left and right fields of
    KDTreeNode are not visible anymore. The implementation changed to be
    proxy-based, so the corresponding methods, used in this commit to fix
    the compile errors, are deprectaed.
    
    We therefore shall also full rewrite the KDtree NN tracker using
    the proxy approach.

--- src/main/java/fiji/plugin/trackmate/tracking/kdtree/NearestNeighborFlagSearchOnKDTree.java
+++ src/main/java/fiji/plugin/trackmate/tracking/kdtree/NearestNeighborFlagSearchOnKDTree.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -78,8 +78,9 @@ public class NearestNeighborFlagSearchOnKDTree< T > implements NearestNeighborSe
 		final boolean leftIsNearBranch = axisDiff < 0;
 
 		// search the near branch
-		final KDTreeNode< FlagNode< T > > nearChild = leftIsNearBranch ? current.left : current.right;
-		final KDTreeNode< FlagNode< T > > awayChild = leftIsNearBranch ? current.right : current.left;
+		// TODO Rewrite to remove deprecated methods, proxy object based.
+		final KDTreeNode< FlagNode< T > > nearChild = leftIsNearBranch ? current.left() : current.right();
+		final KDTreeNode< FlagNode< T > > awayChild = leftIsNearBranch ? current.right() : current.left();
 		if ( nearChild != null )
 			searchNode( nearChild );
 
