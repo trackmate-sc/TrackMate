3489a585019cf928320a07d7cd6c7f83b1012f1f
falgunpatel07 <71875123+falgunpatel07@users.noreply.github.com>
2024-11-28 00:31:30 -0400

    Deficient Encapsulation
    design smell number one of type true positive

--- src/main/java/fiji/plugin/trackmate/action/AbstractTMAction.java
+++ src/main/java/fiji/plugin/trackmate/action/AbstractTMAction.java
@@ -1,36 +1,29 @@
-/*-
- * #%L
- * TrackMate: your buddy for everyday tracking.
- * %%
- * Copyright (C) 2010 - 2024 TrackMate developers.
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
- */
 package fiji.plugin.trackmate.action;
 
 import fiji.plugin.trackmate.Logger;
 
-public abstract class AbstractTMAction implements TrackMateAction
-{
+public abstract class AbstractTMAction implements TrackMateAction {
 
 	protected Logger logger = Logger.VOID_LOGGER;
+	private static final String INFO_TEXT = "<html>"
+			+ "Rename individual spots based on auto-naming rules. "
+			+ "All spot names are changed. There is no undo.</html>";
+	private static final String NAME = "Spot auto-naming";
+	private static final String KEY = "AUTO_NAMING";
+	public static String getInfoText() {
+		return INFO_TEXT;
+	}
+
+	public static String getName() {
+		return NAME;
+	}
+
+	public static String getKey() {
+		return KEY;
+	}
 
 	@Override
-	public void setLogger( final Logger logger )
-	{
+	public void setLogger(final Logger logger) {
 		this.logger = logger;
 	}
 }
