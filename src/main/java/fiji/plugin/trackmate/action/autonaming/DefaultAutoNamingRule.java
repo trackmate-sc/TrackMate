fb1b7860477c35d9e7232104babc62012d1becb8
falgunpatel07 <71875123+falgunpatel07@users.noreply.github.com>
2024-11-29 21:41:30 -0400

    Revert "design smell 3 Broken Modularization"
    This reverts commit 50e5ead9cd0c20894b35f21e7379277d64f569fe.

--- src/main/java/fiji/plugin/trackmate/action/autonaming/DefaultAutoNamingRule.java
+++ src/main/java/fiji/plugin/trackmate/action/autonaming/DefaultAutoNamingRule.java
@@ -28,8 +28,7 @@ import java.util.regex.Pattern;
 import fiji.plugin.trackmate.Spot;
 import fiji.plugin.trackmate.TrackModel;
 
-public class DefaultAutoNamingRule implements AutoNamingRule
-{
+public class DefaultAutoNamingRule implements AutoNamingRule {
 
 	private final String suffixSeparator;
 
@@ -39,30 +38,28 @@ public class DefaultAutoNamingRule implements AutoNamingRule
 
 	private final Pattern branchPattern;
 
-	public DefaultAutoNamingRule()
-	{
-		this( ".", "", true );
+	public DefaultAutoNamingRule() {
+		this(".", "", true);
 	}
 
-	public DefaultAutoNamingRule( final String suffixSeparator, final String branchSeparator, final boolean incrementSuffix )
-	{
+	public DefaultAutoNamingRule(final String suffixSeparator, final String branchSeparator, final boolean incrementSuffix) {
 		this.suffixSeparator = suffixSeparator;
 		this.branchSeparator = branchSeparator;
 		this.incrementSuffix = incrementSuffix;
-		this.branchPattern = Pattern.compile( "^([a-z](?:" + Pattern.quote( branchSeparator ) + "[a-z])*)$" );
+		this.branchPattern = Pattern.compile("^([a-z](?:" + Pattern.quote(branchSeparator) + "[a-z])*)$");
 	}
 
 	@Override
-	public void nameRoot( final Spot root, final TrackModel model )
-	{
-		final Integer id = model.trackIDOf( root );
-		final String trackName = model.name( id );
+	public void nameRoot(final Spot root, final TrackModel model) {
+		final Integer id = model.trackIDOf(root);
+		final String trackName = model.name(id);
 
-		final String rootName = ( incrementSuffix )
+		final String rootName = (incrementSuffix)
 				? trackName + suffixSeparator + "1"
 				: trackName;
-		root.setName( rootName );
+		root.setName(rootName);
 	}
+
 	@Override
 	public void nameBranches(final Spot mother, final Collection<Spot> siblings) {
 		// Sort siblings by their X position.
@@ -124,76 +121,94 @@ public class DefaultAutoNamingRule implements AutoNamingRule
 			bname += 1;
 		}
 	}
+
 	@Override
-	public void nameSpot( final Spot current, final Spot predecessor )
-	{
-		if ( incrementSuffix )
-		{
+	public void nameSpot(final Spot current, final Spot predecessor) {
+		if (incrementSuffix) {
 			final String name = predecessor.getName();
-			final String[] tokens = name.split( Pattern.quote( suffixSeparator ) );
-			final String idstr = tokens[ tokens.length - 1 ];
-			try
-			{
-				final Integer id = Integer.valueOf( idstr );
-				tokens[ tokens.length - 1 ] = Integer.toString( id + 1 );
-				final String name2 = String.join( suffixSeparator, tokens );
-				current.setName( name2 );
+			final String[] tokens = name.split(Pattern.quote(suffixSeparator));
+			final String idstr = tokens[tokens.length - 1];
+			try {
+				final Integer id = Integer.valueOf(idstr);
+				tokens[tokens.length - 1] = Integer.toString(id + 1);
+				final String name2 = String.join(suffixSeparator, tokens);
+				current.setName(name2);
 
+			} catch (final NumberFormatException e) {
+				AutoNamingRule.super.nameSpot(current, predecessor);
 			}
-			catch ( final NumberFormatException e )
-			{
-				AutoNamingRule.super.nameSpot( current, predecessor );
-			}
-		}
-		else
-		{
-			AutoNamingRule.super.nameSpot( current, predecessor );
+		} else {
+			AutoNamingRule.super.nameSpot(current, predecessor);
 		}
 	}
 
 	@Override
-	public String toString()
-	{
-		if ( incrementSuffix )
-			return "Append 'a', 'b' for each branch and increment spot index";
-		else
-			return "Append 'a', 'b' for each branch";
-	}
-
-	@Override
-	public String getInfoText()
-	{
+	public String getInfoText() {
 		final StringBuilder str = new StringBuilder();
-		str.append( "<html>" );
-		str.append( "Rename all the spots in a model giving to daughter branches a name derived "
-				+ "from the mother branch. The daughter branch names are determined "
-				+ "following simple rules based on the X position of spots:" );
-		str.append( "<ul>" );
-		str.append( "<li>The root (first spot) of a track takes the name of the track it belongs to.</li>" );
-		str.append( "<li>The subsequent branches are named from the mother branch they split from. Their name "
-				+ "is suffixed by 'a', 'b', ... depending on the relative X position of the sibbling spots "
-				+ "just after division.</li>" );
-		if ( !branchSeparator.isEmpty() )
-			str.append( "<li>Each of the branch character is separated from others by the character '" + branchSeparator + "'</li>" );
-		if ( !suffixSeparator.isEmpty() )
-			str.append( "<li>The branch suffix ('a' ...) is separated from the root name by the character '" + suffixSeparator + "'</li>" );
-		if ( incrementSuffix )
-			str.append( "<li>Inside a branch, the individual spots are suffixed by a supplemental index ('1', '2', ...), "
-					+ "indicating their order in the branch.</li>" );
-		str.append( "</ul>" );
-		str.append( "<p>" );
-
-		str.append( "For instance, the 3rd spot of the branch following two divisions, first one emerging "
-				+ "from the leftmost sibling and second one emerging from the rightmost sibbling, in  "
-				+ "the track named 'Track_23' will be named: <br>" );
-		String example = "Track_23" + suffixSeparator + "a" + branchSeparator + "b";
-		if ( incrementSuffix )
-			example += suffixSeparator + "3";
-		str.append( "<div style='text-align: center;'>" + example + "<br>" );
-
-		str.append( "The results are undefined if a track is not a tree "
-				+ "(if it has merge points)." );
-		str.append( "</html>" );
+		str.append("<html>");
+		str.append(generateDescriptionText());
+		str.append("<ul>");
+		str.append(generateRootNamingRuleText());
+		str.append(generateBranchNamingRuleText());
+		if (!branchSeparator.isEmpty()) {
+			str.append(generateBranchSeparatorRuleText());
+		}
+		if (!suffixSeparator.isEmpty()) {
+			str.append(generateSuffixSeparatorRuleText());
+		}
+		if (incrementSuffix) {
+			str.append(generateIncrementSuffixRuleText());
+		}
+		str.append("</ul>");
+		str.append(generateExampleDescriptionText());
+		str.append("</html>");
 		return str.toString();
+	}
+
+	// Helper Methods for Explaining Variables
+	private String generateDescriptionText() {
+		return "Rename all the spots in a model giving to daughter branches a name derived "
+				+ "from the mother branch. The daughter branch names are determined "
+				+ "following simple rules based on the X position of spots:";
+	}
+
+	private String generateRootNamingRuleText() {
+		return "<li>The root (first spot) of a track takes the name of the track it belongs to.</li>";
+	}
+
+	private String generateBranchNamingRuleText() {
+		return "<li>The subsequent branches are named from the mother branch they split from. Their name "
+				+ "is suffixed by 'a', 'b', ... depending on the relative X position of the sibling spots "
+				+ "just after division.</li>";
+	}
+
+	private String generateBranchSeparatorRuleText() {
+		return "<li>Each of the branch character is separated from others by the character '"
+				+ branchSeparator + "'</li>";
+	}
+
+	private String generateSuffixSeparatorRuleText() {
+		return "<li>The branch suffix ('a' ...) is separated from the root name by the character '"
+				+ suffixSeparator + "'</li>";
+	}
+
+	private String generateIncrementSuffixRuleText() {
+		return "<li>Inside a branch, the individual spots are suffixed by a supplemental index ('1', '2', ...), "
+				+ "indicating their order in the branch.</li>";
+	}
+
+	private String generateExampleDescriptionText() {
+		final StringBuilder exampleBuilder = new StringBuilder();
+		exampleBuilder.append("<p>");
+		exampleBuilder.append("For instance, the 3rd spot of the branch following two divisions, first one emerging "
+				+ "from the leftmost sibling and second one emerging from the rightmost sibling, in  "
+				+ "the track named 'Track_23' will be named: <br>");
+		String example = "Track_23" + suffixSeparator + "a" + branchSeparator + "b";
+		if (incrementSuffix) {
+			example += suffixSeparator + "3";
+		}
+		exampleBuilder.append("<div style='text-align: center;'>" + example + "<br>");
+		exampleBuilder.append("The results are undefined if a track is not a tree (if it has merge points).");
+		return exampleBuilder.toString();
 	}
 }
