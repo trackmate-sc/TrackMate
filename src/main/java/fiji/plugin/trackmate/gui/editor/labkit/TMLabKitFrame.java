333b4cf861c9c789bbf43e1d8b641bc447ac765d
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2025-06-20 15:08:33 +0200

    Add a close button to send the editor modifications to TrackMate.
    The button simply closes the window, and the launcher logic
    catches this event.

--- src/main/java/fiji/plugin/trackmate/gui/editor/labkit/TMLabKitFrame.java
+++ src/main/java/fiji/plugin/trackmate/gui/editor/labkit/TMLabKitFrame.java
@@ -1,20 +1,30 @@
 package fiji.plugin.trackmate.gui.editor.labkit;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.util.Arrays;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
+import fiji.plugin.trackmate.gui.Icons;
 import net.imglib2.Dimensions;
 import net.imglib2.util.Intervals;
 import net.miginfocom.swing.MigLayout;
@@ -46,6 +56,11 @@ public class TMLabKitFrame extends JFrame
 		final BoxLayout layout = new BoxLayout( leftPanel, BoxLayout.PAGE_AXIS );
 		leftPanel.setLayout( layout );
 
+		// Send to TrackMate panel.
+		final JPanel trackmatePanel = createTrackMatePanel();
+		leftPanel.add( trackmatePanel );
+		leftPanel.add( Box.createVerticalStrut( 5 ) );
+
 		// Image info
 		final JPanel imageInfoPanel = newFramedImageInfoPanel( imageLabelingModel, mainPanel );
 		imageInfoPanel.getComponents()[ 1 ].setBackground( UIManager.getColor( "Panel.background" ) );
@@ -61,7 +76,7 @@ public class TMLabKitFrame extends JFrame
 		// Add to frame.
 		add( initGui( mainPanel, leftPanel ) );
 
-		// Shutdown BDV when closing.
+		// Shutdown BDV when closing, give it the focus when activated.
 		addWindowListener( new WindowAdapter()
 		{
 			@Override
@@ -70,6 +85,48 @@ public class TMLabKitFrame extends JFrame
 				mainPanel.close();
 			}
 		} );
+	}
+
+	private static JPanel createTrackMatePanel()
+	{
+		final JPanel dark = new JPanel();
+		dark.setLayout( new BorderLayout() );
+		final JPanel title = new JPanel();
+		title.setBackground( new Color( 200, 200, 200 ) );
+		title.setLayout( new MigLayout( "insets 4pt, gap 8pt, fillx", "10[][]10" ) );
+		title.add( new JLabel( "TrackMate" ), "push" );
+		dark.setBackground( new Color( 200, 200, 200 ) );
+		dark.add( title, BorderLayout.PAGE_START );
+		final JPanel panel = new JPanel();
+		panel.setLayout( new BoxLayout( panel, BoxLayout.LINE_AXIS ) );
+		final String c = "<html>"
+				+ "<body style='width: 100%'>"
+				+ "<p>Close and send to TrackMate</p>";
+		panel.add( Box.createHorizontalStrut( 5 ) );
+		panel.add( new JLabel( c ) );
+		panel.add( createCloseButton() );
+		panel.add( Box.createHorizontalStrut( 5 ) );
+		dark.add( panel, BorderLayout.CENTER );
+		dark.setMaximumSize( new Dimension( 1000, 220 ) );
+		return dark;
+	}
+
+	private static final JButton createCloseButton()
+	{
+		final Image img = Icons.TRACKMATE_ICON.getImage();
+		final Image img2 = img.getScaledInstance( 32, 32, java.awt.Image.SCALE_SMOOTH );
+		final JButton button = new JButton( new ImageIcon( img2 ) );
+		button.addActionListener( new ActionListener()
+		{
+			@Override
+			public void actionPerformed( final ActionEvent e )
+			{
+				final Window window = SwingUtilities.getWindowAncestor( ( Component ) e.getSource() );
+				if ( window != null )
+					window.dispose();
+			}
+        } );
+		return button;
 	}
 
 	private static JPanel newFramedImageInfoPanel(
