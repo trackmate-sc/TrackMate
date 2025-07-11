64805787b374d80883e2fb37a09913c30a76ece4
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2025-07-11 19:07:27 +0200

    A SpotGlobalDetector can receive the logger from TrackMate.
    Makes sense for a detector that is suppose to deal with all the frames.
    This is similar to what is done for the trackers.

--- src/main/java/fiji/plugin/trackmate/detection/SpotGlobalDetector.java
+++ src/main/java/fiji/plugin/trackmate/detection/SpotGlobalDetector.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -21,6 +21,7 @@
  */
 package fiji.plugin.trackmate.detection;
 
+import fiji.plugin.trackmate.Logger;
 import fiji.plugin.trackmate.SpotCollection;
 import net.imglib2.algorithm.Benchmark;
 import net.imglib2.algorithm.OutputAlgorithm;
@@ -28,4 +29,16 @@ import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 
 public interface SpotGlobalDetector< T extends RealType< T > & NativeType< T > > extends OutputAlgorithm< SpotCollection >, Benchmark
-{}
+{
+
+	/**
+	 * Sets the logger to be used by this detector.
+	 *
+	 * @param logger
+	 *            the logger.
+	 */
+	public default void setLogger( final Logger logger )
+	{
+		// Do nothing by default.
+	}
+}
