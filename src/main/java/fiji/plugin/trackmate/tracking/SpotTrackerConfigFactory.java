1c8b83bd61ec8e28aeaa9fd20895dc838b4cfb02
Jean-Yves Tinevez <tinevez@pasteur.fr>
2026-07-05 22:03:36 +0200

    Interfaces for detector and tracker factories that use a Configurator.

---
+++ src/main/java/fiji/plugin/trackmate/tracking/SpotTrackerConfigFactory.java
@@ -0,0 +1,59 @@
+/*-
+ * #%L
+ * TrackMate: your buddy for everyday tracking.
+ * %%
+ * Copyright (C) 2010 - 2026 TrackMate developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package fiji.plugin.trackmate.tracking;
+
+import java.util.Map;
+
+import org.scijava.ui.config.Configurator;
+import org.scijava.ui.config.visitors.Maps;
+
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.gui.components.ConfigurationPanel;
+import fiji.plugin.trackmate.util.config.FactoryGenericConfig;
+import fiji.plugin.trackmate.util.config.GenericConfigPanel;
+
+/**
+ * Interface for tracker factories that need to be configured with a
+ * {@link Configurator} instance.
+ *
+ * @author Jean-Yves Tinevez
+ *
+ * @param <C>
+ *            the type of {@link Configurator} used to configure the detector
+ *            factory.
+ */
+public interface SpotTrackerConfigFactory< C extends Configurator > extends SpotTrackerFactory, FactoryGenericConfig< C >
+{
+
+	@Override
+	public default ConfigurationPanel getTrackerConfigurationPanel( final Model model )
+	{
+		final C config = createConfig();
+		return new GenericConfigPanel( config );
+	}
+
+	@Override
+	default Map< String, Object > getDefaultSettings()
+	{
+		return Maps.toMap( createConfig() );
+	}
+}
