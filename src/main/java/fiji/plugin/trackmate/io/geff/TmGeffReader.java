b8ce4900978c2e8f3452ebbd8b9e0877a6df1219
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2026-07-21 16:23:27 +0200

    Deserialize the log.

--- src/main/java/fiji/plugin/trackmate/io/geff/TmGeffReader.java
+++ src/main/java/fiji/plugin/trackmate/io/geff/TmGeffReader.java
@@ -1,6 +1,7 @@
 package fiji.plugin.trackmate.io.geff;
 
 import static fiji.plugin.trackmate.io.TmXmlKeys.GUI_STATE_ELEMENT_KEY;
+import static fiji.plugin.trackmate.io.TmXmlKeys.LOG_ELEMENT_KEY;
 
 import java.io.File;
 import java.io.IOException;
@@ -47,6 +48,17 @@ public class TmGeffReader
 	public TmGeffReader( final String geffPath )
 	{
 		this.geffPath = geffPath;
+	}
+
+	public String getLog()
+	{
+		if ( null == metadata )
+			metadata = GeffMetadata.readFromZarr( geffPath );
+
+		@SuppressWarnings( "unchecked" )
+		final Map< String, Object > trackmateMD = ( Map< String, Object > ) metadata.getExtra().get( "trackmate" );
+		final String log = ( String ) trackmateMD.get( LOG_ELEMENT_KEY );
+		return log;
 	}
 
 	public String getGUIState()
