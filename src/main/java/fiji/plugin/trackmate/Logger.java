f43ab42febe077306c28ddd397be700bae48061e
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2026-07-21 16:21:50 +0200

    Change the green color for readability.

--- src/main/java/fiji/plugin/trackmate/Logger.java
+++ src/main/java/fiji/plugin/trackmate/Logger.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -93,7 +93,7 @@ public abstract class Logger extends PrintWriter
 				? new Color( 0.4f, 0.4f, 0.9f )
 				: new Color( 0, 0, 0.7f );
 		GREEN_COLOR = bgIsDark
-				? new Color( 0.4f, 0.9f, 0.4f )
+				? new Color( 0.2f, 0.6f, 0.2f )
 				: new Color( 0, 0.6f, 0 );
 		ERROR_COLOR = bgIsDark
 				? new Color( 0.8f, 0, 0 ).brighter()
